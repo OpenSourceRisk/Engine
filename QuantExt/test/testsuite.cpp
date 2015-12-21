239679227f540cf5bd61204b1f5dd4fed284e87d
Roland Lichters <roland.lichters@quaternion.com>
2015-12-21 18:01:34 +0100

    initial commmit

---
+++ QuantExt/test/testsuite.cpp
@@ -0,0 +1,70 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+  Copyright (C) 2016 ...
+*/
+
+/*! \file testsuite.cpp
+    \brief wrapper calling all individual test cases
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Boost
+#include <boost/timer.hpp>
+using namespace boost;
+
+// Boost.Test
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/parameterized_test.hpp>
+using boost::unit_test::test_suite;
+
+#ifdef BOOST_MSVC
+#  include <qle/auto_link.hpp>
+#endif
+
+// Lib test suites
+// #include ...
+
+namespace {
+
+  boost::timer t;
+
+  void startTimer() { t.restart(); }
+  void stopTimer() {
+    double seconds = t.elapsed();
+    int hours = int(seconds/3600);
+    seconds -= hours * 3600;
+    int minutes = int(seconds/60);
+    seconds -= minutes * 60;
+    std::cout << endl << " QuantExt tests completed in ";
+    if (hours > 0) cout << hours << " h ";
+    if (hours > 0 || minutes > 0) cout << minutes << " m ";
+    cout << fixed << setprecision(0) << seconds << " s" << endl << endl;
+  }
+  
+}
+
+test_suite* init_unit_test_suite(int, char* []) {
+
+  std::string header = "Testing the extension library";
+  std::string rule = string(header.length(),'=');
+
+  BOOST_MESSAGE(rule);
+  BOOST_MESSAGE(header);
+  BOOST_MESSAGE(rule);
+
+  test_suite* test = BOOST_TEST_SUITE("QuantExtTestSuite");
+  
+  test->add(BOOST_TEST_CASE(startTimer));
+
+  // test->add(SomeTest::suite());
+
+  test->add(BOOST_TEST_CASE(stopTimer));
+
+  return test;
+}
