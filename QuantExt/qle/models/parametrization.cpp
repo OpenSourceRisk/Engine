271070525a19c215c2dffffc2f87b50a778ca2e6
Peter Caspers <peter.caspers@quaternion.com>
2015-12-29 14:20:22 +0100

    change the step size for first order derivatives

--- QuantExt/qle/models/parametrization.cpp
+++ QuantExt/qle/models/parametrization.cpp
@@ -22,7 +22,7 @@
 namespace QuantExt {
 
 Parametrization::Parametrization(const Currency &currency)
-    : h_(1.0E-8), h2_(1.0E-4), currency_(currency) {}
+    : h_(1.0E-6), h2_(1.0E-4), currency_(currency) {}
 
 const Currency Parametrization::currency() const { return currency_; }
 
