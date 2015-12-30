5f7e4895aead43154e933aadc2347057d9b8e915
Peter Caspers <peter.caspers@quaternion.com>
2015-12-30 11:13:58 +0100

    cosmetics in paths

--- QuantExt/configure_clang_osx.sh
+++ QuantExt/configure_clang_osx.sh
@@ -1,2 +1,2 @@
 # configure gcc, no open mp support
-./configure CXXFLAGS="-m64 -O3 -g -Wall -std=c++11 -Qunused-arguments -Wno-unused-local-typedef" --with-ql-include=/Users/peter/openxva/QuantLib/ --with-ql-lib=/Users/peter/openxva/QuantLib/ql/.libs --with-mu-include=/Users/peter/quaternion/dev/muparser-2.2.5/include --with-mu-lib=/Users/peter/quaternion/dev/muparser-2.2.5/lib
+./configure CXXFLAGS="-m64 -O3 -g -Wall -std=c++11 -Qunused-arguments -Wno-unused-local-typedef" --with-ql-include=/Users/peter/openxva/QuantLib --with-ql-lib=/Users/peter/openxva/QuantLib/ql/.libs --with-mu-include=/Users/peter/quaternion/dev/muparser-2.2.5/include --with-mu-lib=/Users/peter/quaternion/dev/muparser-2.2.5/lib
