929ee8aed516d11bd3af95c629536d1021fa3827
Peter Caspers <peter.caspers@quaternion.com>
2015-12-30 10:59:00 +0100

    fix include path for copied class

--- QuantExt/qle/math/piecewiseintegral.cpp
+++ QuantExt/qle/math/piecewiseintegral.cpp
@@ -17,7 +17,7 @@
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 
-#include <ql/experimental/math/piecewiseintegral.hpp>
+#include <qle/math/piecewiseintegral.hpp>
 
 namespace QuantLib {
 
