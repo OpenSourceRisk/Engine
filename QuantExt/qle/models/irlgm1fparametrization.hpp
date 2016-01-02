7d97bd1d82ea9b03332a1d84f04db4b3a82fe3df
Peter Caspers <peter.caspers@quaternion.com>
2016-01-02 15:53:01 +0100

    add requirements

--- QuantExt/qle/models/irlgm1fparametrization.hpp
+++ QuantExt/qle/models/irlgm1fparametrization.hpp
@@ -35,7 +35,9 @@ class IrLgm1fParametrization : public Parametrization {
     IrLgm1fParametrization(const Currency &currency,
                            const Handle<YieldTermStructure> &termStructure);
     /*! interface */
+    /*! zeta must satisfy zeta(0) = 0, zeta'(t) >= 0 */
     virtual Real zeta(const Time t) const = 0;
+    /*! H must satisfy H(0) = 0, H'(0) = 1, H'(t) > 0 */
     virtual Real H(const Time t) const = 0;
     /*! inspectors */
     virtual Real alpha(const Time t) const;
