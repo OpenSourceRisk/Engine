fffc67c04a3fe1aaeb8c284b719a1f67013c5eb9
Peter Caspers <pcaspers1973@gmail.com>
2016-01-05 18:24:43 +0100

    fix formatting

--- QuantExt/test/analyticlgmswaptionengine.cpp
+++ QuantExt/test/analyticlgmswaptionengine.cpp
@@ -1,7 +1,8 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
- Copyright (C) 2016 Quaternion Risk Management Ltd.*/
+ Copyright (C) 2016 Quaternion Risk Management Ltd.
+*/
 
 #include "analyticlgmswaptionengine.hpp"
 #include "utilities.hpp"
