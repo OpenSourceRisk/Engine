25effe6014cf51b86ad9793e9d379eb78560ce96
sinead <sinead.kelleher@quaternionrisk.com>
2016-01-07 09:25:01 +0000

    unit test for indexes

--- QuantExt/test/currency.cpp
+++ QuantExt/test/currency.cpp
@@ -15,51 +15,54 @@ using namespace boost::unit_test_framework;
 using namespace std;
 
 namespace {
+
     struct CcyTestData {
         Currency ccy;
         string name;
         string code;
         int numCode;
     };
-}
-
-
-void dataCheck(struct CcyTestData data) {
-    BOOST_CHECK_EQUAL(data.ccy.name(),data.name);
-    BOOST_CHECK_EQUAL(data.ccy.code(),data.code);
-    BOOST_CHECK_EQUAL(data.ccy.numericCode(),data.numCode);
 
 }
+
+
+void dataCheck(struct CcyTestData& data) {
+
+    BOOST_CHECK_EQUAL(data.ccy.name(), data.name);
+    BOOST_CHECK_EQUAL(data.ccy.code(), data.code);
+    BOOST_CHECK_EQUAL(data.ccy.numericCode(), data.numCode);
+}
+
 
 void CurrencyTest::testCurrency() {
 
     BOOST_MESSAGE("Testing QuantExt currencies");
 
-
     CcyTestData data[]={
         //African cuurencies
-        {TNDCurrency(),"Tunisian dinar", "TND", 788},
-        {EGPCurrency(),"Egyptian pound", "EGP", 818},
-        {NGNCurrency(),"Nigerian naira", "NGN", 566},
-        {MADCurrency(),"Moroccan dirham", "MAD", 504},
+        { TNDCurrency(), "Tunisian dinar", "TND", 788},
+        { EGPCurrency(), "Egyptian pound", "EGP", 818},
+        { NGNCurrency(), "Nigerian naira", "NGN", 566},
+        { MADCurrency(), "Moroccan dirham", "MAD", 504},
         //American currencies
-        {MXVCurrency(),"Mexican Unidad de Inversion", "MXV", 979},
-        {CLFCurrency(),"Unidad de Fomento (funds code)", "CLF", 990},
+        { MXVCurrency(), "Mexican Unidad de Inversion", "MXV", 979},
+        { CLFCurrency(), "Unidad de Fomento (funds code)", "CLF", 990},
         //Asian currencies
-        {KZTCurrency(),"Kazakhstani tenge", "KZT", 398},
-        {QARCurrency(),"Qatari riyal", "QAR", 634},
-        {BHDCurrency(),"Bahraini dinar", "BHD", 48},
-        {OMRCurrency(),"Omani rial", "OMR", 512},
-        {AEDCurrency(),"United Arab Emirates dirham", "AED", 784},
-        {PHPCurrency(),"Philippine peso", "PHP", 608},
+        { KZTCurrency(), "Kazakhstani tenge", "KZT", 398},
+        { QARCurrency(), "Qatari riyal", "QAR", 634},
+        { BHDCurrency(), "Bahraini dinar", "BHD", 48},
+        { OMRCurrency(), "Omani rial", "OMR", 512},
+        { AEDCurrency(), "United Arab Emirates dirham", "AED", 784},
+        { PHPCurrency(), "Philippine peso", "PHP", 608},
         //European currencies
-        {UAHCurrency(),"Ukrainian hryvnia", "UAH", 980}
+        { UAHCurrency(), "Ukrainian hryvnia", "UAH", 980}
     };
 
     Size size=sizeof(data)/sizeof(data[0]);
 
-    for( Size i=0; i<size;i++)
+    for(Size i=0; i<size; i++)
         dataCheck(data[i]);
+
 }
 
 
---
+++ QuantExt/test/index.cpp
@@ -0,0 +1,77 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+  Copyright (C) 2016 Quaternion Risk Management Ltd.
+  All rights reserved.
+*/
+
+#include "index.hpp"
+#include <ql/currency.hpp>
+#include <ql/index.hpp>
+#include <qle/indexes/ibor/all.hpp>
+
+using namespace QuantLib;
+using namespace QuantExt;
+using namespace boost::unit_test_framework;
+using namespace std;
+
+namespace {
+    
+    struct IndTestData {
+        IborIndex ind;
+        string name;
+        string calName;
+        string ccyName;
+    };
+
+}
+
+
+void dataCheck(struct IndTestData& data) {
+
+    BOOST_CHECK_EQUAL(data.ind.familyName(), data.name);
+    BOOST_CHECK_EQUAL(data.ind.fixingCalendar().name(), data.calName);
+    BOOST_CHECK_EQUAL(data.ind.currency().name(), data.ccyName);
+
+}
+
+
+void IndexTest::testIborIndex() {
+
+    BOOST_MESSAGE("Testing QuantExt indexes");
+
+    Period pd(3, Months);
+
+    struct IndTestData data[]= {
+        { AUDbbsw(pd), "AUD-BBSW", Australia().name(), AUDCurrency().name()},
+        { AverageBMA(pd), "AverageBMA", UnitedStates(UnitedStates::NYSE).name(), USDCurrency().name()},
+        { CHFTois(), "CHF-TOIS", Switzerland().name(), CHFCurrency().name()},
+        { CZKPribor(pd), "CZK-PRIBOR", CzechRepublic().name(), CZKCurrency().name()},
+        { DKKCibor(pd), "DKK-CIBOR", Denmark().name(), DKKCurrency().name()},
+        { HKDHibor(pd), "HKD-HIBOR", HongKong().name(), HKDCurrency().name()},
+        { HUFBubor(pd), "HUF-BUBOR", Hungary().name(), HUFCurrency().name()},
+        { IDRIdrfix(pd), "IDR-IDRFIX", Indonesia().name(), IDRCurrency().name()},
+        { INRMifor(pd), "INR-MIFOR", India().name(), INRCurrency().name()},
+        { MXNTiie(pd), "MXN-TIIE", Mexico().name(), MXNCurrency().name()},
+        { NOKNibor(pd), "NOK-NIBOR", Norway().name(), NOKCurrency().name()},
+        { NZDBKBM(pd), "NZD-BKBM", NewZealand().name(), NZDCurrency().name()},
+        { PLNWibor(pd), "PLN-WIBOR", Poland().name(), PLNCurrency().name()},
+        { SEKStibor(pd), "SEK-STIBOR", Sweden().name(), SEKCurrency().name()},
+        { SGDSibor(pd), "SGD-SIBOR", Singapore().name(), SGDCurrency().name()},
+        { SGDSor(pd), "SGD-SOR", Singapore().name(), SGDCurrency().name()},
+        { SKKBribor(pd), "SKK-BRIBOR", Slovakia().name(), SKKCurrency().name()},
+        { Tonar(), "TONAR", Japan().name(), JPYCurrency().name()}
+    };
+
+    Size size=sizeof(data)/sizeof(data[0]);
+
+    for(Size i=0; i<size; i++)
+        dataCheck(data[i]);
+ 
+}
+
+test_suite* IndexTest::suite() {
+    test_suite* suite = BOOST_TEST_SUITE("IborIndexTests");
+    suite->add(BOOST_TEST_CASE(&IndexTest::testIborIndex));
+    return suite;
+}
