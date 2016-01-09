e56797570ab2e37120a3d8a4cc62be2dfa8b2de3
Peter Caspers <peter.caspers@quaternion.com>
2016-01-09 18:11:13 +0100

    add brownian bridge multipath generator

--- QuantExt/qle/quantext.hpp
+++ QuantExt/qle/quantext.hpp
@@ -8,6 +8,7 @@
 
 #include <qle/currencies/all.hpp>
 #include <qle/math/all.hpp>
+#include <qle/methods/all.hpp>
 #include <qle/models/all.hpp>
 #include <qle/pricingengines/all.hpp>
 #include <qle/processes/all.hpp>
