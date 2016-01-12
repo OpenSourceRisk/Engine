dc10309bf0b66dd7866f341e61357741ad3af93e
Niall O'Sullivan <niall.osullivan@quaternionrisk.com>
2016-01-12 16:52:51 +0000

    Copyright 2016

--- QuantExt/qle/indexes/ibor/hkdhibor.hpp
+++ QuantExt/qle/indexes/ibor/hkdhibor.hpp
@@ -1,7 +1,7 @@
 /* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
-  Copyright (C) 2010 - 2015 Quaternion Risk Management Ltd.
+  Copyright (C) 2010 - 2016 Quaternion Risk Management Ltd.
   All rights reserved.
 */
 
