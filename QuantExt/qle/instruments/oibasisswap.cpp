a7db07c9092966afa35e1f7fdd828baed771df24
Peter Caspers <peter.caspers@quaternion.com>
2016-01-15 11:26:33 +0100

    add oi and oicc basis swap instruments

---
+++ QuantExt/qle/instruments/oibasisswap.cpp
@@ -0,0 +1,114 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2012 - 2015 Quaternion Risk Management Ltd.
+ All rights reserved
+*/
+
+#include <qle/instruments/oibasisswap.hpp>
+#include <ql/cashflows/overnightindexedcoupon.hpp>
+#include <ql/cashflows/fixedratecoupon.hpp>
+#include <ql/cashflows/iborcoupon.hpp>
+
+namespace QuantLib {
+
+    OvernightIndexedBasisSwap::OvernightIndexedBasisSwap(
+                    Type type,
+                    Real nominal,
+                    const Schedule& oisSchedule,
+                    const boost::shared_ptr<OvernightIndex>& overnightIndex,
+                    const Schedule& iborSchedule,
+                    const boost::shared_ptr<IborIndex>& iborIndex,
+                    Spread oisSpread,
+                    Spread iborSpread)
+    : Swap(2), type_(type),
+      nominals_(std::vector<Real>(1, nominal)),
+      oisSchedule_(oisSchedule), overnightIndex_(overnightIndex), 
+      iborSchedule_(iborSchedule), iborIndex_(iborIndex), 
+      oisSpread_(oisSpread), iborSpread_(iborSpread) {
+        
+        initialize();
+    }
+
+    OvernightIndexedBasisSwap::OvernightIndexedBasisSwap(
+                    Type type,
+                    std::vector<Real> nominals,
+                    const Schedule& oisSchedule,
+                    const boost::shared_ptr<OvernightIndex>& overnightIndex,
+                    const Schedule& iborSchedule,
+                    const boost::shared_ptr<IborIndex>& iborIndex,
+                    Spread oisSpread,
+                    Spread iborSpread)
+    : Swap(2), type_(type), nominals_(nominals),
+      oisSchedule_(oisSchedule), overnightIndex_(overnightIndex), 
+      iborSchedule_(iborSchedule), iborIndex_(iborIndex), 
+      oisSpread_(oisSpread), iborSpread_(iborSpread) {
+        
+        initialize();
+    }
+
+    void OvernightIndexedBasisSwap::initialize() {
+        legs_[0] = IborLeg(iborSchedule_, iborIndex_)
+            .withNotionals(nominals_)
+            .withSpreads(iborSpread_);
+
+        legs_[1] = OvernightLeg(oisSchedule_, overnightIndex_)
+            .withNotionals(nominals_)
+            .withSpreads(oisSpread_);
+
+        for (Size j=0; j<2; ++j) {
+            for (Leg::iterator i = legs_[j].begin(); i!= legs_[j].end(); ++i)
+                registerWith(*i);
+        }
+
+        switch (type_) {
+          case Payer:
+            payer_[0] = -1.0;
+            payer_[1] = +1.0;
+            break;
+          case Receiver:
+            payer_[0] = +1.0;
+            payer_[1] = -1.0;
+            break;
+          default:
+            QL_FAIL("Unknown overnight-swap type");
+        }
+    }
+
+    Spread OvernightIndexedBasisSwap::fairOvernightSpread() const {
+        static Spread basisPoint = 1.0e-4;
+        calculate();
+        return oisSpread_ - NPV_/(overnightLegBPS()/basisPoint);
+    }
+
+    Spread OvernightIndexedBasisSwap::fairIborSpread() const {
+        static Spread basisPoint = 1.0e-4;
+        calculate();
+        return iborSpread_ - NPV_/(iborLegBPS()/basisPoint);
+    }
+
+    Real OvernightIndexedBasisSwap::overnightLegBPS() const {
+        calculate();
+        QL_REQUIRE(legBPS_[1] != Null<Real>(), "result not available");
+        return legBPS_[1];
+    }
+
+    Real OvernightIndexedBasisSwap::iborLegBPS() const {
+        calculate();
+        QL_REQUIRE(legBPS_[0] != Null<Real>(), "result not available");
+        return legBPS_[0];
+    }
+
+    Real OvernightIndexedBasisSwap::iborLegNPV() const {
+        calculate();
+        QL_REQUIRE(legNPV_[0] != Null<Real>(), "result not available");
+        return legNPV_[0];
+    }
+
+    Real OvernightIndexedBasisSwap::overnightLegNPV() const {
+        calculate();
+        QL_REQUIRE(legNPV_[1] != Null<Real>(), "result not available");
+        return legNPV_[1];
+    }
+
+}
---
+++ QuantExt/qle/instruments/oiccbasisswap.hpp
@@ -0,0 +1,135 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2012 - 2015 Quaternion Risk Management Ltd.
+ All rights reserved
+*/
+
+/*! \file oiccbasisswap.hpp
+    \brief Cross currency overnight index swap paying compounded overnight vs. float
+*/
+
+#ifndef quantlib_cc_ois_basis_swap_hpp
+#define quantlib_cc_ois_basis_swap_hpp
+
+#include <ql/instruments/swap.hpp>
+#include <ql/time/daycounter.hpp>
+#include <ql/time/schedule.hpp>
+#include <ql/currency.hpp>
+
+namespace QuantLib {
+
+    class Schedule;
+    class OvernightIndex;
+    class IborIndex;
+
+    //! Basis swap: compounded overnight rate in ccy 1 vs. compounded overnight rate in ccy 2
+    class OvernightIndexedCrossCcyBasisSwap : public Swap {
+      public:
+        class arguments;
+        class results;
+        class engine;
+        OvernightIndexedCrossCcyBasisSwap(
+                             Real payNominal,
+                             Currency payCurrency,
+                             const Schedule& paySchedule,
+                             const boost::shared_ptr<OvernightIndex>& payIndex,
+                             Real paySpread,
+                             Real recNominal,
+                             Currency recCurrency,
+                             const Schedule& recSchedule,
+                             const boost::shared_ptr<OvernightIndex>& recIndex,
+                             Real recSpread);
+        //! \name Inspectors
+        //@{
+        Real payNominal() const;
+        Currency payCurrency() const;
+        const Schedule& paySchedule() { return paySchedule_; }
+        const boost::shared_ptr<OvernightIndex>& payIndex() { return payIndex_; } 
+        Real paySpread() const;
+
+        Real recNominal() const;
+        Currency recCurrency() const;
+        const Schedule& recSchedule() { return recSchedule_; }
+        const boost::shared_ptr<OvernightIndex>& recIndex() { return recIndex_; }
+        Real recSpread() const;
+
+
+        const Leg& payLeg() const { return legs_[0]; }
+        const Leg& recLeg() const { return legs_[1]; }
+        //@}
+
+        //! \name Results
+        //@{
+        Real payLegBPS() const;
+        Real payLegNPV() const;
+        Real fairPayLegSpread() const;
+        
+        Real recLegBPS() const;
+        Real recLegNPV() const;
+        Spread fairRecLegSpread() const;
+        //@}
+        // other
+        void setupArguments(PricingEngine::arguments* args) const;
+        void fetchResults(const PricingEngine::results*) const;
+      private:
+        void initialize();
+        Real payNominal_, recNominal_;
+        Currency payCurrency_, recCurrency_;
+        Schedule paySchedule_, recSchedule_;
+        boost::shared_ptr<OvernightIndex> payIndex_, recIndex_;
+        Spread paySpread_, recSpread_;
+        std::vector<Currency> currency_;
+
+        mutable Real fairPayLegSpread_;
+        mutable Real fairRecLegSpread_;
+    };
+
+    // inline
+
+    inline Real OvernightIndexedCrossCcyBasisSwap::payNominal() const {
+        return payNominal_;
+    }
+
+    inline Real OvernightIndexedCrossCcyBasisSwap::recNominal() const {
+        return recNominal_;
+    }
+
+    inline Currency OvernightIndexedCrossCcyBasisSwap::payCurrency() const {
+        return payCurrency_;
+    }
+
+    inline Currency OvernightIndexedCrossCcyBasisSwap::recCurrency() const {
+        return recCurrency_;
+    }
+
+    inline Real OvernightIndexedCrossCcyBasisSwap::paySpread() const {
+        return paySpread_;
+    }
+
+    inline Real OvernightIndexedCrossCcyBasisSwap::recSpread() const {
+        return recSpread_;
+    }
+
+    class OvernightIndexedCrossCcyBasisSwap::arguments : public Swap::arguments {
+      public:
+        std::vector<Currency> currency;
+        Real paySpread;
+        Real recSpread;
+
+    };
+
+    class OvernightIndexedCrossCcyBasisSwap::results : public Swap::results {
+        public:
+        Real fairPayLegSpread;
+        Real fairRecLegSpread;
+        void reset();
+    };
+
+    class OvernightIndexedCrossCcyBasisSwap::engine 
+        : public GenericEngine<OvernightIndexedCrossCcyBasisSwap::arguments,
+                               OvernightIndexedCrossCcyBasisSwap::results> {};
+
+}
+
+#endif
