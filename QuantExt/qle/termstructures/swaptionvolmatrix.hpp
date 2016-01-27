ad3e9f5804785f53f59454ba9498162b1b206030
Peter Caspers <peter.caspers@quaternion.com>
2016-01-27 11:25:01 +0100

    mirror QL pull request

--- QuantExt/qle/termstructures/swaptionvolmatrix.hpp
+++ QuantExt/qle/termstructures/swaptionvolmatrix.hpp
@@ -185,7 +185,7 @@ namespace QuantExt {
     }
 
     inline Rate SwaptionVolatilityMatrix::minStrike() const {
-        return QL_MIN_REAL;
+        return -QL_MAX_REAL;
     }
 
     inline Rate SwaptionVolatilityMatrix::maxStrike() const {
