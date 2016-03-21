b72b65d1c47aa81addc8901fe90fb49e60d42ffe
roland <roland.lichters@quaternion.com>
2016-03-21 19:44:48 +0100

    timebomb added to ship executable to Trinity and HSH

---
+++ App/timebomb.hpp
@@ -0,0 +1,37 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+  Copyright (C) 2011 - 2016 Quaternion Risk Management Ltd.
+  All rights reserved.
+*/
+
+#pragma once
+
+#include <ql/time/date.hpp>
+#include <version.hpp>
+
+/* Timebomb macro, to be put at begining of every main()
+ */
+
+
+//! no timebomb
+//#define QRE_CHECK_TIMEBOMB 
+
+
+/* Timebomb date hardcoded to 1 May 2016
+ * Use __DATE__ macro to enable "Release date + 90 days" type eval periods.
+ */
+
+#define ORE_CHECK_TIMEBOMB \
+  { \
+    QuantLib::Date timebombDate = QuantLib::Date(1, QuantLib::May, 2016); \
+    QuantLib::Date today = QuantLib::Date::todaysDate(); \
+    if (today >= timebombDate) { \
+        printVersion(); \
+        std::cout << std::endl; \
+        std::cout << "** Evaluation Period Expired **" << std::endl; \
+        std::cout << "Contact info@quaternion.com for licence renewal." << std::endl; \
+        exit (-1); \
+    } \
+  } 
+
---
+++ App/version.hpp
@@ -0,0 +1,34 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+  Copyright (C) 2011 - 2016 Quaternion Risk Management Ltd.
+  All rights reserved.
+*/
+
+#pragma once
+
+#include <iostream>
+
+inline std::string oreVersion () {
+    return "0.1";
+}
+
+inline void printVersion() {
+    std::cout << "Open Risk Engine Version " << oreVersion() << std::endl;
+    std::cout << "Build Date " << __DATE__ << " " << __TIME__ << std::endl;
+    std::cout << "Copyright (C) 2010 - 2016 Quaternion Risk Management Ltd. All Rights Reserved" << std::endl;
+    std::cout << "www.quaternion.com" << std::endl;
+}
+
+// Checks for "-version" or "--version" on the command line paramters.
+// if present, it prints the version and exits with return code 0.
+// We do this here, so we can disply the version number even if ENV_FILE is not set
+inline void checkVersion(int argc, char** argv) {
+    for (unsigned int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "-version" ||
+            std::string(argv[i]) == "--version") {
+            printVersion();
+            exit(0);
+        }
+    }
+}
