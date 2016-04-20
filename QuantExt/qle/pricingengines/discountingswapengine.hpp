a3801f4f04d9a7c8ac082d51e21fe93d30ec0f2c
Peter Caspers <peter.caspers@quaternion.com>
2016-04-20 17:36:38 +0200

    add detail about today's estimation for ibor coupons

--- QuantExt/qle/pricingengines/discountingswapengine.hpp
+++ QuantExt/qle/pricingengines/discountingswapengine.hpp
@@ -25,7 +25,9 @@
 
 /*! \file discountingswapengine.hpp
     \brief discounting swap engine supporting simulated fixings,
-           includes a par coupon approximation for ibor coupons
+           includes a par coupon approximation for ibor coupons,
+           if used today's fixing will always be estimated on
+           the curve for ibor coupons
 */
 
 #ifndef quantext_discounting_swap_engine_hpp
