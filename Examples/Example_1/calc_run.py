6cc95c2a555772a87e6a764ecb8d626bb390dcb5
roland <roland.lichters@quaternion.com>
2016-06-09 14:33:42 +0200

    launcher extensions

--- Examples/Example_1/calc_run.py
+++ Examples/Example_1/calc_run.py
@@ -6,6 +6,34 @@ import datetime
 from subprocess import call
 
 ###############################################
+# Function definitions                        #
+###############################################
+
+def copyCsvToSheet(fileName, sheet):
+    i = 0
+    import csv
+    with open(fileName) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            j = 0
+            for col in row:
+                cell = sheet.getCellByPosition(j,i)
+                cell.String = row[j]
+                j = j + 1
+            i = i + 1
+    return
+
+def copyXmlToSheet(fileName, sheet):
+    with open(fileName) as f:
+        alist = f.read().splitlines()
+    i = 0
+    for line in alist:
+        cell = sheet.getCellByPosition(0,i)
+        cell.String = line
+        i = i + 1
+    return
+
+###############################################
 # Establish contact with Calc                 #
 ###############################################
 
@@ -39,6 +67,31 @@ cell = active_sheet.getCellRangeByName("B5")
 cmd = "../../App/ore " + fileName
 cell.String = "running ORE with " + fileName + " ..."
 return_code = call(cmd, shell=True)
+
+# Import NPV report
+sheets = model.getSheets()
+sheets.insertNewByName('NPV', 1)
+sheet = sheets.getByName('NPV')
+copyCsvToSheet('Output/npv.csv', sheet)
+
+# Import CASHFLOW report
+sheets = model.getSheets()
+sheets.insertNewByName('Cashflow', 2)
+sheet = sheets.getByName('Cashflow')
+copyCsvToSheet('Output/flows.csv', sheet)
+        
+# Import XVA report
+sheets = model.getSheets()
+sheets.insertNewByName('XVA', 3)
+sheet = sheets.getByName('XVA')
+copyCsvToSheet('Output/xva.csv', sheet)
+
+# Import ore.xml
+sheets = model.getSheets()
+sheets.insertNewByName('ORE', 4)
+sheet = sheets.getByName('ORE')
+copyXmlToSheet('Input/ore.xml', sheet)
+
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 cell.String = st + " ORE completed, return code " + str(return_code)
