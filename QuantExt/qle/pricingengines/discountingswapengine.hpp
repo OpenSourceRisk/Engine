c8c043b674a97a1360e5921e145d05c53be8f26c
Niall O'Sullivan <niall.osullivan@quaternionrisk.com>
2016-07-01 16:01:41 +0100

    Licence update

--- QuantExt/qle/pricingengines/discountingswapengine.hpp
+++ QuantExt/qle/pricingengines/discountingswapengine.hpp
@@ -1,10 +1,3 @@
-/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
-/*
-  Copyright (C) 2016 Quaternion Risk Management Ltd.
-  All rights reserved.
-*/
-
 /*
  Copyright (C) 2007, 2009 StatPro Italia srl
  Copyright (C) 2011 Ferdinando Ametrano
@@ -23,11 +16,29 @@
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 
+/*
+ Copyright (C) 2016 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of OpenRiskEngine, a free-software/open-source library
+ for transparent pricing and risk analysis - http://openriskengine.org
+
+ OpenRiskEngine is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program; if not, please email
+ <users@openriskengine.org>. The license is also available online at
+ <http://openriskengine.org/license.shtml>.
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+
 /*! \file qle/pricingengines/discountingswapengine.hpp
     \brief discounting swap engine supporting simulated fixings,
-           includes a par coupon approximation for ibor coupons,
-           if used today's fixing will always be estimated on
-           the curve for ibor coupons
+    \ingroup 
 */
 
 #ifndef quantext_discounting_swap_engine_hpp
--- QuantExt/qle/pricingengines/discountingswapenginemulticurve.hpp
+++ QuantExt/qle/pricingengines/discountingswapenginemulticurve.hpp
@@ -1,12 +1,26 @@
-/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
 /*
- Copyright (C) 2014 - 2015 Quaternion Risk Management Ltd.
+ Copyright (C) 2016 Quaternion Risk Management Ltd
  All rights reserved.
+
+ This file is part of OpenRiskEngine, a free-software/open-source library
+ for transparent pricing and risk analysis - http://openriskengine.org
+
+ OpenRiskEngine is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program; if not, please email
+ <users@openriskengine.org>. The license is also available online at
+ <http://openriskengine.org/license.shtml>.
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
 */
+
 
 /*! \file qle/pricingengines/discountingswapenginemulticurve.hpp
     \brief Swap engine employing assumptions to speed up calculation
+    \ingroup 
 */
 
 #ifndef quantext_discounting_swap_engine_multi_curve_hpp
