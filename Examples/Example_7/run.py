fe20a0aff9f09eaf634c7809c677dbe0b6379ca7
Nikolai Nowaczyk <nikolai.nowaczyk@quaternionrisk.com>
2016-07-04 17:38:04 +0100

    added first draft of Example_7

---
+++ Examples/Example_7/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import matplotlib.pyplot as plt
+import runpy
+
+ore_helpers = runpy.run_path(os.path.join(os.path.dirname(os.getcwd()), "ore_examples_helper.py"))
+OreExample = ore_helpers['OreExample']
+
+oreex = OreExample()
+
+oreex.print_headline("Run ORE to produce NPV cube and exposures, without collateral")
+oreex.run("Input/ore.xml")
+oreex.get_times("Output/log.txt")
