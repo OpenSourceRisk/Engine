39137886b2ffcd8980b9ac77900c28a16de6cf2e
Nikolai Nowaczyk <nikolai.nowaczyk@quaternionrisk.com>
2016-07-05 11:18:52 +0100

    added Example_11

--- Examples/Example_10/run.py
+++ Examples/Example_10/run.py
@@ -7,7 +7,7 @@ import runpy
 ore_helpers = runpy.run_path(os.path.join(os.path.dirname(os.getcwd()), "ore_examples_helper.py"))
 OreExample = ore_helpers['OreExample']
 
-oreex = OreExample(True)
+oreex = OreExample()
 
 oreex.print_headline("Run ORE to produce NPV cube and exposures, without collateral")
 oreex.run("Input/ore.xml")
---
+++ Examples/Example_11/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import matplotlib.pyplot as plt
+import runpy
+
+ore_helpers = runpy.run_path(os.path.join(os.path.dirname(os.getcwd()), "ore_examples_helper.py"))
+OreExample = ore_helpers['OreExample']
+
+oreex = OreExample()
+
+oreex.print_headline("Run ORE to produce SIMM output")
+oreex.run("Input/ore.xml")
+
