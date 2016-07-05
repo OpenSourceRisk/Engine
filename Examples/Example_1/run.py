906115f8191ff61e45f6f2198abe7dd2978f5515
Nikolai Nowaczyk <nikolai.nowaczyk@quaternionrisk.com>
2016-07-05 11:14:34 +0100

    updates all examples but 5 to use plotting wrappers

--- Examples/Example_1/run.py
+++ Examples/Example_1/run.py
@@ -16,20 +16,16 @@ oreex.get_times("Output/log.txt")
 oreex.print_headline("Run ORE again to price European Swaptions")
 oreex.run("Input/ore_swaption.xml")
 
-oreex.print_headline("Plot results: Simulated exposures vs analytical swaption prices")
-time_swap = oreex.get_output_data_from_column("exposure_trade_Swap_20y.csv", 2)
-epes = oreex.get_output_data_from_column("exposure_trade_Swap_20y.csv", 3)
-enes = oreex.get_output_data_from_column("exposure_trade_Swap_20y.csv", 4)
+
 times_swaption = oreex.get_output_data_from_column("swaption_npv.csv", 3)
 npv = oreex.get_output_data_from_column("swaption_npv.csv", 4)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(time_swap, epes, color='b', label="EPE")
-ax.plot(time_swap, enes, color='r', label="ENE")
-ax.plot(times_swaption, npv, color='g', label="NPV")
-ax.legend(loc='upper right', shadow=True)
-ax.set_xlabel("Time / Years")
-ax.set_ylabel("Exposure")
-ax.set_title("Example 1")
+
+oreex.plot(ax, "exposure_trade_Swap_20y.csv", 2, 3, 'b', "EPE")
+oreex.plot(ax, "exposure_trade_Swap_20y.csv", 2, 4, 'r', "ENE")
+oreex.plot(ax, "swaption_npv.csv", 3, 4, 'r', "NPV")
+oreex.decorate_plot(ax, title="Example 1 - Simulated exposures vs analytical swaption prices")
+
 plt.show()
