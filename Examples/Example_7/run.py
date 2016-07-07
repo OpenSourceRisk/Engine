7382bd8a66b790d488c8feaf1b391e338be2cddf
Nikolai <mail@nikno.de>
2016-07-07 18:54:00 +0100

    added plots for example 7

--- Examples/Example_7/run.py
+++ Examples/Example_7/run.py
@@ -11,3 +11,11 @@ oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 oreex.print_headline("Run ORE to produce NPV cube and exposures, without collateral")
 oreex.run("Input/ore.xml")
 oreex.get_times("Output/log.txt")
+
+oreex.setup_plot("example_swap_cash_physical")
+oreex.plot("exposure_trade_Swap.csv", 2, 3, 'b', "EPE Swap")
+oreex.plot("exposure_trade_SwaptionCash.csv", 2, 4, 'r', "EPE Swaption Cash")
+oreex.plot("exposure_trade_SwaptionPhysical.csv", 2, 4, 'g', "EPE Swaption Physical")
+oreex.decorate_plot(title="Example 7")
+oreex.save_plot_to_file()
+
