08351a2e3cfe7c3d0f98acdfc2c91b2b507caaf2
alexey <alexey.indiryakov@quaternionrisk.com>
2017-01-23 09:06:37 +0100

    add euhicp, frhicp, zacpi

--- OREData/ored/utilities/indexparser.cpp
+++ OREData/ored/utilities/indexparser.cpp
@@ -183,27 +183,26 @@ boost::shared_ptr<SwapIndex> parseSwapIndex(const string& s, const Handle<YieldT
 // Zero Inflation Index Parser
 class ZeroInflationIndexParserBase {
 public:
-    virtual boost::shared_ptr<ZeroInflationIndex> build(bool isInterpolated,
-                                                        const Handle<ZeroInflationTermStructure>& h) const = 0;
+    virtual boost::shared_ptr<ZeroInflationIndex> build(bool isInterpolated, const Handle<ZeroInflationTermStructure>& h) const = 0;
 };
 
 template <class T> class ZeroInflationIndexParser : public ZeroInflationIndexParserBase {
 public:
-    boost::shared_ptr<ZeroInflationIndex> build(bool isInterpolated,
-                                                const Handle<ZeroInflationTermStructure>& h) const override {
+    boost::shared_ptr<ZeroInflationIndex> build(bool isInterpolated, const Handle<ZeroInflationTermStructure>& h) const override {
         return boost::make_shared<T>(isInterpolated, h);
     }
 };
 
-boost::shared_ptr<ZeroInflationIndex> parseZeroInflationIndex(const string& s, bool isInterpolated,
-                                                              const Handle<ZeroInflationTermStructure>& h) {
+boost::shared_ptr<ZeroInflationIndex> parseZeroInflationIndex(const string& s, bool isInterpolated, const Handle<ZeroInflationTermStructure>& h) {
     
     static map<string, boost::shared_ptr<ZeroInflationIndexParserBase>> m = {
+        //{"AUCPI", boost::make_shared<ZeroInflationIndexParser<AUCPI>>()},
         {"EUHICP", boost::make_shared<ZeroInflationIndexParser<EUHICP>>()},
         {"EUHICPXT", boost::make_shared<ZeroInflationIndexParser<EUHICPXT>>()},
-        {"UKRPI", boost::make_shared<ZeroInflationIndexParser<UKRPI>>()},
         {"FRHICP", boost::make_shared<ZeroInflationIndexParser<FRHICP>>()},
-        {"USCPI", boost::make_shared<ZeroInflationIndexParser<USCPI>>()}};
+        {"UKRPI", boost::make_shared<ZeroInflationIndexParser<UKRPI>>()},
+        {"USCPI", boost::make_shared<ZeroInflationIndexParser<USCPI>>()},
+        {"ZACPI", boost::make_shared<ZeroInflationIndexParser<ZACPI>>()}};
     
     auto it = m.find(s);
     if (it != m.end()) {
