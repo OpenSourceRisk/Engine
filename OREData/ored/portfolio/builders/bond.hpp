b2fe14062420b92d996620eb62855ed1a528468c
Damien Barker <damien.barker@quaternion.com>
2017-02-27 09:46:18 +0000

    formatting cleanup on bond builder

--- OREData/ored/portfolio/builders/bond.hpp
+++ OREData/ored/portfolio/builders/bond.hpp
@@ -1,19 +1,19 @@
 /*
-Copyright (C) 2017 Quaternion Risk Management Ltd
-All rights reserved.
+ Copyright (C) 2017 Quaternion Risk Management Ltd
+ All rights reserved.
 
-This file is part of ORE, a free-software/open-source library
-for transparent pricing and risk analysis - http://opensourcerisk.org
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
 
-ORE is free software: you can redistribute it and/or modify it
-under the terms of the Modified BSD License.  You should have received a
-copy of the license along with this program.
-The license is also available online at <http://opensourcerisk.org>
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
 
-This program is distributed on the basis that it will form a useful
-contribution to risk analytics and model standardisation, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
 */
 
 /*! \file portfolio/builders/bond.hpp
@@ -62,8 +62,8 @@ public:
 
 protected:
     virtual boost::shared_ptr<PricingEngine> engineImpl(const Currency& ccy, const string& issuerId,
-        const string& securityId,
-        const string& referenceCurveId) override {
+                                                        const string& securityId,
+                                                        const string& referenceCurveId) override {
 
         string tsperiodStr = engineParameters_.at("TimestepPeriod");
         Period tsperiod = parsePeriod(tsperiodStr);
