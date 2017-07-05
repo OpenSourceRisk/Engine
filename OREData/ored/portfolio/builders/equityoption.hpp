a351c170598d8e5dd031899082a0f3ba92362cca
Sinead Kelleher <sinead.kelleher@quaternionrisk.com>
2017-07-05 16:58:36 +0100

    equity curve fixes

--- OREData/ored/portfolio/builders/equityoption.hpp
+++ OREData/ored/portfolio/builders/equityoption.hpp
@@ -28,6 +28,7 @@
 #include <ored/portfolio/enginefactory.hpp>
 #include <ql/pricingengines/vanilla/analyticeuropeanengine.hpp>
 #include <ql/processes/blackscholesprocess.hpp>
+#include <ql/version.hpp>
 #include <ored/utilities/log.hpp>
 
 namespace ore {
@@ -49,18 +50,21 @@ protected:
 
     virtual boost::shared_ptr<PricingEngine> engineImpl(const string& equityName, const Currency& ccy) override {
         string key = keyImpl(equityName, ccy);
-        
-        boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = boost::make_shared<GeneralizedBlackScholesProcess>(
-            market_->equitySpot(equityName, configuration(MarketContext::pricing)),
-            market_->equityDividendCurve(equityName,
+        if( QL_VERSION >= "1.10") {
+            boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = boost::make_shared<GeneralizedBlackScholesProcess>(
+                market_->equitySpot(equityName, configuration(MarketContext::pricing)),
+                market_->equityDividendCurve(equityName,
                                          configuration(MarketContext::pricing)), // dividend yield ~ foreign yield
-            market_->equityForecastCurve(equityName,configuration(MarketContext::pricing)),
-            market_->equityVol(equityName, configuration(MarketContext::pricing)));
+                market_->equityForecastCurve(equityName,configuration(MarketContext::pricing)),
+                market_->equityVol(equityName, configuration(MarketContext::pricing)));
         // separate IR curves required for "discounting" and "forward price estimation"
-        Handle<YieldTermStructure> discountCurve =
-            market_->discountCurve(ccy.code(), configuration(MarketContext::pricing));
+            Handle<YieldTermStructure> discountCurve =
+                market_->discountCurve(ccy.code(), configuration(MarketContext::pricing));
+            return boost::make_shared<QuantLib::AnalyticEuropeanEngine>(gbsp, discountCurve);
+        } else 
+            QL_FAIL("ORE does not support EquityOptionswith QL 1.8 or 1.9. Please upgrade to 1.10");
         
-        return boost::make_shared<QuantLib::AnalyticEuropeanEngine>(gbsp, discountCurve);
+        
     }
 };
 
