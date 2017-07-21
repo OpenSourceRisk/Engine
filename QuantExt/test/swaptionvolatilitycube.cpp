8e8bec6fd0c948a301092989d87671e249ed1c3d
Peter Caspers <peter.caspers@quaternion.com>
2017-07-21 13:06:35 +0200

    fix broken file

--- QuantExt/test/swaptionvolatilitycube.cpp
+++ QuantExt/test/swaptionvolatilitycube.cpp
@@ -1,4 +1,4 @@
-waptionVolatilityCube
+/*
  Copyright (C) 2016 Quaternion Risk Management Ltd
  All rights reserved.
 
@@ -62,7 +62,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
     optionTenors.push_back(Period(5,Years));
     optionTenors.push_back(Period(10,Years));
     optionTenors.push_back(Period(30,Years));
-    
+
     std::vector<Period> swapTenors;
     swapTenors.push_back(Period(1,Years));
     swapTenors.push_back(Period(5,Years));
@@ -75,18 +75,18 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
     strikeSpreads.push_back(-0.000);
     strikeSpreads.push_back(+0.005);
     strikeSpreads.push_back(+0.020);
-    
+
     Natural settlementDays = 0;
     DayCounter dc = ActualActual();
-    VolatilityType volType = QuantLib::ShiftedLognormal; 
+    VolatilityType volType = QuantLib::ShiftedLognormal;
     std::vector<std::vector<Real> > shift(optionTenors.size(), std::vector<Real>(swapTenors.size(), 0.0050));
 
-    
+
     //building vol quotes matrix
     std::vector<std::vector<std::vector<Handle<Quote> > > > volsHandle(strikeSpreads.size(), std::vector<std::vector<Handle<Quote> > >
                 (optionTenors.size(), std::vector<Handle<Quote> >(swapTenors.size(),Handle<Quote>())));
-    
-    std::vector<std::vector<std::vector<boost::shared_ptr<SimpleQuote> > > > volsQuote(strikeSpreads.size(), 
+
+    std::vector<std::vector<std::vector<boost::shared_ptr<SimpleQuote> > > > volsQuote(strikeSpreads.size(),
                 std::vector<std::vector<boost::shared_ptr<SimpleQuote> > > (optionTenors.size(), std::vector<boost::shared_ptr<SimpleQuote> >
                 (swapTenors.size(),boost::shared_ptr<SimpleQuote>())));
 
@@ -108,7 +108,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
             vols[1][3][0]=0.1650; vols[1][3][1]=0.1480; vols[1][3][2]=0.1380; vols[1][3][3]=0.1230;
             vols[1][4][0]=0.1410; vols[1][4][1]=0.1310; vols[1][4][2]=0.1260; vols[1][4][3]=0.1110;
             vols[1][5][0]=0.1140; vols[1][5][1]=0.1100; vols[1][5][2]=0.1080; vols[1][5][3]=0.0940;
-            
+
             //this is the ATM level
             vols[2][0][0]=0.1300; vols[2][0][1]=0.1560; vols[2][0][2]=0.1390; vols[2][0][3]=0.1220;
             vols[2][1][0]=0.1440; vols[2][1][1]=0.1580; vols[2][1][2]=0.1460; vols[2][1][3]=0.1260;
@@ -125,7 +125,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
             vols[3][4][0]=0.1390; vols[3][4][1]=0.1290; vols[3][4][2]=0.1240; vols[3][4][3]=0.1090;
             vols[3][5][0]=0.1120; vols[3][5][1]=0.1080; vols[3][5][2]=0.1060; vols[3][5][3]=0.0920;
 
-            
+
             //strikeSpread 0.02
             vols[4][0][0]=0.1280; vols[4][0][1]=0.1540; vols[4][0][2]=0.1370; vols[4][0][3]=0.1200;
             vols[4][1][0]=0.1420; vols[4][1][1]=0.1560; vols[4][1][2]=0.1440; vols[4][1][3]=0.1240;
@@ -145,7 +145,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
     }
 
     //storing the vol spreads
-    std::vector<std::vector<Handle<Quote> > > volSpreadsHandle( optionTenors.size()*swapTenors.size(), 
+    std::vector<std::vector<Handle<Quote> > > volSpreadsHandle( optionTenors.size()*swapTenors.size(),
             std::vector<Handle<Quote> >(strikeSpreads.size(), Handle<Quote>()));
 
     for (Size i = 0; i < strikeSpreads.size(); i++) {
@@ -156,7 +156,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
             }
         }
     }
-    
+
     //building swapVolCube2
     boost::shared_ptr<YieldTermStructure> yts(
             new FlatForward(today, 0.05, ActualActual()));
@@ -165,22 +165,22 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
             EuriborSwapIsdaFixA(2*Years, termStructure));
     boost::shared_ptr<SwapIndex> shortSwapIndexBase = boost::shared_ptr<SwapIndex>(new
             EuriborSwapIsdaFixA(1*Years, termStructure));
-    
-    boost::shared_ptr<SwaptionVolatilityStructure> atm = 
+
+    boost::shared_ptr<SwaptionVolatilityStructure> atm =
             boost::shared_ptr<SwaptionVolatilityStructure>(new SwaptionVolatilityMatrix(
                     cal, bdc, optionTenors, swapTenors, volsHandle[2], dc,
                 false, QuantLib::ShiftedLognormal, shift));
 
     Handle<SwaptionVolatilityStructure> hATM(atm);
-    
+
     boost::shared_ptr<QuantExt::SwaptionVolCube2> cubeATM = boost::make_shared<QuantExt::SwaptionVolCube2>(
                 hATM, optionTenors, swapTenors, strikeSpreads, volSpreadsHandle, swapIndexBase, shortSwapIndexBase,
                 false, false);
     cubeATM->enableExtrapolation();
 
     //building swaptionvolatilitycube
-    boost::shared_ptr<QuantExt::SwaptionVolatilityCube> cubeFull( new QuantExt::SwaptionVolatilityCube(                                          
-                optionTenors, swapTenors, strikeSpreads, volsHandle, swapIndexBase, 
+    boost::shared_ptr<QuantExt::SwaptionVolatilityCube> cubeFull( new QuantExt::SwaptionVolatilityCube(
+                optionTenors, swapTenors, strikeSpreads, volsHandle, swapIndexBase,
                 shortSwapIndexBase, false, volType, bdc, dc, cal, 0, shift));
     cubeFull->enableExtrapolation();
 
@@ -204,7 +204,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
         for (Size j = 0; j < optionTenors.size(); ++j) {
             for (Size k = 0; k < swapTenors.size(); ++k) {
                 BOOST_TEST_MESSAGE(strike<<" "<<j<<" "<<k);
-                BOOST_CHECK_CLOSE(cubeATM->volatility(optionTenors[j], swapTenors[k], strike), 
+                BOOST_CHECK_CLOSE(cubeATM->volatility(optionTenors[j], swapTenors[k], strike),
                     cubeFull->volatility(optionTenors[j], swapTenors[k], strike), 1e-12);
             }
         }
@@ -223,7 +223,7 @@ void SwaptionVolatilityCubeTest::testSwaptionVolatilityCube() {
         for (Size j = 0; j < optionTenors.size(); ++j) {
             for (Size k = 0; k < swapTenors.size(); ++k) {
                 BOOST_TEST_MESSAGE(strike<<" "<<j<<" "<<k);
-                BOOST_CHECK_CLOSE(cubeATM->volatility(optionTenors[j], swapTenors[k], strike), 
+                BOOST_CHECK_CLOSE(cubeATM->volatility(optionTenors[j], swapTenors[k], strike),
                     cubeFull->volatility(optionTenors[j], swapTenors[k], strike), 1e-12);
             }
         }
