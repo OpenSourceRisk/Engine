9092bd719c91f27386e1f0b6cee08a2fa0054351
nikno <mail@nikno.de>
2017-08-18 10:54:37 +0100

    test code generation automated

--- Examples/run_examples_testsuite.py
+++ Examples/run_examples_testsuite.py
@@ -6,16 +6,22 @@ import shutil
 import numpy as np
 
 
-from ore_examples_helper import run_example
+from ore_examples_helper import run_example, examples
+import functools
 
 output_directory = "Output"
 
 
 class ExamplesTest(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        pass
+    def __init__(self, *args, **kwargs):
+        def boilerplate_test(example_directory):
+            self.check_example(example_directory)
+        super(ExamplesTest, self).__init__(*args, **kwargs)
+        for example in examples[:2]:
+            test_method = functools.partial(boilerplate_test, example_directory=example)
+            test_method.__name__ = "test_" + example
+            setattr(ExamplesTest, test_method.__name__, test_method)
 
     def _clear_output_directory(self, example_subdir):
         example_dir = os.path.join(os.getcwd(), example_subdir)
@@ -45,80 +51,10 @@ class ExamplesTest(TestCase):
         self.assertTrue(len([f for f in produced_output_files if ".pdf" in f]) > 0)
 
     def check_example(self, example_subdir):
-        self._clear_output_directory(example_subdir)
+        #self._clear_output_directory(example_subdir)
         oreex = run_example(example_subdir)
-        self._check_output(example_subdir)
+        #self._check_output(example_subdir)
         self.assertEqual(np.all(np.array(oreex.return_codes, dtype=int)), 0)
 
 
-
-
-    def test_Example_1(self):
-        self.check_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_2(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_3(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_4(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_5(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_6(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_7(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_8(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_9(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_10(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_11(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_12(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_13(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_14(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_15(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_16(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_17(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_18(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_19(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_20(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_21(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_22(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
-    #
-    # def test_Example_23(self):
-    #     self.run_example(inspect.stack()[0][3].split("test_")[1])
 
