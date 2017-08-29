97e3dc8a1b4b12e2fde8af062dc502fd6d819710
Sinead Kelleher <sinead.kelleher@quaternionrisk.com>
2017-08-29 15:57:58 +0100

    combine xsd files

--- xsd/check.sh
+++ xsd/check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 function validate {
-find Examples -name $1 -print0 | \
+find $1 -name $2 -print0 | \
 { 
 fail=0
     while read -d $'\0' file
     do
-        OUTPUT=$(xmllint --schema $2 --path xsd --noout $file 2>&1)
+        OUTPUT=$(xmllint --schema $3 --path xsd --noout $file 2>&1)
         if [ "$?" -gt "0" ] 
         then 
         fail=1
@@ -15,19 +15,9 @@ fail=0
     done
 return $fail
 }
-
 }
 
 status=0
-validate 'conventions.xml' conventions.xsd || status=1
-validate 'curveconfig.xml' curveconfig.xsd || status=1
-validate 'portfolio*.xml' instruments.xsd || status=1
-validate 'netting*.xml' nettingsetdefinitions.xsd || status=1
-validate 'simulation*.xml' simulation.xsd || status=1
-validate 'pricingeng*.xml' pricingengines.xsd || status=1
-validate 'todaysmarket*.xml' todaysmarket.xsd || status=1
-validate 'sensi*.xml' sensitivity.xsd || status=1
-validate 'stress*.xml' stress.xsd || status=1
-validate 'ore*.xml' ore.xsd || status=1
+validate Examples '*.xml' input.xsd || status=1
 
 exit $status
