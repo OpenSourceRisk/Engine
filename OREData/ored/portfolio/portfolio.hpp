1a987a1c79621ea01977ab7357a9b66e7bd4cbe4
nikno <mail@nikno.de>
2017-09-25 14:08:11 +0100

    made spacing in portfolio.hpp consistent

--- OREData/ored/portfolio/portfolio.hpp
+++ OREData/ored/portfolio/portfolio.hpp
@@ -42,20 +42,27 @@ class Portfolio {
 public:
     //! Default constructor
     Portfolio() {}
+
     //! Add a trade to the portfoliio
     void add(const boost::shared_ptr<Trade>& trade) { trades_.push_back(trade); }
+
     //! Clear the portfolio
     void clear() { trades_.clear(); }
+
     //! Reset all trade data
     void reset();
+
     //! Portfolio size
     QuantLib::Size size() const { return trades_.size(); }
+
     //! Load using a default or user supplied TradeFactory, existing trades are kept
     void load(const std::string& fileName,
               const boost::shared_ptr<TradeFactory>& tf = boost::make_shared<TradeFactory>());
+
     //! Load from an XML string using a default or user supplied TradeFactory, existing trades are kept
     void loadFromXMLString(const std::string& xmlString,
         const boost::shared_ptr<TradeFactory>& tf = boost::make_shared<TradeFactory>());
+
     //! Save portfolio to an XML file
     void save(const std::string& fileName) const;
 
