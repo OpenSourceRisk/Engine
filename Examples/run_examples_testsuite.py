31f997a42c5b014f449b0bc2400c347a6822320b
nikno <mail@nikno.de>
2017-09-25 12:10:24 +0100

    removed spurious code from run_examples_testsuite

--- Examples/run_examples_testsuite.py
+++ Examples/run_examples_testsuite.py
@@ -1,6 +1,3 @@
-import os
-
-import shutil
 import numpy as np
 
 from ore_examples_helper import run_example, examples
@@ -10,38 +7,10 @@ output_directory = "Output"
 
 class TestExamples(object):
 
-    def _clear_output_directory(self, example_subdir):
-        example_dir = os.path.join(os.getcwd(), example_subdir)
-        example_output_dir = os.path.join(example_dir, output_directory)
-        try:
-            shutil.rmtree(example_output_dir)
-        except OSError:
-            pass
-
-    def _check_output(self, example_subdir):
-        example_dir = os.path.join(os.getcwd(), example_subdir)
-
-        example_output_dir = os.path.join(example_dir, output_directory)
-        assert os.path.exists(example_output_dir)
-
-        produced_output_files = os.listdir(example_output_dir)
-        assert "npv.csv" in produced_output_files
-        assert "flows.csv" in produced_output_files
-        assert "curves.csv" in produced_output_files
-        assert "xva.csv" in produced_output_files
-        assert "rawcube.csv" in produced_output_files
-        assert "netcube.csv" in produced_output_files
-        assert len([f for f in produced_output_files if "exposure_trade" in f]) > 0
-        assert len([f for f in produced_output_files if "exposure_nettingset" in f]) > 0
-        assert len([f for f in produced_output_files if ".dat" in f]) > 0
-        assert len([f for f in produced_output_files if ".pdf" in f]) > 0
-
     def test_examples(self):
-        for example in examples[:3]:
+        for example in examples[:1]:
             yield self.check_example, example
 
     def check_example(self, example_subdir):
-        #self._clear_output_directory(example_subdir)
         oreex = run_example(example_subdir)
-        #self._check_output(example_subdir)
-        assert np.all(np.array(oreex.return_codes, dtype=int)) == 0
+        assert np.all(np.array(oreex.return_codes, dtype=int) == 0)
