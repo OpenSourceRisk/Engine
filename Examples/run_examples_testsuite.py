d7e3ec2d351c9cb924ab7892bcb3bdab154d67ff
nikno <mail@nikno.de>
2017-09-25 12:18:38 +0100

    added some docstrings for testsuite

--- Examples/run_examples_testsuite.py
+++ Examples/run_examples_testsuite.py
@@ -8,9 +8,21 @@ output_directory = "Output"
 class TestExamples(object):
 
     def test_examples(self):
+        """
+        This test factory produces 1 test for each example in 'examples'
+        by calling 'check_example' with example.
+        """
+
         for example in examples[:1]:
             yield self.check_example, example
 
     def check_example(self, example_subdir):
+        """
+        Runs the example in 'example_subdir', retrieves the exit codes of each call of ore.exe in there and checks
+        that all these exit codes are identical to 0.
+
+        :param example_subdir: A subdirectory of an example to test, for instance 'Example_5'.
+        """
+
         oreex = run_example(example_subdir)
         assert np.all(np.array(oreex.return_codes, dtype=int) == 0)
