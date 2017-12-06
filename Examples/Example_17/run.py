64f99d1b02fcba7355ed17b4f1e883a5a8da51fe
Damien Barker <damien.barker@quaternion.com>
2017-12-06 12:30:49 +0000

    update example 17

--- Examples/Example_17/run.py
+++ Examples/Example_17/run.py
@@ -10,12 +10,14 @@ oreex.print_headline("Run ORE to produce NPV")
 oreex.run("Input/ore.xml")
 oreex.get_times("Output/log.txt")
 
-oreex.print_headline("Run ORE to produce NPV cube and exposures")
-oreex.run("Input/ore_exposure.xml")
-
 oreex.print_headline("Plot results: Simulated exposures")
 
+oreex.setup_plot("CPI Swap")
+oreex.plot("exposure_trade_CPI_Swap_1.csv", 2, 3, 'b', "EPE CPI Swap")
+oreex.decorate_plot(title="Example 17", ylabel="Exposure")
+oreex.save_plot_to_file()
+
 oreex.setup_plot("YoY Swap")
-oreex.plot("exposure_trade_YearOnYear_Swap.csv", 2, 3, 'b', "EPE Forward Swap")
+oreex.plot("exposure_trade_YearOnYear_Swap.csv", 2, 3, 'b', "EPE YoY Swap")
 oreex.decorate_plot(title="Example 17", ylabel="Exposure")
 oreex.save_plot_to_file()
