dbf748b6093660284da6fdbcbeda330f2623ab10
Niall O'Sullivan <niall.osullivan@quaternionrisk.com>
2017-12-11 16:08:12 +0000

    add run.py

--- Examples/Example_20/run.py
+++ Examples/Example_20/run.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
-print("TODO")
+import sys
+sys.path.append('../')
+from ore_examples_helper import OreExample
+
+oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
+
+oreex.print_headline("Run ORE to produce CDS NPV")
+oreex.run("Input/ore.xml")
+oreex.get_times("Output/log.txt")
+
