0da449c17337281b70b717be5e31e222fe842376
francis <francis.duffy@quaternionrisk.com>
2018-03-01 18:15:44 +0000

    Forgot the \file statement

--- OREData/test/ored_commodityforward.hpp
+++ OREData/test/ored_commodityforward.hpp
@@ -16,7 +16,7 @@
  FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
 */
 
-/*! \file test/oredCommodityforward.hpp
+/*! \file test/ored_commodityforward.hpp
     \brief Commodity forward trade and engine builder test
     \ingroup tests
 */
