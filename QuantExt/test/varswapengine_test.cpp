3ca05b1135d0fb9dcee25b5c81655093b63b1290
James Mac Hale <james.machale@quaternion.com>
2018-03-20 18:10:19 +0000

    Added a test case directly targeting the replicating swap engine. Fixed the case targeting the QuantExt pricer.

--- QuantExt/test/varswapengine_test.cpp
+++ QuantExt/test/varswapengine_test.cpp
@@ -15,8 +15,9 @@ contribution to risk analytics and model standardisation, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
 */
+//remove these after debugging
 #include <iostream>
-
+#include <ql/pricingengines/forward/replicatingvarianceswapengine.hpp>
 
 #include "varswapengine_test.hpp"
 #include "utilities.hpp"
@@ -41,6 +42,44 @@ using namespace QuantExt;
 
 using boost::unit_test_framework::test_suite;
 
+namespace {
+
+    struct MCVarianceSwapData {
+        Position::Type type;
+        Real varStrike;
+        Real nominal;
+        Real s;         // spot
+        Rate q;         // dividend
+        Rate r;         // risk-free rate
+        Time t1;        // intermediate time
+        Time t;         // time to maturity
+        Volatility v1;  // volatility at t1
+        Volatility v;   // volatility at t
+        Real result;    // result
+        Real tol;       // tolerance
+    };
+
+    struct ReplicatingVarianceSwapData {
+        Position::Type type;
+        Real varStrike;
+        Real nominal;
+        Real s;         // spot
+        Rate q;         // dividend
+        Rate r;         // risk-free rate
+        Time t;         // time to maturity
+        Volatility v;   // volatility at t
+        Real result;    // result
+        Real tol;       // tolerance
+    };
+
+    struct Datum {
+        Option::Type type;
+        Real strike;
+        Volatility v;
+    };
+
+}
+
 namespace testsuite {
 
 void VarSwapEngineTest::testT0Pricing() {
@@ -48,29 +87,29 @@ void VarSwapEngineTest::testT0Pricing() {
     std::cout << "Running QuantExt VarSwap test." << std::endl;
     const Date today = Date::todaysDate();
     DayCounter dc = Actual365Fixed();
-    Date exDate = today + Integer(365.0 * 0.246575);
+    Date exDate = today + Integer(0.246575 * 365 + 0.5);
     std::vector<Date> dates = { exDate };
 
     std::vector<Real> strikes = { 50.0, 55.0, 60.0, 65.0, 70.0, 75.0, 80.0, 85.0, 90.0, 95.0, 100.0, //Put Strikes
-                                    100.0, 105.0, 110.0, 115.0, 120.0, 125.0, 130.0, 135.0 };   //Call Strikes
+                                    105.0, 110.0, 115.0, 120.0, 125.0, 130.0, 135.0 };   //Call Strikes
     std::vector<Real> volsVector = { 0.3, 0.29, 0.28, 0.27, 0.26, 0.25, 0.24, 0.23, 0.22, 0.21, 0.2,    //Put Vols
-                                        0.2, 0.19, 0.18, 0.17, 0.16, 0.15, 0.14, 0.13 };    //Call Vols
-    Matrix vols(19,1,volsVector.begin(), volsVector.end());
+                                        0.19, 0.18, 0.17, 0.16, 0.15, 0.14, 0.13 };    //Call Vols
+    Matrix vols(18,1,volsVector.begin(), volsVector.end());
 
-    BOOST_TEST_MESSAGE("Testing t0 priocing of the QuantExt VarSwap engine, as per Demeterfi et. al (1999).");
+    BOOST_TEST_MESSAGE("Testing t0 pricing of the QuantExt VarSwap engine, as per Demeterfi et. al (1999).");
     std::string equityName = "STE";
     boost::shared_ptr<SimpleQuote> spot(new SimpleQuote(0.0));
     Handle<Quote> equityPrice = Handle<Quote>(boost::make_shared<SimpleQuote>(100.0));
-    Handle<YieldTermStructure> yieldTS = Handle<YieldTermStructure>(boost::make_shared<FlatForward>(0, NullCalendar(), 0.0, dc));
+    Handle<YieldTermStructure> yieldTS = Handle<YieldTermStructure>(boost::make_shared<FlatForward>(0, NullCalendar(), 0.05, dc));
     Handle<YieldTermStructure> dividendTS = Handle<YieldTermStructure>(boost::make_shared<FlatForward>(0, NullCalendar(), 0.0, dc));
-    Handle<BlackVolTermStructure>& volTS = Handle<BlackVolTermStructure>(boost::make_shared<BlackVarianceSurface>(today, NullCalendar(), dates, strikes, vols, dc));
+    Handle<BlackVolTermStructure> volTS = Handle<BlackVolTermStructure>(boost::make_shared<BlackVarianceSurface>(today, NullCalendar(), dates, strikes, vols, dc));
     Handle<YieldTermStructure> discountingTS = Handle<YieldTermStructure>(boost::make_shared<FlatForward>(0, NullCalendar(), 0.05, dc));
     Size numPuts = 11;
     Size numCalls = 8;
     Real stepSize = 0.05 / sqrt(ActualActual().yearFraction(today, exDate)); //This is % step size between option strikes / Time to Maturity
 
     boost::shared_ptr<GeneralizedBlackScholesProcess> stochProcess(
-        new BlackScholesMertonProcess(equityPrice, dividendTS, yieldTS, volTS));
+        new BlackScholesMertonProcess(equityPrice, dividendTS, discountingTS, volTS));
 
     boost::shared_ptr<PricingEngine> engine(
         new VarSwapEngine(equityName,
@@ -86,16 +125,141 @@ void VarSwapEngineTest::testT0Pricing() {
     VarianceSwap varianceSwap(Position::Long, 0.04, 50000, today, exDate);
     varianceSwap.setPricingEngine(engine);
 
-    Real resVariance = varianceSwap.variance();
-    Real resExpectedValue = 0.04189;
-    Real error = std::fabs(resVariance - resExpectedValue);
+    Real result = varianceSwap.variance();
+    Real expected = 0.041888574;
     Real tol = 1.0e-4;
-    BOOST_CHECK_CLOSE(resVariance, resExpectedValue, tol);
-
+    BOOST_CHECK_CLOSE(result, expected, tol);
 }
+
+void VarSwapEngineTest::testReplicatingVarianceSwap() {
+
+    BOOST_TEST_MESSAGE("Testing variance swap with replicating cost engine...");
+
+    ReplicatingVarianceSwapData values[] = {
+
+        // data from "A Guide to Volatility and Variance Swaps",
+        //   Derman, Kamal & Zou, 1999
+        //   with maturity t corrected from 0.25 to 0.246575
+        //   corresponding to Jan 1, 1999 to Apr 1, 1999
+
+        //type, varStrike, nominal,     s,    q,    r,        t,    v,  result, tol
+        { Position::Long,      0.04,   50000, 100.0, 0.00, 0.05, 0.246575, 0.20, 0.041888574, 1.0e-4 }
+
+    };
+
+    Datum replicatingOptionData[] = {
+
+        // data from "A Guide to Volatility and Variance Swaps",
+        //   Derman, Kamal & Zou, 1999
+
+        //Option::Type, strike, v
+        { Option::Put,   50,  0.30 },
+        { Option::Put,   55,  0.29 },
+        { Option::Put,   60,  0.28 },
+        { Option::Put,   65,  0.27 },
+        { Option::Put,   70,  0.26 },
+        { Option::Put,   75,  0.25 },
+        { Option::Put,   80,  0.24 },
+        { Option::Put,   85,  0.23 },
+        { Option::Put,   90,  0.22 },
+        { Option::Put,   95,  0.21 },
+        { Option::Put,  100,  0.20 },
+        { Option::Call, 100,  0.20 },
+        { Option::Call, 105,  0.19 },
+        { Option::Call, 110,  0.18 },
+        { Option::Call, 115,  0.17 },
+        { Option::Call, 120,  0.16 },
+        { Option::Call, 125,  0.15 },
+        { Option::Call, 130,  0.14 },
+        { Option::Call, 135,  0.13 }
+    };
+
+    DayCounter dc = Actual365Fixed();
+    Date today = Date::todaysDate();
+
+    boost::shared_ptr<SimpleQuote> spot(new SimpleQuote(0.0));
+    boost::shared_ptr<SimpleQuote> qRate(new SimpleQuote(0.0));
+    boost::shared_ptr<YieldTermStructure> qTS = boost::shared_ptr<YieldTermStructure>(new FlatForward(today, Handle<Quote>(qRate), dc));
+    boost::shared_ptr<SimpleQuote> rRate(new SimpleQuote(0.0));
+    boost::shared_ptr<YieldTermStructure> rTS = boost::shared_ptr<YieldTermStructure>(new FlatForward(today, Handle<Quote>(rRate), dc));
+
+    for (Size i = 0; i<LENGTH(values); i++) {
+        Date exDate = today + Integer(values[i].t * 365 + 0.5);
+        std::vector<Date> dates(1);
+        dates[0] = exDate;
+
+        spot->setValue(values[i].s);
+        qRate->setValue(values[i].q);
+        rRate->setValue(values[i].r);
+
+        Size options = LENGTH(replicatingOptionData);
+        std::vector<Real> callStrikes, putStrikes, callVols, putVols;
+
+        // Assumes ascending strikes and same min call and max put strikes
+        Size j;
+        for (j = 0; j<options; j++) {
+            if (replicatingOptionData[j].type == Option::Call) {
+                callStrikes.push_back(replicatingOptionData[j].strike);
+                callVols.push_back(replicatingOptionData[j].v);
+            }
+            else if (replicatingOptionData[j].type == Option::Put) {
+                putStrikes.push_back(replicatingOptionData[j].strike);
+                putVols.push_back(replicatingOptionData[j].v);
+            }
+            else {
+                QL_FAIL("unknown option type");
+            }
+        }
+
+        Matrix vols(options - 1, 1);
+        std::vector<Real> strikes;
+        for (j = 0; j<putVols.size(); j++) {
+            vols[j][0] = putVols[j];
+            strikes.push_back(putStrikes[j]);
+        }
+
+        for (Size k = 1; k<callVols.size(); k++) {
+            Size j = putVols.size() - 1;
+            vols[j + k][0] = callVols[k];
+            strikes.push_back(callStrikes[k]);
+        }
+
+        boost::shared_ptr<BlackVolTermStructure> volTS(new
+            BlackVarianceSurface(today, NullCalendar(),
+                dates, strikes, vols, dc));
+
+        boost::shared_ptr<GeneralizedBlackScholesProcess> stochProcess(
+            new BlackScholesMertonProcess(
+                Handle<Quote>(spot),
+                Handle<YieldTermStructure>(qTS),
+                Handle<YieldTermStructure>(rTS),
+                Handle<BlackVolTermStructure>(volTS)));
+
+
+        boost::shared_ptr<PricingEngine> engine(
+            new ReplicatingVarianceSwapEngine(stochProcess, 5.0,
+                callStrikes,
+                putStrikes));
+
+        VarianceSwap varianceSwap(values[i].type,
+            values[i].varStrike,
+            values[i].nominal,
+            today,
+            exDate);
+        varianceSwap.setPricingEngine(engine);
+
+        Real calculated = varianceSwap.variance();
+        Real expected = values[i].result;
+        Real error = std::fabs(calculated - expected);
+        Real tol = 1.0e-4;
+        BOOST_CHECK_CLOSE(calculated, expected, tol);
+    }
+}
+
 
 test_suite* VarSwapEngineTest::suite() {
     test_suite* suite = BOOST_TEST_SUITE("QuantExt variance swap engine test");
+    suite->add(BOOST_TEST_CASE(&VarSwapEngineTest::testReplicatingVarianceSwap));
     suite->add(BOOST_TEST_CASE(&VarSwapEngineTest::testT0Pricing));
     return suite;
 } 
