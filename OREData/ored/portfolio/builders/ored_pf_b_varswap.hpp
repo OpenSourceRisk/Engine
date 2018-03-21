70f6c06ece03c59c35286564892a5a5d78ee5269
James Mac Hale <james.machale@quaternion.com>
2018-03-21 16:44:13 +0000

    Fixing varswap engine builder and adding references to engine and trade factories.

--- OREData/ored/portfolio/builders/ored_pf_b_varswap.hpp
+++ OREData/ored/portfolio/builders/ored_pf_b_varswap.hpp
@@ -27,14 +27,14 @@
 #include <ored/portfolio/builders/cachingenginebuilder.hpp>
 #include <ored/portfolio/enginefactory.hpp>
 #include <ored/utilities/log.hpp>
-#include <ql/pricingengines/vanilla/analyticeuropeanengine.hpp>
+#include <qle/pricingengines/varswapengine.hpp>
 #include <ql/processes/blackscholesprocess.hpp>
 #include <ql/version.hpp>
 
 namespace ore {
 namespace data {
 
-//! Engine Builder for European Equity Options
+//! Engine Builder for Variance Swaps
 /*! Pricing engines are cached by equity/currency
 
     \ingroup builders
@@ -42,7 +42,7 @@ namespace data {
 class VarSwapEngineBuilder : public CachingPricingEngineBuilder<string, const string&, const Currency&> {
 public:
     VarSwapEngineBuilder()
-        : CachingEngineBuilder("BlackScholesMerton", "ReplicationEngine", {"VarianceSwaps"}) {}
+        : CachingEngineBuilder("BlackScholesMerton", "ReplicatingVarianceSwapEngine", {"VarianceSwap"}) {}
 
 protected:
     virtual string keyImpl(const string& equityName, const Currency& ccy) override {
@@ -50,18 +50,24 @@ protected:
     }
 
     virtual boost::shared_ptr<PricingEngine> engineImpl(const string& equityName, const Currency& ccy) override {
-        string key = keyImpl(equityName, ccy);
-        boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = boost::make_shared<GeneralizedBlackScholesProcess>(
-            market_->equitySpot(equityName, configuration(MarketContext::pricing)),
-            market_->equityDividendCurve(equityName,
-                                         configuration(MarketContext::pricing)), // dividend yield ~ foreign yield
-            market_->equityForecastCurve(equityName, configuration(MarketContext::pricing)),
-            market_->equityVol(equityName, configuration(MarketContext::pricing)));
-        // separate IR curves required for "discounting" and "forward price estimation"
-        Handle<YieldTermStructure> discountCurve =
-            market_->discountCurve(ccy.code(), configuration(MarketContext::pricing));
+        //string key = keyImpl(equityName, ccy);
+        Integer numCalls = parseInteger(engineParameters_.at("NumberOfCalls"));
+        Integer numPuts = parseInteger(engineParameters_.at("NumberOfPuts"));
+        Real moneyStep = parseReal(engineParameters_.at("MoneynessStep"));
+        
+        QL_REQUIRE(numCalls > 0, "Variance swap engine: not enough calls specified. Should be at least one, more is better.");
+        QL_REQUIRE(numPuts > 0, "Variance swap engine: not enough puts specified. Should be at least one, more is better.");
+        QL_REQUIRE(moneyStep > 0, "Variance swap engine: moneyness step is a percentage of strike and should satisfy be greater than 0.");
+        // It is not necessarily required that the step size be less than 1. The step size is scaled with sqrt(T), which can be less than 1.
 
-        return boost::make_shared<QuantLib::AnalyticEuropeanEngine>(gbsp, discountCurve);
+        return boost::make_shared<QuantExt::VarSwapEngine>(
+            equityName, 
+            market_->equitySpot(equityName, configuration(MarketContext::pricing)),
+            market_->equityForecastCurve(equityName, configuration(MarketContext::pricing)),
+            market_->equityDividendCurve(equityName, configuration(MarketContext::pricing)),
+            market_->equityVol(equityName, configuration(MarketContext::pricing)),
+            market_->discountCurve(ccy.code(), configuration(MarketContext::pricing)),
+            numPuts, numCalls, moneyStep);
     }
 };
 
--- OREData/ored/portfolio/enginefactory.cpp
+++ OREData/ored/portfolio/enginefactory.cpp
@@ -101,6 +101,7 @@ void EngineFactory::addDefaultBuilders() {
 
     registerBuilder(boost::make_shared<EquityForwardEngineBuilder>());
     registerBuilder(boost::make_shared<EquityOptionEngineBuilder>());
+    registerBuilder(boost::make_shared<VarSwapEngineBuilder>());
 
     registerBuilder(boost::make_shared<BondDiscountingEngineBuilder>());
 
