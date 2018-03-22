a25cd611b39df1fa91d04d9c88bfba86f40d7f02
chrissmithqrm <christopher.smith@quaternion.com>
2018-03-22 09:24:26 +0000

    Updated Run.py

--- Examples/Example_26/run.py
+++ Examples/Example_26/run.py
@@ -1,3 +1,16 @@
-#!/usr/bin/env python
 
-print("Hello World");
+import sys
+sys.path.append('../')
+from ore_examples_helper import OreExample
+
+oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
+
+oreex.print_headline("Running in currency trades")
+oreex.run("Input/ore.xml")
+
+oreex.print_headline("Running EUR out of currency trades")
+oreex.run("Input/EUR_xois_ore.xml")
+
+oreex.print_headline("Running USD out of currency trades")
+oreex.run("Input/USD_xois_ore.xml")
+
