df533e3641ebe0ead43f56263adfae6e607bb28a
James Mac Hale <james.machale@quaternion.com>
2018-04-06 08:58:55 +0100

    Added handling for future starting varswap. It's a pretty small implementation, but calculation has a lot of overhead in creating 2 new var swaps for pricing.

--- QuantExt/qle/pricingengines/varianceswapgeneralreplicationengine.cpp
+++ QuantExt/qle/pricingengines/varianceswapgeneralreplicationengine.cpp
@@ -72,8 +72,21 @@ void GeneralisedReplicatingVarianceSwapEngine::calculate() const {
     Real variance = 0;
     if (arguments_.startDate > today) {
         // forward starting.
-        QL_FAIL("Cannot price Forward starting variance swap");
-        //variance = calculateFutureVariance();
+        boost::shared_ptr<PricingEngine> thisEngine(this);
+        boost::shared_ptr<VarianceSwap> varSwapTs(new VarianceSwap(arguments_.position, arguments_.strike, arguments_.notional, calendar_.advance(today,1,Days), arguments_.startDate));
+        boost::shared_ptr<VarianceSwap> varSwapTe(new VarianceSwap(arguments_.position, arguments_.strike, arguments_.notional, calendar_.advance(today,1,Days), arguments_.maturityDate)); 
+        varSwapTs->setPricingEngine(thisEngine);
+        varSwapTe->setPricingEngine(thisEngine);
+        
+        Real tsTime = calendar_.businessDaysBetween(today, arguments_.startDate, true, true);
+        Real teTime = calendar_.businessDaysBetween(today, arguments_.maturityDate, false, true);
+        Real fwdTime = calendar_.businessDaysBetween(arguments_.startDate, arguments_.maturityDate, true, true);
+
+        variance = (varSwapTe->variance() * teTime - varSwapTs->variance() * tsTime) / fwdTime;
+
+        results_.additionalResults["accruedVariance"] = 0;
+        results_.additionalResults["futureVariance"] = variance;
+
     }
     else if (arguments_.startDate == today) {
         // The only time the QL price works
