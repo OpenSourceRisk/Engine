5a8ad44c6a789a1332549e7060a541b3977ca346
Niall O'Sullivan <niall.osullivan@quaternionrisk.com>
2018-12-31 16:12:27 +0000

    fix for doxygen (and proof I worked all day)

--- QuantExt/qle/termstructures/blackvariancecurve3.hpp
+++ QuantExt/qle/termstructures/blackvariancecurve3.hpp
@@ -43,13 +43,13 @@ using namespace QuantLib;
 
     \todo check time extrapolation
 
-            \ingroup termstructures
+    \ingroup termstructures
 */
 class BlackVarianceCurve3 : public LazyObject, public BlackVarianceTermStructure {
 public:
+    // If requireMonotoneVariance is true, we check for monotone variance in performCalculations and throw if not
     BlackVarianceCurve3(Natural settlementDays, const Calendar& cal, BusinessDayConvention bdc, const DayCounter& dc,
                         const std::vector<Time>& times, const std::vector<Handle<Quote> >& blackVolCurve,
-                        //! If true, we check for monotone variance in performCalculations and throw if not
                         bool requireMonotoneVariance = true);
     //! \name TermStructure interface
     //@{
