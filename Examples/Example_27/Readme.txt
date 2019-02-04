90764d5c53bd94cf16b3ddc242472756e96f8a47
Sinead Kelleher <sinead.kelleher@quaternionrisk.com>
2019-02-04 11:39:38 +0000

    examples running

--- Examples/Example_27/Readme.txt
+++ Examples/Example_27/Readme.txt
@@ -1,1 +1,19 @@
-This example is intentionally left blank
+1) Portfolio
+
+   Digital CMS Spread Swap
+
+2) Market
+
+   Pseudo market data as of 2016-02-05
+
+3) Pricing
+
+   Single curve
+
+4) Analytics
+
+   NPV, cashflows, sensitiivities
+
+5) Run Example
+
+   python run.py
--- Examples/Example_27/run.py
+++ Examples/Example_27/run.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
-print("Hello World");
+import sys
+sys.path.append('../')
+from ore_examples_helper import OreExample
+
+oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
+
+oreex.print_headline("Run ORE to produce Digital CMS spread sensitivities")
+oreex.run("Input/ore.xml")
+oreex.get_times("Output/log.txt")
+
+
