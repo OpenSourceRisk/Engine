072ddbcfd4182662e7a16dbcd6115015cae03393
Niall O'Sullivan <niall.osullivan@quaternionrisk.com>
2019-02-08 17:44:56 +0000

    more

--- ORETest/oret/basedatapath.hpp
+++ ORETest/oret/basedatapath.hpp
@@ -25,6 +25,7 @@
 #include <ql/errors.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 #include <string>
 
 using boost::filesystem::path;
