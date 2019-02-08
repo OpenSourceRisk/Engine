a4b2cdd4ce729004ea95e9a6e1369d6d6ec77f65
QuaternionRisk <niall.osullivan@quaternionrisk.com>
2019-02-08 17:23:38 +0000

    update for v4

--- QuantExt/test/deposit.cpp
+++ QuantExt/test/deposit.cpp
@@ -30,7 +30,7 @@ using namespace QuantLib;
 using namespace QuantExt;
 using namespace boost::unit_test_framework;
 
-BOOST_FIXTURE_TEST_SUITE(QleDataTestSuite, qle::test::TopLevelFixture)
+BOOST_FIXTURE_TEST_SUITE(QuantExtTestSuite, qle::test::TopLevelFixture)
 
 BOOST_AUTO_TEST_SUITE(DepositTest)
 
