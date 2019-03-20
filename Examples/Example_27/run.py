077b34a9702d89ee97bdf4896f4a2cf027149516
Roland Lichters <roland.lichers@quaternion.com>
2019-03-20 12:24:18 +0100

    consolidate examples, add new examples to user guide

--- Examples/Example_27/run.py
+++ Examples/Example_27/run.py
@@ -6,8 +6,6 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE to produce Digital CMS spread sensitivities")
+oreex.print_headline("Run ORE to produce NPV cube and exposures")
 oreex.run("Input/ore.xml")
 oreex.get_times("Output/log.txt")
-
-
