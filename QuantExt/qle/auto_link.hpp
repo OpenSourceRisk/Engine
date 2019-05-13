155fac4fbd65e42ed5862d94b6a5227d7f2aa561
francis <francis.duffy@quaternionrisk.com>
2019-05-13 14:32:37 +0100

    auto_link.hpp updates as per QuantLib - Avoid using '==' and use '>=' so that you link against the correct library

--- QuantExt/qle/auto_link.hpp
+++ QuantExt/qle/auto_link.hpp
@@ -6,32 +6,20 @@
 #include <boost/config.hpp>
 
 // select toolset:
-#if (_MSC_VER < 1310)
-#error "unsupported Microsoft compiler"
-#elif(_MSC_VER == 1310)
-#define QE_LIB_TOOLSET "vc71"
-#elif(_MSC_VER == 1400)
-#define QE_LIB_TOOLSET "vc80"
-#elif(_MSC_VER == 1500)
-#ifdef x64
-#define QE_LIB_TOOLSET "vc90-x64"
+#if (_MSC_VER >= 1920)
+#  define QE_LIB_TOOLSET "vc142"
+#elif (_MSC_VER >= 1910)
+#  define QE_LIB_TOOLSET "vc141"
+#elif (_MSC_VER >= 1900)
+#  define QE_LIB_TOOLSET "vc140"
+#elif (_MSC_VER >= 1800)
+#  define QE_LIB_TOOLSET "vc120"
+#elif (_MSC_VER >= 1700)
+#  define QE_LIB_TOOLSET "vc110"
+#elif (_MSC_VER >= 1600)
+#  define QE_LIB_TOOLSET "vc100"
 #else
-#define QE_LIB_TOOLSET "vc90"
-#endif
-#elif(_MSC_VER == 1600)
-#define QE_LIB_TOOLSET "vc100"
-#elif(_MSC_VER == 1700)
-#define QE_LIB_TOOLSET "vc110"
-#elif(_MSC_VER == 1800)
-#define QE_LIB_TOOLSET "vc120"
-#elif(_MSC_VER == 1900)
-#define QE_LIB_TOOLSET "vc140"
-#elif (_MSC_VER == 1910)
-#define QE_LIB_TOOLSET "vc141"
-#elif (_MSC_VER == 1920)
-#define QE_LIB_TOOLSET "vc142"
-#else
-#define QE_LIB_TOOLSET "vc142"
+#  error "unsupported Microsoft compiler"
 #endif
 
 #ifdef _M_X64
