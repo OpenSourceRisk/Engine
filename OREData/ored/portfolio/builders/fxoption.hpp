e2fe90a1cc1595ffbd1bbf38c3460129f020aaf1
Alex Chow <alex.chow@quaternion.com>
2019-06-04 18:05:44 +0100

    added Andreasen Huge local vol, not tested

--- OREData/ored/portfolio/builders/fxoption.hpp
+++ OREData/ored/portfolio/builders/fxoption.hpp
@@ -26,14 +26,87 @@
 #include <boost/make_shared.hpp>
 #include <ored/portfolio/builders/cachingenginebuilder.hpp>
 #include <ored/portfolio/enginefactory.hpp>
+#include <ql/math/solvers1d/bisection.hpp>
 #include <ql/methods/finitedifferences/solvers/fdmbackwardsolver.hpp>
 #include <ql/pricingengines/vanilla/analyticeuropeanengine.hpp>
 #include <ql/pricingengines/vanilla/fdblackscholesvanillaengine.hpp>
 #include <ql/pricingengines/vanilla/baroneadesiwhaleyengine.hpp>
 #include <ql/processes/blackscholesprocess.hpp>
+#include <ql/termstructures/volatility/equityfx/andreasenhugelocalvoladapter.hpp>
+#include <ql/termstructures/volatility/equityfx/andreasenhugevolatilityinterpl.hpp>
 
 namespace ore {
 namespace data {
+
+namespace {
+    struct VanillaOptionData {
+        Real strike;
+        Time maturity;
+        Option::Type optionType;
+    };
+    ext::shared_ptr<VanillaOption> makeVanillaOption(const VanillaOptionData& params) {
+        Date maturity = Date(Settings::instance().evaluationDate())
+                                          + Period(Size(params.maturity*365), Days);
+        ext::shared_ptr<Exercise> exercise(new EuropeanExercise(maturity));
+        ext::shared_ptr<StrikedTypePayoff> payoff(
+                    new PlainVanillaPayoff(params.optionType, params.strike));
+        return ext::make_shared<VanillaOption>(payoff, exercise);
+    }
+
+    typedef std::pair<ext::shared_ptr<VanillaOption>, ext::shared_ptr<Quote>> CalibrationDatum;
+    CalibrationDatum makeCalibrationDatum(boost::shared_ptr<PricingEngine> europeanEngine,
+                                          const VanillaOptionData& params) {
+        auto v = makeVanillaOption(params);
+        v->setPricingEngine(europeanEngine);
+        boost::shared_ptr<Quote> quote = boost::make_shared<SimpleQuote>(v->NPV());
+        return { v, quote };
+    }
+
+    class OptionDelta {
+        public:
+            OptionDelta(const boost::shared_ptr<PricingEngine>& engine, Time maturity, Option::Type optionType, Real target)
+            : engine_(engine), maturity_(maturity), optionType_(optionType), target_(target) {};
+            Real operator()(Real x) const {
+                auto v = makeVanillaOption({ x, maturity_, optionType_ });
+                v->setPricingEngine(engine_);
+                return v->delta() - target_;
+            };
+        private:
+            boost::shared_ptr<PricingEngine> engine_;
+            Time maturity_;
+            Option::Type optionType_;
+            Real target_;
+    };
+
+    std::vector<CalibrationDatum> makeCalibrationData(boost::shared_ptr<PricingEngine> engine,
+                                                      const std::vector<Time>& expiries) {
+        QL_REQUIRE(expiries.size() > 0, "expiry list for calibration cannot be empty");
+
+        std::vector<CalibrationDatum> calibrationSet;
+        static std::vector<Option::Type> callOrPuts = { Option::Type::Call, Option::Type::Put };
+
+        for (auto expiry : expiries) {
+            auto v = makeVanillaOption({0, expiry, Option::Type::Call});
+            Real atmStrike = v->NPV();
+
+            static std::vector<Real> deltas = {0.10, 0.25};
+            for (auto type : callOrPuts) {
+                std::vector<Real> strikes = { atmStrike };
+                for (auto delta : deltas) {
+                    OptionDelta deltaFunc(engine, expiry, type, delta * (type == Option::Type::Call ? 1 : -1));
+                    strikes.push_back(Bisection().solve(deltaFunc, 1e-4, atmStrike, 0));
+                }
+                for (auto strike : strikes) {
+                    auto v = makeVanillaOption({strike, expiry, type});
+                    v->setPricingEngine(engine);
+                    boost::shared_ptr<Quote> quote = boost::make_shared<SimpleQuote>(v->NPV());
+                    calibrationSet.push_back({v, quote});
+                }
+            }
+        }
+        return calibrationSet;
+    }
+}
 
 //! Abstract Engine Builder for FX Options
 /*! Pricing engines are cached by currency pair
@@ -50,15 +123,33 @@ protected:
         return forCcy.code() + domCcy.code();
     }
 
-    boost::shared_ptr<GeneralizedBlackScholesProcess> getBlackScholesProcess(const Currency& forCcy, const Currency& domCcy) {
+    boost::shared_ptr<GeneralizedBlackScholesProcess> getBlackScholesProcess(const Currency& forCcy, const Currency& domCcy,
+                                                                             const bool localVol = false,
+                                                                             const std::string& localVolType = "") {
         string pair = keyImpl(forCcy, domCcy);
-        return boost::make_shared<GeneralizedBlackScholesProcess>(
-            market_->fxSpot(pair, configuration(ore::data::MarketContext::pricing)),
-            market_->discountCurve(forCcy.code(),
-                                   configuration(ore::data::MarketContext::pricing)), // dividend yield ~ foreign yield
-            market_->discountCurve(domCcy.code(), configuration(ore::data::MarketContext::pricing)),
-            market_->fxVol(pair, configuration(ore::data::MarketContext::pricing))
-            );
+        const Handle<Quote>& spot = market_->fxSpot(pair, configuration(ore::data::MarketContext::pricing));
+        const Handle<YieldTermStructure>& rTS = market_->discountCurve(
+            domCcy.code(), configuration(ore::data::MarketContext::pricing));
+        const Handle<YieldTermStructure>& qTS = market_->discountCurve(
+            forCcy.code(), configuration(ore::data::MarketContext::pricing));
+        const Handle<BlackVolTermStructure>& blackVolTS = market_->fxVol(
+            pair, configuration(ore::data::MarketContext::pricing));
+
+        if (!localVol || localVolType == "Dupire")
+            return boost::make_shared<GeneralizedBlackScholesProcess>(spot, qTS, rTS, blackVolTS);
+        if (localVolType == "AndreasenHuge") {
+
+            boost::shared_ptr<PricingEngine> europeanEngine =
+                boost::make_shared<AnalyticEuropeanEngine>(getBlackScholesProcess(forCcy, domCcy));
+
+            AndreasenHugeVolatilityInterpl::CalibrationSet calibrationSet = makeCalibrationData(
+                europeanEngine, { 1/12.0, 3/12.0, 6/12.0, 1, 2, 3, 5, 10, 20, 30 });
+
+            auto volInterpl = boost::make_shared<AndreasenHugeVolatilityInterpl>(calibrationSet, spot, rTS, qTS);
+            Handle<LocalVolTermStructure> adaptor(boost::make_shared<AndreasenHugeLocalVolAdapter>(volInterpl));
+            return boost::make_shared<GeneralizedBlackScholesProcess>(spot, qTS, rTS, blackVolTS, adaptor);
+        }
+        QL_FAIL("unknown local volatility type: " << localVolType);
     }
 };
 
@@ -103,15 +194,12 @@ public:
 protected:
 
     virtual boost::shared_ptr<PricingEngine> engineImpl(const Currency& forCcy, const Currency& domCcy) override {
-        boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = getBlackScholesProcess(forCcy, domCcy);
         std::string scheme = engineParameter("Scheme");
         Size tGrid = ore::data::parseInteger(engineParameter("TimeGrid"));
         Size xGrid = ore::data::parseInteger(engineParameter("XGrid"));
         Size dampingSteps = ore::data::parseInteger(engineParameter("DampingSteps"));
         bool localVol = ore::data::parseBool(engineParameter("LocalVol"));
-
-        QL_REQUIRE(!localVol, "local vol is not supported yet");
-        // TODO: add local vol support
+        std::string localVolType = engineParameter("LocalVolType");
 
         static std::map<std::string, FdmSchemeDesc> fdmSchemeMap = {
             {"Hundsdorfer", FdmSchemeDesc::Hundsdorfer()},
@@ -126,13 +214,12 @@ protected:
 
         auto it = fdmSchemeMap.find(scheme);
         QL_REQUIRE(it != fdmSchemeMap.end(), "unknown scheme for finite difference method: " << scheme);
-
+        boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = getBlackScholesProcess(forCcy, domCcy, localVol, localVolType);
         return boost::make_shared<FdBlackScholesVanillaEngine>(gbsp, tGrid, xGrid,
                                                                dampingSteps,
                                                                it->second,
                                                                localVol);
     }
-
 };
 
 //! Engine Builder for FX American Options using Barone Adesi Whaley Approximation
