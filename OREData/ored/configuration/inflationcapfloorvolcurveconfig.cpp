e3a2f3e935d2f0ed7445c8b8f5eac55e31104951
francis <francis.duffy@quaternionrisk.com>
2019-06-20 13:48:41 +0100

    [touch: 741]Not and ibor index so don't need internalIndexName

--- OREData/ored/configuration/inflationcapfloorvolcurveconfig.cpp
+++ OREData/ored/configuration/inflationcapfloorvolcurveconfig.cpp
@@ -47,12 +47,14 @@ InflationCapFloorVolatilityCurveConfig::InflationCapFloorVolatilityCurveConfig(
     const string& index, const string& indexCurve, const string& yieldTermStructure)
     : CurveConfig(curveID, curveDescription), type_(type), volatilityType_(volatilityType), extrapolate_(extrapolate),
       tenors_(tenors), strikes_(strikes), dayCounter_(dayCounter), settleDays_(settleDays), calendar_(calendar),
-      businessDayConvention_(businessDayConvention), index_(internalIndexName(index)), indexCurve_(indexCurve),
+      businessDayConvention_(businessDayConvention), index_(index), indexCurve_(indexCurve),
       yieldTermStructure_(yieldTermStructure) {}
 
 const vector<string>& InflationCapFloorVolatilityCurveConfig::quotes() {
     if (quotes_.size() == 0) {
-        boost::shared_ptr<IborIndex> index = parseIborIndex(index_);
+        QL_REQUIRE(isInflationIndex(index_), "Index '" << index_ << "' for InflationCapFloorVolatilityCurveConfig '" <<
+            curveID_ << "' should be an inflation index");
+        boost::shared_ptr<InflationIndex> index = parseZeroInflationIndex(index_);
         Currency ccy = index->currency();
 
         string type;
@@ -117,7 +119,7 @@ void InflationCapFloorVolatilityCurveConfig::fromXML(XMLNode* node) {
     dayCounter_ = parseDayCounter(XMLUtils::getChildValue(node, "DayCounter", true));
     businessDayConvention_ = parseBusinessDayConvention(XMLUtils::getChildValue(node, "BusinessDayConvention", true));
 
-    index_ = internalIndexName(XMLUtils::getChildValue(node, "Index", true));
+    index_ = XMLUtils::getChildValue(node, "Index", true);
     indexCurve_ = XMLUtils::getChildValue(node, "IndexCurve", true);
     yieldTermStructure_ = XMLUtils::getChildValue(node, "YieldTermStructure", true);
 }
