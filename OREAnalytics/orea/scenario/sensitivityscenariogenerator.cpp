cf7caffaf18c88e18f95342c77e3610dfa626fba
Ruan Richter <ruan.richter@quaternion.com>
2019-06-28 11:15:19 +0100

    fx vol sensi scenario label ATM instead of 1

--- OREAnalytics/orea/scenario/sensitivityscenariogenerator.cpp
+++ OREAnalytics/orea/scenario/sensitivityscenariogenerator.cpp
@@ -2160,7 +2160,7 @@ SensitivityScenarioGenerator::fxVolScenarioDescription(string ccypair, Size expi
     Size index = strikeBucket * data.shiftExpiries.size() + expiryBucket;
     RiskFactorKey key(RiskFactorKey::KeyType::FXVolatility, ccypair, index);
     std::ostringstream o;
-    if (data.shiftStrikes.size() == 0 ) {
+    if (data.shiftStrikes.size() == 0 || close_enough(data.shiftStrikes[strikeBucket], 1) ) { // moneyness
         o << data.shiftExpiries[expiryBucket] << "/ATM";
     } else {
         QL_REQUIRE(strikeBucket < data.shiftStrikes.size(), "strike bucket " << strikeBucket << " out of range");
