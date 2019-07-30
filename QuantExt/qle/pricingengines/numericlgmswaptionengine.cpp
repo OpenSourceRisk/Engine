612590365b53bd98a895e6b11d0db15f2ad1ab05
Peter Caspers <peter.caspers@quaternion.com>
2019-07-30 15:30:54 +0200

    factor out convolution logic so that we can reuse that in other engines

--- QuantExt/qle/pricingengines/numericlgmswaptionengine.cpp
+++ QuantExt/qle/pricingengines/numericlgmswaptionengine.cpp
@@ -25,13 +25,18 @@ using std::vector;
 
 namespace QuantExt {
 
+NumericLgmSwaptionEngineBase::NumericLgmSwaptionEngineBase(const boost::shared_ptr<LinearGaussMarkovModel>& model,
+                                                           const Real sy, const Size ny, const Real sx, const Size nx,
+                                                           const Handle<YieldTermStructure>& discountCurve)
+    : LgmConvolutionSolver(model, sy, ny, sx, nx) {}
+
 Real NumericLgmSwaptionEngineBase::calculate() const {
 
-    iborModelCurve_ = boost::make_shared<LgmImpliedYtsFwdFwdCorrected>(model_, iborIndex_->forwardingTermStructure());
+    iborModelCurve_ = boost::make_shared<LgmImpliedYtsFwdFwdCorrected>(model(), iborIndex_->forwardingTermStructure());
 
     iborIndexCorrected_ = iborIndex_->clone(Handle<YieldTermStructure>(iborModelCurve_));
 
-    Date settlement = model_->parametrization()->termStructure()->referenceDate();
+    Date settlement = model()->parametrization()->termStructure()->referenceDate();
 
     if (exercise_->dates().back() <= settlement) { // swaption is expired, possibly generated swap is not
                                                    // valued
@@ -45,84 +50,32 @@ Real NumericLgmSwaptionEngineBase::calculate() const {
 
     int options = idx - minIdxAlive + 1;
 
-    // x grid for each expiry
+    // terminal payoff
 
-    vector<Real> te(options);       // time to expiry
-    vector<Real> sigma(options);    // standard deviation of x
-    vector<Real> dx(options);       // x-grid spacing
-    Matrix x(options, 2 * mx_ + 1); // x-coordinate of grid points for each expiry
-    Matrix u(options, 2 * mx_ + 1); // conditional underlying value in grid point i
-    Matrix v(options, 2 * mx_ + 1); // conditional continuation value in grid point i
-    for (int j = 0; j < options; j++) {
-        te[j] = model_->parametrization()->termStructure()->timeFromReference(exercise_->dates()[minIdxAlive + j]);
-        sigma[j] = sqrt(model_->parametrization()->zeta(te[j]));
-        dx[j] = sigma[j] / nx_;
-        for (int k = 0; k <= 2 * mx_; k++) {
-            x[j][k] = dx[j] * (k - mx_);
-            // Payoff goes here
-            u[j][k] = conditionalSwapValue(x[j][k], te[j], exercise_->dates()[minIdxAlive + j]);
-            // Continuation value is zero at final expiry.
-            // This will be updated for j < jmax in the rollback loop
-            v[j][k] = std::max(u[j][k], 0.0);
-        }
+    Real t =
+        model()->parametrization()->termStructure()->timeFromReference(exercise_->dates()[minIdxAlive + options - 1]);
+    std::vector<Real> x = stateGrid(t);
+    std::vector<Real> v(x.size());
+    for (Size k = 0; k < x.size(); ++k) {
+        v[k] = std::max(conditionalSwapValue(x[k], t, exercise_->dates()[minIdxAlive + options - 1]), 0.0);
     }
 
     // roll back
 
-    for (int j = options - 1; j >= 0; j--) {
-        if (j == 0) {
-            Real value = 0.0;
-            for (int i = 0; i <= 2 * my_; i++) {
-                // Map y index to x index, not integer in general
-                Real kp = y_[i] * sigma[j] / dx[j] + mx_;
-                // Adjacent integer x index <= k
-                int kk = int(floor(kp));
-                // Get value at kp by linear interpolation on
-                // kk <= kp <= kk + 1 with flat extrapolation
-                Real vp;
-                if (kk < 0)
-                    vp = v[0][0];
-                else if (kk + 1 > 2 * mx_)
-                    vp = v[0][2 * mx_];
-                else
-                    vp = (kp - kk) * v[0][kk + 1] + (1.0 + kk - kp) * v[0][kk];
-
-                value += w_[i] * vp;
-            }
-            return value;
+    for (int j = options - 1; j > 0; --j) {
+        Real t_to =
+            model()->parametrization()->termStructure()->timeFromReference(exercise_->dates()[minIdxAlive + j - 1]);
+        v = rollback(v, t, t_to, 0.0);
+        x = stateGrid(t_to);
+        for (Size k = 0; k < x.size(); ++k) {
+            QL_REQUIRE(v[k] > 0 || close_enough(v[k], 0.0), "negative value in rollback");
+            // choose: continue or exercise
+            v[k] = std::max(v[k], conditionalSwapValue(x[k], t_to, exercise_->dates()[minIdxAlive + j - 1]));
         }
-        // intermediate rollback
-        else {
-            Real std = sqrt(model_->parametrization()->zeta(te[j]) - model_->parametrization()->zeta(te[j - 1]));
-            for (int k = 0; k <= 2 * mx_; k++) {
-                int imin = 0;
-                int imax = 2 * my_;
-                Real value = 0.0;
-                for (int i = imin; i <= imax; i++) {
-                    // Map y index to x index, not integer in general
-                    Real kp = (dx[j - 1] * (k - mx_) + y_[i] * std) / dx[j] + mx_;
-                    // Adjacent integer x index <= k
-                    int kk = int(floor(kp));
-                    // Get value at kp by linear interpolation on
-                    // kk <= kp <= kk + 1 with flat extrapolation
-                    Real vp;
-                    if (kk < 0)
-                        vp = v[j][0];
-                    else if (kk + 1 > 2 * mx_)
-                        vp = v[j][2 * mx_];
-                    else
-                        vp = (kp - kk) * v[j][kk + 1] + (1.0 + kk - kp) * v[j][kk];
-
-                    value += w_[i] * vp;
-                    QL_REQUIRE(vp >= 0, "negative value in rollback");
-                }
-                // choose: continue (value) or exercise (u[j-1][k])
-                v[j - 1][k] = std::max(value, u[j - 1][k]);
-            }
-        }
-    } // for options
-
-    QL_FAIL("This point should never be reached");
+        t = t_to;
+    }
+    v = rollback(v, t, 0.0, 0.0);
+    return v[0];
 
 } // NumericLgmSwaptionEngineBase::calculate
 
@@ -145,17 +98,17 @@ Real NumericLgmSwaptionEngine::conditionalSwapValue(Real x, Real t, const Date e
 
     Real floatingLegNpv = 0.0;
     for (Size l = k1; l < arguments_.floatingCoupons.size(); l++) {
-        Real T = model_->parametrization()->termStructure()->timeFromReference(arguments_.floatingPayDates[l]);
+        Real T = model()->parametrization()->termStructure()->timeFromReference(arguments_.floatingPayDates[l]);
         floatingLegNpv +=
             arguments_.nominal * arguments_.floatingAccrualTimes[l] *
             (arguments_.floatingSpreads[l] + iborIndexCorrected_->fixing(arguments_.floatingFixingDates[l])) *
-            model_->reducedDiscountBond(t, T, x, discountCurve_);
+            model()->reducedDiscountBond(t, T, x, discountCurve_);
     }
 
     Real fixedLegNpv = 0.0;
     for (Size l = j1; l < arguments_.fixedCoupons.size(); l++) {
-        Real T = model_->parametrization()->termStructure()->timeFromReference(arguments_.fixedPayDates[l]);
-        fixedLegNpv += arguments_.fixedCoupons[l] * model_->reducedDiscountBond(t, T, x, discountCurve_);
+        Real T = model()->parametrization()->termStructure()->timeFromReference(arguments_.fixedPayDates[l]);
+        fixedLegNpv += arguments_.fixedCoupons[l] * model()->reducedDiscountBond(t, T, x, discountCurve_);
     }
 
     Option::Type type = arguments_.type == VanillaSwap::Payer ? Option::Call : Option::Put;
@@ -183,22 +136,22 @@ Real NumericLgmNonstandardSwaptionEngine::conditionalSwapValue(Real x, Real t, c
 
     Real floatingLegNpv = 0.0;
     for (Size l = k1; l < arguments_.floatingCoupons.size(); l++) {
-        Real T = model_->parametrization()->termStructure()->timeFromReference(arguments_.floatingPayDates[l]);
+        Real T = model()->parametrization()->termStructure()->timeFromReference(arguments_.floatingPayDates[l]);
         if (arguments_.floatingIsRedemptionFlow[l]) {
-            floatingLegNpv += arguments_.floatingCoupons[l] * model_->reducedDiscountBond(t, T, x, discountCurve_);
+            floatingLegNpv += arguments_.floatingCoupons[l] * model()->reducedDiscountBond(t, T, x, discountCurve_);
         } else {
             floatingLegNpv +=
                 arguments_.floatingNominal[l] * arguments_.floatingAccrualTimes[l] *
                 (arguments_.floatingSpreads[l] +
                  arguments_.floatingGearings[l] * iborIndexCorrected_->fixing(arguments_.floatingFixingDates[l])) *
-                model_->reducedDiscountBond(t, T, x, discountCurve_);
+                model()->reducedDiscountBond(t, T, x, discountCurve_);
         }
     }
 
     Real fixedLegNpv = 0.0;
     for (Size l = j1; l < arguments_.fixedCoupons.size(); l++) {
-        Real T = model_->parametrization()->termStructure()->timeFromReference(arguments_.fixedPayDates[l]);
-        fixedLegNpv += arguments_.fixedCoupons[l] * model_->reducedDiscountBond(t, T, x, discountCurve_);
+        Real T = model()->parametrization()->termStructure()->timeFromReference(arguments_.fixedPayDates[l]);
+        fixedLegNpv += arguments_.fixedCoupons[l] * model()->reducedDiscountBond(t, T, x, discountCurve_);
     }
 
     Option::Type type = arguments_.type == VanillaSwap::Payer ? Option::Call : Option::Put;
