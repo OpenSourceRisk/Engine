a350c6e64b6ecc300435fbdd9eae9802af15349e
Peter Caspers <peter.caspers@quaternion.com>
2019-12-04 14:30:21 +0100

    [touch:947]if expiry is period based we did not choose the right swap index

--- OREData/ored/model/lgmbuilder.cpp
+++ OREData/ored/model/lgmbuilder.cpp
@@ -333,7 +333,7 @@ void LgmBuilder::buildSwaptionBasket() const {
 
         // rounded to whole years, only used to distinguish between short and long
         // swap tenors, which in practice always are multiples of whole years
-        Period termTmp = expiryDateBased ? static_cast<Size>(termT + 0.5) * Years : 0*Days; // FIXME
+        Period termTmp = static_cast<Size>(termT + 0.5) * Years;
         auto iborIndex = termTmp > shortSwapIndex_->tenor() ? swapIndex_->iborIndex() : shortSwapIndex_->iborIndex();
         auto fixedLegTenor =
             termTmp > shortSwapIndex_->tenor() ? swapIndex_->fixedLegTenor() : shortSwapIndex_->fixedLegTenor();
