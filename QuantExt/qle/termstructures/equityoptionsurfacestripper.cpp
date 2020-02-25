26b92db4fc610de8d04d007c883d0f89d0c644e2
Damien Barker <damien.barker@quaternion.com>
2020-02-25 16:20:47 +0000

    equity vol curve

---
+++ QuantExt/qle/termstructures/equityoptionsurfacestripper.cpp
@@ -0,0 +1,214 @@
+/*
+ Copyright (C) 2019 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+#include <qle/pricingengines/baroneadesiwhaleyengine.hpp>
+#include <qle/termstructures/equityoptionsurfacestripper.hpp>
+#include <qle/termstructures/blackvariancesurfacesparse.hpp>
+#include <ql/instruments/impliedvolatility.hpp>
+#include <ql/instruments/vanillaoption.hpp>
+#include <ql/math/solvers1d/brent.hpp>
+#include <ql/pricingengines/vanilla/analyticeuropeanengine.hpp>
+#include <ql/processes/blackscholesprocess.hpp>
+#include <ql/termstructures/volatility/equityfx/blackconstantvol.hpp>
+#include <boost/make_shared.hpp>
+
+using std::vector;
+using std::pair;
+using namespace QuantLib;
+
+namespace {
+
+class PriceError {
+public:
+    PriceError(const VanillaOption& option,
+        SimpleQuote& vol, Real targetValue) : 
+        option_(option), vol_(vol), targetValue_(targetValue) {};
+    Real operator()(Volatility x) const;
+private:
+    const VanillaOption& option_;
+    SimpleQuote& vol_;
+    Real targetValue_;
+};
+
+Real PriceError::operator()(Volatility x) const {
+    vol_.setValue(x);
+    Real npv;
+    // Barone Adesi Whaley fails for very small variance, so wrap in a try/catch
+    try {
+        npv = option_.NPV();
+    } catch (...) {
+        npv = 0.0;
+    }
+    return npv - targetValue_;
+}
+
+}
+
+namespace QuantExt {
+
+EquityOptionSurfaceStripper::EquityOptionSurfaceStripper(
+    const boost::shared_ptr<OptionInterpolatorBase>& callSurface,
+    const boost::shared_ptr<OptionInterpolatorBase>& putSurface,
+    const Handle<EquityIndex>& eqIndex,
+    const Calendar& calendar,
+    const DayCounter& dayCounter,
+    Exercise::Type type,
+    bool lowerStrikeConstExtrap,
+    bool upperStrikeConstExtrap,
+    bool timeFlatExtrapolation) :
+    callSurface_(callSurface), putSurface_(putSurface), eqIndex_(eqIndex), calendar_(calendar), 
+    dayCounter_(dayCounter), type_(type), lowerStrikeConstExtrap_(lowerStrikeConstExtrap), 
+    upperStrikeConstExtrap_(upperStrikeConstExtrap), timeFlatExtrapolation_(timeFlatExtrapolation) {
+
+    // the call and put surfaces should have the same expiries/strikes/reference date/day counters, some checks to ensure this
+    QL_REQUIRE(callSurface_->referenceDate() == putSurface_->referenceDate(), "Mismatch between Call and Put reference dates in EquityOptionPremiumCurveStripper");
+   
+    // register with all market data
+    registerWith(eqIndex);
+    registerWith(Settings::instance().evaluationDate());
+}
+
+void EquityOptionSurfaceStripper::performCalculations() const {
+    // create a set of all dates
+    std::set<Date> allExpiries;
+    for (auto expiry : callSurface_->expiries())
+        allExpiries.insert(expiry);
+    for (auto expiry : putSurface_->expiries())
+        allExpiries.insert(expiry);
+
+    boost::shared_ptr<SimpleQuote> volQuote;
+    boost::shared_ptr<PricingEngine> engine;
+
+    // if the surface is a price surface then we have premiums
+    bool premiumSurfaces = boost::dynamic_pointer_cast<OptionPriceSurface>(callSurface_) != NULL;
+    if (premiumSurfaces) {
+        // first also check the put surface
+        QL_REQUIRE(boost::dynamic_pointer_cast<OptionPriceSurface>(putSurface_) != NULL, 
+            "Call price surface provided, but no put price surface");
+
+        // Set up the engine for implying the vols
+        // term structures needed to get implied vol
+        volQuote = boost::make_shared<SimpleQuote>(0.1);
+        Handle<BlackVolTermStructure> volTs(boost::make_shared<BlackConstantVol>(callSurface_->referenceDate(),
+            calendar_, Handle<Quote>(volQuote), dayCounter_));
+
+        // a black scholes process
+        boost::shared_ptr<GeneralizedBlackScholesProcess> gbsp = boost::make_shared<BlackScholesMertonProcess>(
+            eqIndex_->equitySpot(), eqIndex_->equityDividendCurve(), eqIndex_->equityForecastCurve(), volTs);
+
+        // hard code the engines here
+        // BaroneAdesiWhaley for American options - much faster than alternatives
+        // Black for European
+        if (type_ == Exercise::American) {
+            engine = boost::make_shared<QuantExt::BaroneAdesiWhaleyApproximationEngine>(gbsp);
+        } else if (type_ == Exercise::European) {
+            engine = boost::make_shared<QuantExt::AnalyticEuropeanEngine>(gbsp);
+        } else {
+            QL_FAIL("Unsupported exercise type for option stripping");
+        }
+    }
+
+    vector<Real> volStrikes;
+    vector<Real> volData;
+    vector<Date> volExpiries;
+
+    // loop over each expiry
+    for (auto exp : allExpiries) {
+        // get the forward price at time
+        Real forward = eqIndex_->fixing(exp);
+
+        vector<Real> callStrikes, putStrikes;
+        auto itc = std::find(callSurface_->expiries().begin(), callSurface_->expiries().end(), exp);
+        if (itc != callSurface_->expiries().end()) {
+            auto pos = std::distance(callSurface_->expiries().begin(), itc);
+            callStrikes = callSurface_->strikes().at(pos);
+        }
+        auto itp = std::find(putSurface_->expiries().begin(), putSurface_->expiries().end(), exp);
+        if (itp != putSurface_->expiries().end()) {
+            auto pos = std::distance(putSurface_->expiries().begin(), itp);
+            putStrikes = putSurface_->strikes().at(pos);
+        }
+
+        // We want a set of prices both sides of ATM forward
+        // We take calls where strike < atm, and puts where strike > atm
+
+        bool haveCalls = false, havePuts = false;
+        // check if calls in the correct range
+        if (callStrikes.size() > 0 && callStrikes.front() < forward)
+            haveCalls = true;
+
+        // check if puts in the correct range
+        if (putStrikes.size() > 0 && putStrikes.back() > forward)
+            havePuts = true;
+
+        for (auto cs : callStrikes) {
+            if (!havePuts || cs < forward)
+                volStrikes.push_back(cs);
+            volExpiries.push_back(exp);
+            if (premiumSurfaces) {
+                volData.push_back(implyVol(exp, cs, Option::Call, engine, volQuote));
+            } else {
+                volData.push_back(callSurface_->getValue(exp, cs));
+            }
+        }
+        for (auto ps : putStrikes) {
+            if (!havePuts || ps > forward)
+                volStrikes.push_back(ps);
+            volExpiries.push_back(exp);
+            if (premiumSurfaces) {
+                volData.push_back(implyVol(exp, ps, Option::Put, engine, volQuote));
+            } else {
+                volData.push_back(putSurface_->getValue(exp, ps));
+            }
+        }
+
+        volSurface_ = boost::make_shared<BlackVarianceSurfaceSparse>(callSurface_->referenceDate(), calendar_,
+            volExpiries, volStrikes, volData, dayCounter_, lowerStrikeConstExtrap_, upperStrikeConstExtrap_,
+            timeFlatExtrapolation_);
+    }
+}
+
+Real EquityOptionSurfaceStripper::implyVol(Date expiry, Real strike, Option::Type type,
+    boost::shared_ptr<PricingEngine> engine, boost::shared_ptr<SimpleQuote> volQuote) const {
+
+    // create an american option for current strike/expiry and type
+    boost::shared_ptr<StrikedTypePayoff> payoff(new PlainVanillaPayoff(type, strike));
+    boost::shared_ptr<Exercise> exercise = boost::make_shared<AmericanExercise>(expiry);
+    VanillaOption option(payoff, exercise);
+    option.setPricingEngine(engine);
+
+    // option.setPricingEngine(engine);
+    Real targetPrice = type == Option::Call ? callSurface_->getValue(expiry, strike) :
+        putSurface_->getValue(expiry, strike);
+
+    // calculate the implied volatility using a solver
+    Real vol;
+    try {
+        PriceError f(option, *volQuote, targetPrice);
+        Brent solver;
+        solver.setMaxEvaluations(100);
+        solver.setLowerBound(0.0001);
+        vol = solver.solve(f, 0.0001, 0.2, 0.01);
+    } catch (...) {
+        vol = 0.0;
+    }
+    return vol;
+}
+
+#pragma once
+} // QuantExt
