fe0ad3d9b12a541bfcd961e9495042a0cc909f2d
Peter Caspers <peter.caspers@quaternion.com>
2020-06-03 09:18:50 +0200

    do not require ql instrument in cashflow report

--- OREAnalytics/orea/app/reportwriter.cpp
+++ OREAnalytics/orea/app/reportwriter.cpp
@@ -244,45 +244,50 @@ void ReportWriter::writeCashflow(ore::data::Report& report, boost::shared_ptr<or
             // we assume fixed labels and types for these results, otherwise we don't
             // write out any conditional flows
             auto qlInstr = trades[k]->instrument()->qlInstrument();
-            auto condCfAmounts = qlInstr->additionalResults().find("cashflowAmounts");
-            auto condCfDates = qlInstr->additionalResults().find("cashflowDates");
-            auto condCfCurrencies = qlInstr->additionalResults().find("cashflowCurrencies");
-            if (condCfAmounts != qlInstr->additionalResults().end() &&
-                condCfDates != qlInstr->additionalResults().end() &&
-                condCfCurrencies != qlInstr->additionalResults().end()) {
-                QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>),
-                           "cashflowAmounts type not handled");
-                QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>), "cashflowDates type not handled");
-                QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>),
-                           "cashflowCurrencies type not handled");
-                std::vector<Real> condCfAmountsVec = boost::any_cast<std::vector<Real>>(condCfAmounts->second);
-                std::vector<Date> condCfDatesVec = boost::any_cast<std::vector<Date>>(condCfDates->second);
-                std::vector<string> condCfCurrenciesVec =
-                    boost::any_cast<std::vector<string>>(condCfCurrencies->second);
-                QL_REQUIRE(condCfAmountsVec.size() == condCfDatesVec.size(),
-                           "cashflowAmounts and cashflowDates size mismatch");
-                QL_REQUIRE(condCfAmountsVec.size() == condCfCurrenciesVec.size(),
-                           "cashflowAmounts and cashflowCurrencies size mismatch");
-                for (Size i = 0; i < condCfAmountsVec.size(); ++i) {
-                    Real effectiveAmount = condCfAmountsVec[i] * (condCfAmountsVec[i] == Null<Real>() ? 1.0 : multiplier);
-                    report.next()
-                        .add(trades[k]->id())
-                        .add(trades[k]->tradeType())
-                        .add(i + 1)
-                        .add(i)
-                        .add(condCfDatesVec[i])
-                        .add("")
-                        .add(effectiveAmount)
-                        .add(condCfCurrenciesVec[i])
-                        .add(Null<Real>())
-                        .add(Null<Real>())
-                        .add(Null<Date>())
-                        .add(Null<Real>())
-                        .add(Null<Real>());
-                    if (write_discount_factor) {
-                        Handle<YieldTermStructure> discountCurve = market->discountCurve(condCfCurrenciesVec[i]);
-                        Real discountFactor = discountCurve->discount(condCfDatesVec[i]);
-                        report.add(discountFactor).add(discountFactor * effectiveAmount);
+            // we don't require a ql instrument always
+            if (qlInstr) {
+                auto condCfAmounts = qlInstr->additionalResults().find("cashflowAmounts");
+                auto condCfDates = qlInstr->additionalResults().find("cashflowDates");
+                auto condCfCurrencies = qlInstr->additionalResults().find("cashflowCurrencies");
+                if (condCfAmounts != qlInstr->additionalResults().end() &&
+                    condCfDates != qlInstr->additionalResults().end() &&
+                    condCfCurrencies != qlInstr->additionalResults().end()) {
+                    QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>),
+                               "cashflowAmounts type not handled");
+                    QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>),
+                               "cashflowDates type not handled");
+                    QL_REQUIRE(condCfAmounts->second.type() == typeid(std::vector<Real>),
+                               "cashflowCurrencies type not handled");
+                    std::vector<Real> condCfAmountsVec = boost::any_cast<std::vector<Real>>(condCfAmounts->second);
+                    std::vector<Date> condCfDatesVec = boost::any_cast<std::vector<Date>>(condCfDates->second);
+                    std::vector<string> condCfCurrenciesVec =
+                        boost::any_cast<std::vector<string>>(condCfCurrencies->second);
+                    QL_REQUIRE(condCfAmountsVec.size() == condCfDatesVec.size(),
+                               "cashflowAmounts and cashflowDates size mismatch");
+                    QL_REQUIRE(condCfAmountsVec.size() == condCfCurrenciesVec.size(),
+                               "cashflowAmounts and cashflowCurrencies size mismatch");
+                    for (Size i = 0; i < condCfAmountsVec.size(); ++i) {
+                        Real effectiveAmount =
+                            condCfAmountsVec[i] * (condCfAmountsVec[i] == Null<Real>() ? 1.0 : multiplier);
+                        report.next()
+                            .add(trades[k]->id())
+                            .add(trades[k]->tradeType())
+                            .add(i + 1)
+                            .add(i)
+                            .add(condCfDatesVec[i])
+                            .add("")
+                            .add(effectiveAmount)
+                            .add(condCfCurrenciesVec[i])
+                            .add(Null<Real>())
+                            .add(Null<Real>())
+                            .add(Null<Date>())
+                            .add(Null<Real>())
+                            .add(Null<Real>());
+                        if (write_discount_factor) {
+                            Handle<YieldTermStructure> discountCurve = market->discountCurve(condCfCurrenciesVec[i]);
+                            Real discountFactor = discountCurve->discount(condCfDatesVec[i]);
+                            report.add(discountFactor).add(discountFactor * effectiveAmount);
+                        }
                     }
                 }
             }
