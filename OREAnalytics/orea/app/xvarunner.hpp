153b4a13619ee2f8f94b6621af21fa04d204beda
Damien Barker <damien.barker@quaternion.com>
2020-09-16 09:58:52 +0100

    add extra bumps for fx fwds, ir vegas etc

--- OREAnalytics/orea/app/xvarunner.hpp
+++ OREAnalytics/orea/app/xvarunner.hpp
@@ -34,6 +34,8 @@ namespace analytics {
 
 class XvaRunner {
 public:
+    virtual ~XvaRunner() {};
+
     XvaRunner(QuantLib::Date asof,
         const std::string& baseCurrency,
         const boost::shared_ptr<ore::data::Portfolio>& portfolio,
