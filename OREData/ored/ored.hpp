78e2ebb223435ef09b5bb969c27c0d3b91facd46
Peter Caspers <peter.caspers@quaternion.com>
2020-10-05 10:02:16 +0200

    fix generated header files

--- OREData/ored/ored.hpp
+++ OREData/ored/ored.hpp
@@ -65,6 +65,7 @@
 #include <ored/marketdata/yieldvolcurve.hpp>
 #include <ored/model/calibrationbasket.hpp>
 #include <ored/model/calibrationinstrumentfactory.hpp>
+#include <ored/model/calibrationinstruments/cpicapfloor.hpp>
 #include <ored/model/crossassetmodelbuilder.hpp>
 #include <ored/model/crossassetmodeldata.hpp>
 #include <ored/model/eqbsbuilder.hpp>
