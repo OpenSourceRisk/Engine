6cb3b503c93d61baad3c28478513445ba7e7fb2e
nathaniel-volfango <nvolfango@gmail.com>
2020-12-11 12:31:10 +0000

    Reapply fix currency comparisons 71e8413024

--- OREData/ored/portfolio/fxforward.cpp
+++ OREData/ored/portfolio/fxforward.cpp
@@ -79,24 +79,18 @@ void FxForward::build(const boost::shared_ptr<EngineFactory>& engineFactory) {
 
     QL_REQUIRE(tradeActions().empty(), "TradeActions not supported for FxForward");
 
-    try {
-        DLOG("Build FxForward with maturity date " << QuantLib::io::iso_date(maturityDate) << " and pay date "
-                                                   << QuantLib::io::iso_date(payDate));
+    DLOG("Build FxForward with maturity date " << QuantLib::io::iso_date(maturityDate) << " and pay date "
+                                                << QuantLib::io::iso_date(payDate));
 
-        boost::shared_ptr<QuantLib::Instrument> instrument = boost::make_shared<QuantExt::FxForward>(
-            boughtAmount_, boughtCcy, soldAmount_, soldCcy, maturityDate, false, settlement_ == "Physical", payDate,
-            payCcy, fixingDate, fxIndex);
-        instrument_.reset(new VanillaInstrument(instrument));
+    boost::shared_ptr<QuantLib::Instrument> instrument = boost::make_shared<QuantExt::FxForward>(
+        boughtAmount_, boughtCcy, soldAmount_, soldCcy, maturityDate, false, settlement_ == "Physical", payDate,
+        payCcy, fixingDate, fxIndex);
+    instrument_.reset(new VanillaInstrument(instrument));
 
-        npvCurrency_ = soldCurrency_;
-        notional_ = Null<Real>(); // soldAmount_;
-        notionalCurrency_ = "";   // soldCurrency_;
-        maturity_ = maturityDate;
-
-    } catch (std::exception&) {
-        instrument_.reset();
-        throw;
-    }
+    npvCurrency_ = soldCurrency_;
+    notional_ = Null<Real>(); // soldAmount_;
+    notionalCurrency_ = "";   // soldCurrency_;
+    maturity_ = maturityDate;
 
     // Set up Legs
     legs_ = {{boost::make_shared<SimpleCashFlow>(boughtAmount_, payDate)},
