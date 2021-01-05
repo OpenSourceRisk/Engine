4462ce82e27c7cd8c32870e4e92c023ff36924ff
Peter Caspers <peter.caspers@quaternion.com>
2021-01-05 15:41:49 +0100

    [touch:1412]cmake generated

--- QuantExt/qle/quantext.hpp
+++ QuantExt/qle/quantext.hpp
@@ -305,6 +305,7 @@
 #include <qle/termstructures/probabilitytraits.hpp>
 #include <qle/termstructures/spreadedblackvolatilitycurve.hpp>
 #include <qle/termstructures/spreadedblackvolatilitysurfacemoneyness.hpp>
+#include <qle/termstructures/spreadedblackvolatilitysurfacestddevs.hpp>
 #include <qle/termstructures/spreadeddiscountcurve.hpp>
 #include <qle/termstructures/spreadedoptionletvolatility.hpp>
 #include <qle/termstructures/spreadedoptionletvolatility2.hpp>
