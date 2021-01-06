d693e085ed8696df1148954832ebe04e0ebdce40
Peter Caspers <peter.caspers@quaternion.com>
2021-01-06 10:21:30 +0100

    [touch:1412]add spreaded zero inflation curve

---
+++ QuantExt/qle/termstructures/spreadedinflationcurve.cpp
@@ -0,0 +1,70 @@
+/*
+ Copyright (C) 2020 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+#include <qle/math/interpolation/flatextrapolation.hpp>
+#include <qle/termstructures/spreadedinflationcurve.hpp>
+
+namespace QuantExt {
+
+SpreadedZeroInflationCurve::SpreadedZeroInflationCurve(const Handle<ZeroInflationTermStructure>& referenceCurve,
+                                                       const std::vector<Time>& times,
+                                                       const std::vector<Handle<Quote>>& quotes)
+    : ZeroInflationTermStructure(referenceCurve->dayCounter(), referenceCurve->baseRate(),
+                                 referenceCurve->observationLag(), referenceCurve_->frequency(),
+                                 referenceCurve->indexIsInterpolated(), referenceCurve->nominalTermStructure(),
+                                 referenceCurve->seasonality()),
+      referenceCurve_(referenceCurve), times_(times), quotes_(quotes), data_(times_.size(), 1.0) {
+    QL_REQUIRE(times_.size() > 1, "SpreadedDiscountCurve: at least two times required");
+    QL_REQUIRE(times_.size() == quotes.size(), "SpreadedDiscountCurve: size of time and quote vectors do not match");
+    for (Size i = 0; i < quotes.size(); ++i) {
+        registerWith(quotes_[i]);
+    }
+    interpolation_ = boost::make_shared<FlatExtrapolation>(
+        boost::make_shared<LinearInterpolation>(times_.begin(), times_.end(), data_.begin()));
+    interpolation_->enableExtrapolation();
+    registerWith(referenceCurve_);
+}
+
+Date SpreadedZeroInflationCurve::maxDate() const { return referenceCurve_->maxDate(); }
+
+void SpreadedZeroInflationCurve::update() {
+    LazyObject::update();
+    TermStructure::update();
+}
+
+const Date& SpreadedZeroInflationCurve::referenceDate() const { return referenceCurve_->referenceDate(); }
+
+Calendar SpreadedZeroInflationCurve::calendar() const { return referenceCurve_->calendar(); }
+
+Natural SpreadedZeroInflationCurve::settlementDays() const { return referenceCurve_->settlementDays(); }
+
+void SpreadedZeroInflationCurve::performCalculations() const {
+    for (Size i = 0; i < times_.size(); ++i) {
+        QL_REQUIRE(!quotes_[i].empty(), "SpreadedZeroInflationCurve: quote at index " << i << " is empty");
+        data_[i] = quotes_[i]->value();
+        QL_REQUIRE(data_[i] > 0, "SpreadedZeroInflationCurve: invalid value " << data_[i] << " at index " << i);
+    }
+    interpolation_->update();
+}
+
+Real SpreadedZeroInflationCurve::zeroRateImpl(Time t) const {
+    calculate();
+    referenceCurve_->zeroRate(t) + (*interpolation_)(t);
+}
+
+} // namespace QuantExt
