353f41c22c060804853d6586366c7e14eac73f2d
francis <francis.duffy@acadiasoft.com>
2021-03-14 10:54:11 +0000

    [Touch:1641]Revert commit 372be20402, expect QL Error, update reg results.

--- QuantExt/qle/termstructures/eqcommoptionsurfacestripper.cpp
+++ QuantExt/qle/termstructures/eqcommoptionsurfacestripper.cpp
@@ -255,8 +255,7 @@ Real OptionSurfaceStripper::implyVol(Date expiry, Real strike, Option::Type type
     try {
         PriceError f(option, volQuote, targetPrice);
         vol = solver_(f);
-    } catch (...) {
-        vol = 0.0;
+    } catch (const Error&) {
     }
 
     return vol;
