95cec91af54d085c5e0d73327b4457cb491e8ae8
Peter Caspers <peter.caspers@acadia.inc>
2021-07-17 19:35:29 +0200

    Revert "[touch #1949] Add some FX logic to addMarketFixingDates in order to ensure FX fixings are available for a pair even when the valuation date is not a business date according to the index."
    This reverts commit 10f439f800cc2338e7ada9e33e6493c6e4ad7bdb.

--- OREData/ored/portfolio/fixingdates.cpp
+++ OREData/ored/portfolio/fixingdates.cpp
@@ -659,30 +659,6 @@ void addMarketFixingDates(map<string, set<Date>>& fixings, const TodaysMarketPar
             }
         }
 
-        // If there are FX indexes we need fixings for the last week in order to handle cases where the evaluation date
-        // is a holiday for one of the currencies.
-        if (mktParams.hasMarketObject(MarketObject::FXSpot)) {
-            auto fxLookback = 7 * Days;
-
-            // Dates that will be used for each of the FX indices
-            Date today = Settings::instance().evaluationDate();
-            Date lookback = NullCalendar().advance(today, -fxLookback);
-            set<Date> dates;
-            do {
-                TLOG("Adding date " << io::iso_date(lookback) << " to fixings for any fx indices");
-                dates.insert(lookback);
-                lookback = NullCalendar().advance(lookback, 1 * Days);
-            } while (lookback <= today);
-
-            for (const auto& kv : fixings) {
-                if (isFxIndex(kv.first)) {
-                // For each of the fx indices in market parameters, insert the dates
-                    TLOG("Adding extra fixing dates for fx index " << kv.first);
-                    fixings[kv.first].insert(dates.begin(), dates.end());
-                }
-            }
-        }
-
         LOG("Finished adding market fixing dates for configuration '" << configuration << "'");
     }
 }
