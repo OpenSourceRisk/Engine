6d449caf9830727db5c9769224345a3f9bbee203
Henning Segger <henningsegger@DSA18041HNGSGR.local>
2021-09-01 15:51:33 +0200

    code clean up

--- QuantExt/qle/cashflows/zerofixedcoupon.cpp
+++ QuantExt/qle/cashflows/zerofixedcoupon.cpp
@@ -30,9 +30,8 @@ namespace QuantExt {
                     Coupon(schedule.calendar().adjust(schedule.endDate(), bdc), nominal, schedule.startDate(), schedule.endDate()),
                     nominal_(nominal), fixedRate_(fixedRate), comp_(comp), schedule_(schedule), dc_(dc), subtractNotional_(subtractNotional){
 
-                    QL_REQUIRE(comp_ == QuantLib::Compounded || comp_ == QuantLib::Simple,
-                        "Compounding method " << comp_ << " not supported");
-
+                        QL_REQUIRE(comp_ == QuantLib::Compounded || comp_ == QuantLib::Simple,
+                            "Compounding method " << comp_ << " not supported");
 
                     };
 
@@ -47,9 +46,6 @@ DayCounter ZeroFixedCoupon::dayCounter() const { return dc_; }
 Real ZeroFixedCoupon::accruedAmount(const Date& accrualEnd) const {
 
     Real fixedAmount = nominal_;
-
-
-
     std::vector<Date> dates = schedule_.dates();
 
     // we loop over the dates in the schedule, computing the compound factor.
@@ -59,17 +55,19 @@ Real ZeroFixedCoupon::accruedAmount(const Date& accrualEnd) const {
     // For the Simple rule:
     // (1 + r * dcf_0) * (1 + r * dcf_1)...
 
-    double totalDCF = 0;
-    double compoundFactor = 1;
+    double totalDCF = 0.0;
+    double compoundFactor = 1.0;
     for (Size i = 0; i < dates.size() - 1; i++) {
 
-        if(dates[i] > accrualEnd)
+        Date periodStart = dates[i];
+        if(periodStart > accrualEnd)
             continue;
 
-        double dcf = dc_.yearFraction(dates[i], dates[i + 1]);
+        Date periodEnd = dates[i+1];
+        if(periodEnd > accrualEnd)
+            periodEnd = accrualEnd;
 
-        if(dates[i + 1] > accrualEnd)
-            dcf = dc_.yearFraction(dates[i], accrualEnd);
+        double dcf = dc_.yearFraction(periodStart, periodEnd);
 
         if (comp_ == QuantLib::Simple)
             compoundFactor *= (1 + fixedRate_ * dcf);
