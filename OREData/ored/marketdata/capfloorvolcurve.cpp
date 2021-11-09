7807392223e4e53a977b4026e22387240b184cac
GBFredrik <fredrik.gerdin.borjesson@hotmail.com>
2021-11-09 18:03:23 +0100

    Enable extrapolation of sparse cap/floor vols #90
    Fix for #90.

--- OREData/ored/marketdata/capfloorvolcurve.cpp
+++ OREData/ored/marketdata/capfloorvolcurve.cpp
@@ -640,11 +640,13 @@ CapFloorVolCurve::capSurface(const Date& asof, CapFloorVolatilityCurveConfig& co
     if (optionalQuotes) {
         QL_REQUIRE(quoteCounter > 0, "No Quotes provided for CapFloor surface " << config.curveID());
         if (config.interpolationMethod() == CapFloorTermVolSurfaceExact::Bilinear) {
-            return boost::make_shared<QuantExt::CapFloorTermVolSurfaceSparse<Linear, Linear>>(config.settleDays(), config.calendar(),
-                config.businessDayConvention(), config.dayCounter(), qtTenors, qtStrikes, qtData, false, false);
+            return boost::make_shared<QuantExt::CapFloorTermVolSurfaceSparse<Linear, Linear>>(
+                config.settleDays(), config.calendar(), config.businessDayConvention(), config.dayCounter(), qtTenors,
+                qtStrikes, qtData, true, true);
         } else if (config.interpolationMethod() == CapFloorTermVolSurfaceExact::BicubicSpline) {
-            return boost::make_shared<QuantExt::CapFloorTermVolSurfaceSparse<Cubic, Cubic>>(config.settleDays(), config.calendar(), 
-                config.businessDayConvention(), config.dayCounter(), qtTenors, qtStrikes, qtData, false, false);
+            return boost::make_shared<QuantExt::CapFloorTermVolSurfaceSparse<Cubic, Cubic>>(
+                config.settleDays(), config.calendar(), config.businessDayConvention(), config.dayCounter(), qtTenors,
+                qtStrikes, qtData, true, true);
         } else {
             QL_FAIL("Invalid Interpolation method for capfloor surface " << config.curveID() << ", must be either "
                 << CapFloorTermVolSurfaceExact::Bilinear << " or " << CapFloorTermVolSurfaceExact::BicubicSpline << ".");
