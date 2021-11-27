6cb9c6fc6a7236a20c53f7b71344b75ac131b62b
nathaniel.volfango <nathaniel.volfango@acadia.inc>
2021-11-27 22:47:11 +0000

    QPR-7499, QPR-7504, QPR-7505 -- Add NettingSetDetails class to envelope.cpp/.hpp, and incorporate NettingSetDetails into Envelope class.

--- OREData/ored/portfolio/envelope.cpp
+++ OREData/ored/portfolio/envelope.cpp
@@ -17,15 +17,57 @@
 */
 
 #include <ored/portfolio/envelope.hpp>
+#include <ored/utilities/log.hpp>
 
 using ore::data::XMLUtils;
 
 namespace ore {
 namespace data {
 
+NettingSetDetails::NettingSetDetails(const NettingSetDetails::Key& nettingSetKey) {
+    QL_REQUIRE(fieldNames(true).size() == boost::tuples::length<NettingSetDetails::Key>::value,
+               "NettingSetDetails: Netting set key provided does not match the number of expected fields.");
+
+    nettingSetId_ = boost::get<0>(nettingSetKey);
+    agreementType_ = boost::get<1>(nettingSetKey);
+    callType_ = boost::get<2>(nettingSetKey);
+    initialMarginType_ = boost::get<3>(nettingSetKey);
+    legalEntityId_ = boost::get<4>(nettingSetKey);
+}
+
+NettingSetDetails::NettingSetDetails(const map<string, string>& nettingSetMap) {
+    const vector<string>& supportedFields = fieldNames();
+    
+    for (const pair<string, string>& m : nettingSetMap) {
+        if (m.first == "NettingSetId") {
+            nettingSetId_ = nettingSetMap.at(m.first);
+        } else if (m.first == "AgreementType") {
+            agreementType_ = nettingSetMap.at(m.first);
+        } else if (m.first == "CallType") {
+            agreementType_ = nettingSetMap.at(m.first);
+        } else if (m.first == "InitialMarginType") {
+            initialMarginType_ = nettingSetMap.at(m.first);
+        } else if (m.first == "LegalEntityId") {
+            legalEntityId_ = nettingSetMap.at(m.first);
+        } else {
+            WLOG("NettingSetDetails:: Unsupported field \'" << m.second << "\'");
+        }
+    }
+}
+
 void Envelope::fromXML(XMLNode* node) {
     XMLUtils::checkNode(node, "Envelope");
     counterparty_ = XMLUtils::getChildValue(node, "CounterParty", true);
+
+    XMLNode* nettingSetIdNode = XMLUtils::getChildNode(node, "NettingSetId");
+    XMLNode* nettingSetDetailsNode = XMLUtils::getChildNode(node, "NettingSetDetails");
+    QL_REQUIRE(!(nettingSetIdNode && nettingSetDetailsNode),
+               "Only one of NettingSetId and NettingSetDetails should be provided.");
+    if (nettingSetDetailsNode) {
+        nettingSetDetails_.fromXML(nettingSetDetailsNode);
+    } else {
+        nettingSetDetails_ = NettingSetDetails();
+    }   
     nettingSetId_ = XMLUtils::getChildValue(node, "NettingSetId", false);
 
     portfolioIds_.clear();
@@ -47,7 +89,11 @@ void Envelope::fromXML(XMLNode* node) {
 XMLNode* Envelope::toXML(XMLDocument& doc) {
     XMLNode* node = doc.allocNode("Envelope");
     XMLUtils::addChild(doc, node, "CounterParty", counterparty_);
-    XMLUtils::addChild(doc, node, "NettingSetId", nettingSetId_);
+    if (nettingSetDetails_.empty()) {
+        XMLUtils::addChild(doc, node, "NettingSetId", nettingSetId_);
+    } else {
+        XMLUtils::appendNode(node, nettingSetDetails_.toXML(doc));
+    }
     XMLNode* portfolioNode = doc.allocNode("PortfolioIds");
     XMLUtils::appendNode(node, portfolioNode);
     for (const auto& p : portfolioIds_)
@@ -58,5 +104,63 @@ XMLNode* Envelope::toXML(XMLDocument& doc) {
         XMLUtils::addChild(doc, additionalNode, it.first, it.second);
     return node;
 }
+
+void NettingSetDetails::fromXML(XMLNode* node) {
+    nettingSetId_ = XMLUtils::getChildValue(node, "NettingSetId", true);
+    agreementType_ = XMLUtils::getChildValue(node, "AgreementType", false);
+    callType_ = XMLUtils::getChildValue(node, "CallType", false);
+    initialMarginType_ = XMLUtils::getChildValue(node, "InitialMarginType", false);
+    legalEntityId_ = XMLUtils::getChildValue(node, "LegalEntityId", false);
+}
+
+XMLNode* NettingSetDetails::toXML(XMLDocument& doc) {
+    XMLNode* nettingSetDetailsNode = doc.allocNode("NettingSetDetails");
+    XMLUtils::addChild(doc, nettingSetDetailsNode, "NettingSetId", nettingSetId_);
+    XMLUtils::addChild(doc, nettingSetDetailsNode, "AgreementType", agreementType_);
+    XMLUtils::addChild(doc, nettingSetDetailsNode, "CallType", callType_);
+    XMLUtils::addChild(doc, nettingSetDetailsNode, "InitialMarginType", initialMarginType_);
+    XMLUtils::addChild(doc, nettingSetDetailsNode, "LegalEntityId", legalEntityId_);
+
+    return nettingSetDetailsNode;
+}
+
+const vector<string> NettingSetDetails::fieldNames(bool includeOptionalFields) {
+    vector<string> fieldNames;
+    if (includeOptionalFields)
+        fieldNames = {"NettingSetId", "AgreementType", "CallType", "InitialMarginType", "LegalEntityId"};
+    else
+        fieldNames = {"NettingSetId"};
+
+    return fieldNames;
+}
+
+const vector<string> NettingSetDetails::optionalFieldNames() {
+    return vector<string>({"AgreementType", "CallType", "InitialMarginType", "LegalEntityId"});
+}
+
+const map<string, string> NettingSetDetails::mapRepresentation() const {
+    map<string, string> rep;
+    rep.insert({"NettingSetId", nettingSetId()});
+    rep.insert({"AgreementType", agreementType()});
+    rep.insert({"CallType", callType()});
+    rep.insert({"InitialMarginType", initialMarginType()});
+    rep.insert({"LegalEntityId", legalEntityId()});
+
+    return rep;
+}
+
+std::ostream& operator<<(std::ostream& out, const NettingSetDetails& nettingSetDetails) {
+    return out << "NettingSetId=\'" << nettingSetDetails.nettingSetId() << "\', AgreementType=\'"
+               << nettingSetDetails.agreementType() << "\', CallType=\'" << nettingSetDetails.callType()
+               << "\', InitialMarginType=\'" << nettingSetDetails.initialMarginType() << "\', LegalEntityId=\'"
+               << nettingSetDetails.legalEntityId() << "\'";
+}
+
+bool operator==(const NettingSetDetails& lhs, const NettingSetDetails& rhs) {
+    return (lhs.nettingSetId() == rhs.nettingSetId() && lhs.agreementType() == rhs.agreementType() &&
+            lhs.callType() == rhs.callType() && lhs.initialMarginType() == rhs.initialMarginType() &&
+            lhs.legalEntityId() == rhs.legalEntityId());
+}
+
 } // namespace data
 } // namespace ore
--- OREData/ored/portfolio/envelope.hpp
+++ OREData/ored/portfolio/envelope.hpp
@@ -25,6 +25,8 @@
 
 #include <map>
 #include <ored/utilities/xmlutils.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
 #include <set>
 
 namespace ore {
@@ -35,6 +37,71 @@ using std::map;
 using std::set;
 using std::string;
 
+//! Serializable object holding netting set identification data
+class NettingSetDetails : public XMLSerializable {
+public:
+    typedef boost::tuple<string, string, string, string, string> Key;
+
+    //! Default constructor
+    NettingSetDetails()
+        : nettingSetId_(string()), agreementType_(string()), callType_(string()), initialMarginType_(string()),
+          legalEntityId_(string()){};
+
+    //! Constructor with all fields
+    NettingSetDetails(const string& nettingSetId, const string& agreementType = "", const string& callType = "",
+                      const string& initialMarginType = "", const string& legalEntityId = "")
+        : nettingSetId_(nettingSetId), agreementType_(agreementType), callType_(callType),
+          initialMarginType_(initialMarginType), legalEntityId_(legalEntityId) {}
+
+    //! Constructor to reconstruct NettingSetDetails from from NettingSetDetails::Key
+    NettingSetDetails(const NettingSetDetails::Key& nettingSetKey);
+
+    //! Constructor to reconstruct NettingSetDetails from map (field name to field value)
+    NettingSetDetails(const map<string, string>& nettingSetMap);
+
+    //! \name Serialisation
+    //@{
+    virtual void fromXML(XMLNode* node) override;
+    virtual XMLNode* toXML(XMLDocument& doc) override;
+    //@}
+
+    //! \name Inspectors
+    //@{
+    const string& nettingSetId() const { return nettingSetId_; }
+    const string& agreementType() const { return agreementType_; }
+    const string& callType() const { return callType_; }
+    const string& initialMarginType() const { return initialMarginType_; }
+    const string& legalEntityId() const { return legalEntityId_; }
+    //@}
+
+    //! \name Utility
+    //@{
+    //! Check if the netting set details have been populated
+    bool empty() const { return nettingSetId_.empty(); }
+    bool emptyOptionalFields() const {
+        return agreementType_.empty() && callType_.empty() && initialMarginType_.empty() && legalEntityId_.empty();
+    }
+    //! Returns the object members as a tuple for use as a map key
+    Key key() const {
+        return boost::make_tuple(nettingSetId(), agreementType(), callType(), initialMarginType(), legalEntityId());
+    }
+
+    //! Returns a map representation of the object
+    const map<string, string> mapRepresentation() const;
+
+    //! Returns the XML field names of all the private members
+    static const vector<string> fieldNames(bool includeOptionalFields = true);
+    static const vector<string> optionalFieldNames();
+    //@}
+
+private:
+    string nettingSetId_;
+    string agreementType_;
+    string callType_;
+    string initialMarginType_;
+    string legalEntityId_;
+};
+
 //! Serializable object holding generic trade data, reporting dimensions
 /*!
   \ingroup tradedata
@@ -44,10 +111,12 @@ public:
     //! Default constructor
     Envelope() {}
 
-    //! Constructor with netting set and portfolio ids, without additional fields
+    //! Constructor with netting set details and portfolio ids, without additional fields
     Envelope(const string& counterparty, const string& nettingSetId = "",
+             const NettingSetDetails& nettingSetDetails = NettingSetDetails(),
              const set<string>& portfolioIds = set<string>())
-        : counterparty_(counterparty), nettingSetId_(nettingSetId), portfolioIds_(portfolioIds) {}
+        : counterparty_(counterparty), nettingSetId_(nettingSetId), nettingSetDetails_(nettingSetDetails),
+          portfolioIds_(portfolioIds) {}
 
     // For some reason have a ctor with:
     // map<string, string>& additionalFields = map<string,string>()
@@ -56,13 +125,14 @@ public:
 
     //! Constructor without netting set / portfolio ids, with additional fields
     Envelope(const string& counterparty, const map<string, string>& additionalFields)
-        : counterparty_(counterparty), nettingSetId_(""), additionalFields_(additionalFields) {}
+        : counterparty_(counterparty), nettingSetId_(""), nettingSetDetails_(NettingSetDetails()),
+          additionalFields_(additionalFields) {}
 
     //! Constructor with netting set / portfolio ids, with additional fields
     Envelope(const string& counterparty, const string& nettingSetId, const map<string, string>& additionalFields,
              const set<string>& portfolioIds = set<string>())
-        : counterparty_(counterparty), nettingSetId_(nettingSetId), portfolioIds_(portfolioIds),
-          additionalFields_(additionalFields) {}
+        : counterparty_(counterparty), nettingSetId_(nettingSetId), nettingSetDetails_(NettingSetDetails()),
+          portfolioIds_(portfolioIds), additionalFields_(additionalFields) {}
 
     //! \name Serialisation
     //@{
@@ -73,7 +143,10 @@ public:
     //! \name Inspectors
     //@{
     const string& counterparty() const { return counterparty_; }
-    const string& nettingSetId() const { return nettingSetId_; }
+    const string& nettingSetId() const {
+        return (nettingSetDetails_.empty() ? nettingSetId_ : nettingSetDetails_.nettingSetId());
+    }
+    const NettingSetDetails nettingSetDetails() { return nettingSetDetails_; }
     const set<string>& portfolioIds() const { return portfolioIds_; }
     const map<string, string>& additionalFields() const { return additionalFields_; }
     //@}
@@ -81,14 +154,23 @@ public:
     //! \name Utility
     //@{
     //! Check if the envelope has been populated
-    bool empty() const { return counterparty_ == ""; };
+    bool empty() const { return counterparty_ == ""; }
+    //! Check if the netting set details have been populated
+    bool hasNettingSetDetails() const { return !nettingSetDetails_.empty(); }
     //@}
 
 private:
     string counterparty_;
     string nettingSetId_;
+    NettingSetDetails nettingSetDetails_;
     set<string> portfolioIds_;
     map<string, string> additionalFields_;
 };
+
+//! Enable writing of netting set details
+std::ostream& operator<<(std::ostream& out, const NettingSetDetails& nettingSetDetails);
+
+bool operator==(const NettingSetDetails& lhs, const NettingSetDetails& rhs);
+
 } // namespace data
 } // namespace ore
