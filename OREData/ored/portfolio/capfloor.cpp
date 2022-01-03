17ea9277b58f1e164b3733b00a06668d92d27e20
GBFredrik <fredrik.gerdin.borjesson@hotmail.com>
2022-01-03 16:53:30 +0100

    Refactor CapFloor additionalData to use vectors

--- OREData/ored/portfolio/capfloor.cpp
+++ OREData/ored/portfolio/capfloor.cpp
@@ -416,22 +416,36 @@ const std::map<std::string, boost::any>& CapFloor::additionalData() const {
     additionalData_["legType"] = legData_.legType();
     additionalData_["notionalCurrency"] = legData_.currency();
 
-    size_t i = 0;
+    vector<Real> amounts;
+    vector<Date> paymentDates;
+    vector<Real> currentNotionals;
+    vector<Rate> rates;
+    vector<Date> fixingDates;
+    vector<Rate> indexFixings;
+    vector<Spread> spreads;
+    vector<Rate> caps;
+    vector<Rate> effectiveCaps;
+    vector<Volatility> capletVols;
+    vector<Real> capletPrices;
+    vector<Rate> floors;
+    vector<Rate> effectiveFloors;
+    vector<Volatility> floorletVols;
+    vector<Real> floorletPrices;
+
     for (const auto& flow : legs_[0]) {
         // pick flow with earliest future payment date on this leg
         if (flow->date() > asof) {
-            string numCf = std::to_string(i);
-            additionalData_["amount[" + numCf + "]"] = flow->amount();
-            additionalData_["paymentDate[" + numCf + "]"] = to_string(flow->date());
+            amounts.push_back(flow->amount());
+            paymentDates.push_back(flow->date());
             boost::shared_ptr<Coupon> coupon = boost::dynamic_pointer_cast<Coupon>(flow);
             if (coupon) {
-                additionalData_["currentNotional[" + numCf + "]"] = coupon->nominal();
-                additionalData_["rate[" + numCf + "]"] = coupon->rate();
+                currentNotionals.push_back(coupon->nominal());
+                rates.push_back(coupon->rate());
                 boost::shared_ptr<FloatingRateCoupon> frc = boost::dynamic_pointer_cast<FloatingRateCoupon>(flow);
                 if (frc) {
-                    additionalData_["fixingDate[" + numCf + "]"] = frc->fixingDate();
-                    additionalData_["indexFixing[" + numCf + "]"] = frc->indexFixing();
-                    additionalData_["spread[" + numCf + "]"] = frc->spread();
+                    fixingDates.push_back(frc->fixingDate());
+                    indexFixings.push_back(frc->indexFixing());
+                    spreads.push_back(frc->spread());
 
                     // The below code adds cap/floor levels, vols, and prices
                     // for capped/floored Ibor coupons
@@ -447,28 +461,45 @@ const std::map<std::string, boost::any>& CapFloor::additionalData() const {
                     if (pricer && (cfc->fixingDate() > asof)) {
                         // We write the vols if an Ibor coupon pricer is found and the fixing date is in the future
                         if (cfc->isCapped()) {
-                            additionalData_["cap[" + numCf + "]"] = cfc->cap();
-                            Rate effectiveCap = cfc->effectiveCap();
-                            additionalData_["effectiveCap[" + numCf + "]"] = effectiveCap;
-                            additionalData_["capletVol[" + numCf + "]"] =
-                                pricer->capletVolatility()->volatility(cfc->fixingDate(), effectiveCap);
-                            additionalData_["capletPrice[" + numCf + "]"] =
-                                pricer->capletPrice(effectiveCap) * coupon->nominal();
+                            caps.push_back(cfc->cap());
+                            const Rate effectiveCap = cfc->effectiveCap();
+                            effectiveCaps.push_back(effectiveCap);
+                            capletVols.push_back(
+                                pricer->capletVolatility()->volatility(cfc->fixingDate(), effectiveCap));
+                            capletPrices.push_back(pricer->capletPrice(effectiveCap) * coupon->nominal());
                         }
                         if (cfc->isFloored()) {
-                            additionalData_["floor[" + numCf + "]"] = cfc->floor();
-                            Rate effectiveFloor = cfc->effectiveFloor();
-                            additionalData_["effectiveFloor[" + numCf + "]"] = effectiveFloor;
-                            additionalData_["floorletVol[" + numCf + "]"] =
-                                pricer->capletVolatility()->volatility(cfc->fixingDate(), effectiveFloor);
-                            additionalData_["floorletPrice[" + numCf + "]"] =
-                                pricer->floorletPrice(effectiveFloor) * coupon->nominal();
+                            floors.push_back(cfc->floor());
+                            const Rate effectiveFloor = cfc->effectiveFloor();
+                            effectiveFloors.push_back(effectiveFloor);
+                            floorletVols.push_back(
+                                pricer->capletVolatility()->volatility(cfc->fixingDate(), effectiveFloor));
+                            floorletPrices.push_back(pricer->floorletPrice(effectiveFloor) * coupon->nominal());
                         }
                     }
                 }
             }
-            ++i;
         }
+    }
+
+    additionalData_["amounts"] = amounts;
+    additionalData_["paymentDates"] = paymentDates;
+    additionalData_["currentNotionals"] = currentNotionals;
+    additionalData_["rates"] = rates;
+    additionalData_["fixingDates"] = fixingDates;
+    additionalData_["indexFixings"] = indexFixings;
+    additionalData_["spreads"] = spreads;
+    if (caps.size() > 0) {
+        additionalData_["caps"] = caps;
+        additionalData_["effectiveCaps"] = effectiveCaps;
+        additionalData_["capletVols"] = capletVols;
+        additionalData_["capletPrices"] = capletPrices;
+    }
+    if (floors.size() > 0) {
+        additionalData_["floors"] = floors;
+        additionalData_["effectiveFloors"] = effectiveFloors;
+        additionalData_["floorletVols"] = floorletVols;
+        additionalData_["floorletPrices"] = floorletPrices;
     }
 
     if (legs_[0].size() > 0) {
