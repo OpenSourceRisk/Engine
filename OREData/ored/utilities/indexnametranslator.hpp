23bb9844a7ec63a2cb7e5b6e30079749a0cee768
Peter Caspers <peter.caspers@acadia.inc>
2022-01-11 16:32:55 +0100

    QPR-9717,QPR-9558 add index name translator

---
+++ OREData/ored/utilities/indexnametranslator.hpp
@@ -0,0 +1,59 @@
+/*
+ Copyright (C) 2022 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+/*! \file ored/utilities/indexnametranslator.hpp
+    \brief translates between QuantLib::Index::name() and ORE names
+    \ingroup utilities
+*/
+
+#pragma once
+
+#include <ql/patterns/singleton.hpp>
+
+#include <boost/bimap.hpp>
+
+#include <string>
+
+namespace ore {
+namespace data {
+
+//! IndexNameTranslator
+/*! Allows translating from QuantLib::Index::name() to the ORE name that parses to that index and vice versa. Supported
+    indices are those that were processed by parseIborIndex() before. We can easily extend the index type coverage, if
+    there is a use case.
+*/
+class IndexNameTranslator : public QuantLib::Singleton<IndexNameTranslator> {
+public:
+    //! returns an empty string if qlName is not known
+    std::string oreName(const std::string& qlName) const;
+
+    //! returns an empty string if oreName is not known
+    std::string qlName(const std::string& oreName) const;
+
+    //! adds a pair to the mapping
+    void add(const std::string& qlName, const std::string& oreName);
+
+    //! clears the mapping
+    void clear();
+
+private:
+    boost::bimap<std::string, std::string> data_;
+};
+
+} // namespace data
+} // namespace ore
