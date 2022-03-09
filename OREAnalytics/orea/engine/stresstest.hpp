9fd857bb46d8479d897e280805be2010e1fc3be3
Damien Barker <damien.barker@acadia.inc>
2022-03-09 10:31:38 +0000

    QPR-10640 should be a const ref to market

--- OREAnalytics/orea/engine/stresstest.hpp
+++ OREAnalytics/orea/engine/stresstest.hpp
@@ -55,7 +55,7 @@ namespace analytics {
 class StressTest {
 public:
     //! Constructor
-    StressTest(const boost::shared_ptr<ore::data::Portfolio>& portfolio, boost::shared_ptr<ore::data::Market>& market,
+    StressTest(const boost::shared_ptr<ore::data::Portfolio>& portfolio, const boost::shared_ptr<ore::data::Market>& market,
                const string& marketConfiguration, const boost::shared_ptr<ore::data::EngineData>& engineData,
                boost::shared_ptr<ScenarioSimMarketParameters>& simMarketData,
                const boost::shared_ptr<StressTestScenarioData>& stressData,
