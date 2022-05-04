2de0df7f6da433b928ae039fdae3913b4baad06f
Cian O’Brien <cian.obrien@acadiasoft.com>
2022-05-04 22:07:14 +0100

    QPR-10686 run xjc in python script

--- xsd/compile_java_classes.py
+++ xsd/compile_java_classes.py
@@ -4,10 +4,13 @@ import subprocess
 
 root = "C:\\Users\\cian.obrien\\dev\\openxva\\xsd"
 
-print("Begin compiling classes")
+
 os.chdir(root)
+print("Generate Java class files:")
+subprocess.call("xjc trade.xsd -b ore_types_bindings.xjb")
 os.mkdir(os.getcwd() + "\\generated\\classes")
 
+print("\nBegin compiling classes:")
 for file in os.listdir(os.getcwd() + "\\generated"):
 	print("Now in file: ", file)
 	if file.endswith(".java"):
