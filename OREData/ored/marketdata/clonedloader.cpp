efa7d64dfe234ad7698efb8c9f24a97ababf40cc
Peter Caspers <peter.caspers@acadia.inc>
2022-05-09 16:17:47 +0200

    QPR-10782 add fixings and dividends to cloned loader, simplify implementation

--- OREData/ored/marketdata/clonedloader.cpp
+++ OREData/ored/marketdata/clonedloader.cpp
@@ -3,36 +3,23 @@
   All rights reserved.
 */
 
-#include <orepcapital/ored/marketdata/clonedloader.hpp>
+#include <ored/marketdata/clonedloader.hpp>
 
 using namespace std;
 using namespace ore::data;
 using namespace QuantLib;
 
-namespace oreplus {
+namespace ore {
 namespace data {
 
-ClonedLoader::ClonedLoader(const Date& loaderDate, const boost::shared_ptr<Loader>& inLoader) :
-                           loaderDate_(loaderDate) {
-    // loop over all quotes in the loader for given date and store a copy
-    for (auto md : inLoader->loadQuotes(loaderDate)) {
-        auto tmp = md->clone();
-        data_[md->name()] = tmp;
-        datums_.push_back(tmp);
+ClonedLoader::ClonedLoader(const Date& loaderDate, const boost::shared_ptr<Loader>& inLoader)
+    : loaderDate_(loaderDate) {
+    for (const auto& md : inLoader->loadQuotes(loaderDate)) {
+        data_[loaderDate].push_back(md->clone());
     }
-}
-
-const std::vector<boost::shared_ptr<MarketDatum>>& ClonedLoader::loadQuotes(const Date& d) const {
-    QL_REQUIRE(d == loaderDate_, "Date " << d << " does not match loader date " << loaderDate_);
-    return datums_;
-}
-
-const boost::shared_ptr<MarketDatum>& ClonedLoader::get(const string& name, const Date& d) const {
-    QL_REQUIRE(d == loaderDate_, "Date " << d << " does not match loader date " << loaderDate_);
-    auto it = data_.find(name);
-    QL_REQUIRE(it != data_.end(), "No datum for " << name << " on date " << d);
-    return it->second;
+    fixings_ = inLoader->loadFixings();
+    dividends_ = inLoader->loadDividends();
 }
 
 } // namespace data
-} // namespace oreplus
+} // namespace ore
