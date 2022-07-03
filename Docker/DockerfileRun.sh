c53e930094aa2db2c1104bda2ba25a73dd68e95a
Peter Caspers <peter.caspers@acadia.inc>
2022-07-03 11:52:01 +0200

    remove unneeded files from github

--- Docker/DockerfileRun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose-boost.yml build --no-cache
-docker-compose -f docker-compose-quantlib.yml build --no-cache
-docker-compose -f docker-compose-ore.yml build --no-cache
--- Docs/UserGuide/latexcommand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export TEXINPUTS=.:$ORE/Docs/UserGuide:$ORE/DocsPlus::
-#pdflatex --file-line-error --synctex=1 --shell-escape $1
-pdflatex --shell-escape $1
