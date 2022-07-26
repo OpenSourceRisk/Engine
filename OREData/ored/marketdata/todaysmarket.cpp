989dba6596f87a5044f38a3b6312d2385b0c6121
Damien Barker <damien.barker@acadia.inc>
2022-07-26 11:41:31 +0100

    QPR-11078 change to DLOG

--- OREData/ored/marketdata/todaysmarket.cpp
+++ OREData/ored/marketdata/todaysmarket.cpp
@@ -332,7 +332,7 @@ void TodaysMarket::buildNode(const std::string& configuration, Node& node) const
                 itr = requiredFxSpots_.insert(make_pair(fxspec->name(), fxSpot)).first;
                 fxT_.addQuote(fxspec->subName().substr(0, 3) + fxspec->subName().substr(4, 3), itr->second->handle()->fxQuote(true));
             }
-            LOG("Adding FXIndex (" << node.name << ") with spec " << *fxspec << " to configuration " << configuration);
+            DLOG("Adding FXIndex (" << node.name << ") with spec " << *fxspec << " to configuration " << configuration);
             // add the market spot rate and the rate today, both are needed
             fxIndices_[configuration].addIndex(node.name, itr->second->handle());
             break;
