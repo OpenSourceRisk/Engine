844b9feb1fd22c0cce3358672beec428b6f32f19
Damien Barker <damien.barker@acadia.inc>
2022-07-27 13:25:13 +0100

    QPR-10037 allow all periods for trs and bondTRS

--- OREData/ored/portfolio/durationadjustedcmslegbuilder.cpp
+++ OREData/ored/portfolio/durationadjustedcmslegbuilder.cpp
@@ -56,13 +56,14 @@ Leg DurationAdjustedCmsLegBuilder::buildLeg(const LegData& data, const boost::sh
 
     applyAmortization(notionals, data, schedule, false);
 
+    PaymentLag paymentLag = data.paymentLag();
     DurationAdjustedCmsLeg leg = DurationAdjustedCmsLeg(schedule, index, cmsData->duration())
                                      .withNotionals(notionals)
                                      .withSpreads(spreads)
                                      .withGearings(gearings)
                                      .withPaymentDayCounter(dc)
                                      .withPaymentAdjustment(bdc)
-                                     .withPaymentLag(data.paymentLag())
+                                     .withPaymentLag(boost::apply_visitor(PaymentLagInteger(), paymentLag))
                                      .withFixingDays(fixingDays)
                                      .inArrears(cmsData->isInArrears());
 
