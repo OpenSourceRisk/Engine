de5de7f2cecf0591b7225675c036ac4f7335ad9c
Peter Caspers <peter.caspers@acadia.inc>
2022-11-09 15:00:46 +0100

    QPR-11318 update example 37

--- Examples/Example_37/run.py
+++ Examples/Example_37/run.py
@@ -4,21 +4,32 @@ import sys
 sys.path.append('../')
 from ore_examples_helper import OreExample
 
+import pca
+import os
+
+# undo the overwrite during the automatic testing
+os.environ.pop('OVERWRITE_SCENARIOGENERATOR_SAMPLES', None)
+        
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE to produce NPV cube and exposures")
+oreex.print_headline("Run ORE to produce scenariodump.csv")
 oreex.run("Input/ore.xml")
 oreex.get_times("Output/log.txt")
 
-oreex.print_headline("Run ORE again to price European Swaptions")
-oreex.run("Input/ore_swaption.xml")
+oreex.print_headline("Run python script to perform PCA on scenariodump data")
 
-oreex.print_headline("Plot results: Simulated exposures vs analytical swaption prices")
+pca.run_pca()
 
-oreex.setup_plot("swaptions")
-oreex.plot("exposure_trade_Swap_20y.csv", 2, 3, 'b', "Swap EPE")
-oreex.plot("exposure_trade_Swap_20y.csv", 2, 4, 'r', "Swap ENE")
-oreex.plot_npv("swaption_npv.csv", 6, 'g', "NPV Swaptions", marker='s')
-oreex.decorate_plot(title="Example 1 - Simulated exposures vs analytical swaption prices")
+oreex.print_headline("Plot results: Eigenvectors")
+
+oreex.setup_plot("Eigenvectors")
+oreex.plot("eigenvectors.csv", 0, 1, 'b', "Model Eigenvector 1")
+oreex.plot("eigenvectors.csv", 0, 2, 'r', "Model Eigenvector 2")
+oreex.plot("../Input/inputeigenvectors.csv", 0, 1, 'g', "Input Eigenvector 1")
+oreex.plot("../Input/inputeigenvectors.csv", 0, 2, 'y', "Input Eigenvector 2")
+# oreex.plot("exposure_trade_Swap_20y.csv", 2, 4, 'r', "Swap ENE")
+# # oreex.plot_npv("swaption_npv.csv", 6, 'g', "NPV Swaptions", marker='s')
+oreex.decorate_plot(title="Example 37 - Model implied principal components vs. input principal components", ylabel="shift",
+                    y_format_as_int = False, display_grid = True, legend_loc = "best")
 oreex.save_plot_to_file()
 
