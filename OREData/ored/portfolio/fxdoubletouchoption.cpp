584c1701f253d74940efc66c38f71d7ad2edff6c
nathaniel.volfango <nathaniel.volfango@acadia.inc>
2022-11-30 01:03:04 +0000

    QPR-10992 -- Add settlement data for FxDoubleTouchOption

--- OREData/ored/portfolio/fxdoubletouchoption.cpp
+++ OREData/ored/portfolio/fxdoubletouchoption.cpp
@@ -20,6 +20,7 @@
 #include <ored/portfolio/barrieroptionwrapper.hpp>
 #include <ored/portfolio/fxdoubletouchoption.hpp>
 #include <ored/portfolio/enginefactory.hpp>
+#include <ored/portfolio/structuredtradewarning.hpp>
 #include <ored/utilities/indexparser.hpp>
 #include <ored/utilities/log.hpp>
 #include <ored/utilities/marketdata.hpp>
@@ -83,6 +84,26 @@ void FxDoubleTouchOption::build(const boost::shared_ptr<EngineFactory>& engineFa
         DLOG("Payoff at hit not yet supported for FxDoubleTouchOptions, setting to payoff at expiry");
     }
 
+    Natural payLag = 0;
+    BusinessDayConvention payConvention = Unadjusted;
+    Calendar payCalendar = NullCalendar();
+    Date payDate = expiryDate;
+    const boost::optional<OptionPaymentData>& opd = option_.paymentData();
+    if (opd) {
+        if (opd->rulesBased()) {
+            payLag = opd->lag();
+            payConvention = opd->convention();
+            payCalendar = opd->calendar();
+            payDate = payCalendar.advance(expiryDate, opd->lag(), Days, opd->convention());
+        } else {
+            if (opd->dates().size() > 1)
+                WLOG(ore::data::StructuredTradeWarningMessage(
+                    id(), tradeType(), "Trade build", "Found more than 1 payment date. The first one will be used."));
+            payDate = opd->dates().front();
+        }
+    }
+    QL_REQUIRE(payDate >= expiryDate, "Settlement date cannot be earlier than expiry date");
+
     Real levelLow = barrier_.levels()[0].value();
     Real levelHigh = barrier_.levels()[1].value();
     QL_REQUIRE(levelLow < levelHigh, "barrier levels are not in ascending order");
@@ -109,7 +130,7 @@ void FxDoubleTouchOption::build(const boost::shared_ptr<EngineFactory>& engineFa
     boost::shared_ptr<StrikedTypePayoff> payoff(new CashOrNothingPayoff(Option::Call, (levelLow + levelHigh) / 2, 1.0));
     Leg leg;
 
-    leg.push_back(boost::shared_ptr<CashFlow>(new SimpleCashFlow(1.0, expiryDate)));
+    leg.push_back(boost::shared_ptr<CashFlow>(new SimpleCashFlow(1.0, payDate)));
 
     boost::shared_ptr<Exercise> exercise = boost::make_shared<EuropeanExercise>(expiryDate);
 
@@ -127,7 +148,7 @@ void FxDoubleTouchOption::build(const boost::shared_ptr<EngineFactory>& engineFa
     QL_REQUIRE(builder, "No builder found for " << tradeType_);
     boost::shared_ptr<FxDoubleTouchOptionEngineBuilder> fxDoubleTouchOptBuilder =
         boost::dynamic_pointer_cast<FxDoubleTouchOptionEngineBuilder>(builder);
-    doubleTouch->setPricingEngine(fxDoubleTouchOptBuilder->engine(fgnCcy, domCcy, flipResults));
+    doubleTouch->setPricingEngine(fxDoubleTouchOptBuilder->engine(fgnCcy, domCcy, payDate, flipResults));
 
     // if a knock-in option is triggered it becomes a simple forward cashflow
     // which we price as a swap
@@ -152,12 +173,12 @@ void FxDoubleTouchOption::build(const boost::shared_ptr<EngineFactory>& engineFa
     npvCurrency_ = payoffCurrency_;
     notional_ = payoffAmount_;
     notionalCurrency_ = payoffCurrency_;
-    maturity_ = std::max(lastPremiumDate, expiryDate);
+    maturity_ = std::max(lastPremiumDate, payDate);
 
     Calendar fixingCal = fxIndex ? fxIndex->fixingCalendar() : cal;
     if (start != Date()) {
         for (Date d = start; d <= expiryDate; d = fixingCal.advance(d, 1 * Days))
-            requiredFixings_.addFixingDate(d, fxIndex_, expiryDate);
+            requiredFixings_.addFixingDate(d, fxIndex_, payDate);
     }
 
     additionalData_["payoffAmount"] = payoffAmount_;
