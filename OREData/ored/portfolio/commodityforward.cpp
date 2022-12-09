c684cb98b37f366f37e156759bfbdb434c7bb4ed
Eric Mandolesi <eric.mandolesi@acadia.inc>
2022-12-09 10:45:02 +0000

    Revert "Merge remote-tracking branch 'origin/master' into QPR-11244_ter"
    This reverts commit 6c1b92c84e97f639ea9ee60fdc5928ae443c42d5, reversing
    changes made to 6d8c355b19a5de99a0f3abc1c32e12fea0edd232.

--- OREData/ored/portfolio/commodityforward.cpp
+++ OREData/ored/portfolio/commodityforward.cpp
@@ -116,7 +116,7 @@ void CommodityForward::build(const boost::shared_ptr<EngineFactory>& engineFacto
     }
 
     // add required commodity fixing
-    requiredFixings_.addFixingDate(maturity_, index->name(), paymentDate == Date() ? maturity_ : paymentDate);
+    requiredFixings_.addFixingDate(maturity_, index->name(), paymentDate);
 
     // Create the commodity forward instrument
     Currency currency = parseCurrency(currency_);
