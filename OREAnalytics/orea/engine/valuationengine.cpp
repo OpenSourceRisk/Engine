6ca3cd102012253c7d31e0f38d4f1b3e527428bb
henning <henning.segger@acadia.inc>
2023-01-10 19:57:44 +0100

    QPR-11360 bugfix upon error all results to be zero

--- OREAnalytics/orea/engine/valuationengine.cpp
+++ OREAnalytics/orea/engine/valuationengine.cpp
@@ -278,12 +278,13 @@ void ValuationEngine::buildCube(const boost::shared_ptr<data::Portfolio>& portfo
             ALOG("setting all results in output cube to zero for trade '"
                  << trades[i]->id() << "' since there was at least one error during simulation");
             for (Size index = 0; index < outputCube->depth(); ++index) {
-                outputCube->setT0(0.0, i, index);
+                outputCube->setT0(1.0, i, index); //set interim value, such that it interacts with scenario set below (set method compare t0 and scenario values, zeroes are dangerous)
                 for (Size dateIndex = 0; dateIndex < outputCube->numDates(); ++dateIndex) {
                     for (Size sample = 0; sample < outputCube->samples(); ++sample) {
                         outputCube->set(0.0, i, dateIndex, sample, index);
                     }
                 }
+                outputCube->setT0(0.0, i, index); //set the value finally to zero
             }
         }
     }
