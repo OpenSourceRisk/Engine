c62a329626785b67dda25e80e12d8546c03a51af
Peter Caspers <peter.caspers@acadia.inc>
2023-03-09 12:31:58 +0100

    QPR-11546 refactor index cds option engine to allow for several extensions

---
+++ QuantExt/qle/pricingengines/indexcdsoptiobasenengine.cpp
@@ -0,0 +1,134 @@
+/*
+ Copyright (C) 2021 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+#include <qle/pricingengines/indexcdsoptionbaseengine.hpp>
+
+#include <ql/exercise.hpp>
+#include <ql/pricingengines/blackformula.hpp>
+#include <ql/pricingengines/credit/isdacdsengine.hpp>
+#include <ql/pricingengines/credit/midpointcdsengine.hpp>
+#include <ql/termstructures/credit/flathazardrate.hpp>
+#include <ql/termstructures/yield/flatforward.hpp>
+#include <ql/time/daycounters/actual360.hpp>
+#include <qle/utilities/time.hpp>
+
+#include <numeric>
+
+using namespace QuantLib;
+using std::string;
+using std::vector;
+
+namespace QuantExt {
+
+BlackIndexCdsOptionEngine::BlackIndexCdsOptionEngine(const Handle<DefaultProbabilityTermStructure>& probability,
+                                                     Real recovery, const Handle<YieldTermStructure>& discount,
+                                                     const Handle<QuantExt::CreditVolCurve>& volatility)
+    : probabilities_({probability}), recoveries_({recovery}), discount_(discount), volatility_(volatility),
+      indexRecovery_(recovery) {
+    registerWithMarket();
+}
+
+BlackIndexCdsOptionEngine::BlackIndexCdsOptionEngine(
+    const vector<Handle<DefaultProbabilityTermStructure>>& probabilities, const vector<Real>& recoveries,
+    const Handle<YieldTermStructure>& discount, const Handle<QuantExt::CreditVolCurve>& volatility, Real indexRecovery)
+    : probabilities_(probabilities), recoveries_(recoveries), discount_(discount), volatility_(volatility),
+      indexRecovery_(indexRecovery) {
+
+    QL_REQUIRE(!probabilities_.empty(), "BlackIndexCdsOptionEngine: need at least one probability curve.");
+    QL_REQUIRE(probabilities_.size() == recoveries_.size(), "BlackIndexCdsOptionEngine: mismatch between size"
+                                                                << " of probabilities (" << probabilities_.size()
+                                                                << ") and recoveries (" << recoveries_.size() << ").");
+
+    registerWithMarket();
+
+    // If the index recovery is not populated, use the average recovery.
+    if (indexRecovery_ == Null<Real>())
+        indexRecovery_ = accumulate(recoveries_.begin(), recoveries_.end(), 0.0) / recoveries_.size();
+}
+
+const vector<Handle<DefaultProbabilityTermStructure>>& BlackIndexCdsOptionEngine::probabilities() const {
+    return probabilities_;
+}
+
+const vector<Real>& BlackIndexCdsOptionEngine::recoveries() const { return recoveries_; }
+
+const Handle<YieldTermStructure> BlackIndexCdsOptionEngine::discount() const { return discount_; }
+
+const Handle<QuantExt::CreditVolCurve> BlackIndexCdsOptionEngine::volatility() const { return volatility_; }
+
+void BlackIndexCdsOptionEngine::calculate() const {
+
+    // Underlying index CDS
+    const auto& cds = *arguments_.swap;
+
+    // If given constituent curves, store constituent notionals. Otherwise, store top level notional.
+    if (probabilities_.size() > 1) {
+        notionals_ = cds.underlyingNotionals();
+        QL_REQUIRE(probabilities_.size() == notionals_.size(), "BlackIndexCdsOptionEngine: mismatch between size"
+                                                                   << " of probabilities (" << probabilities_.size()
+                                                                   << ") and notionals (" << notionals_.size() << ").");
+    } else {
+        notionals_ = {cds.notional()};
+    }
+
+    // Get additional results of underlying index CDS.
+    cds.NPV();
+    results_.additionalResults = cds.additionalResults();
+
+    // Calculate option value depending on strike type.
+    if (arguments_.strikeType == CdsOption::Spread)
+        spreadStrikeCalculate(fep());
+    else
+        priceStrikeCalculate(fep());
+}
+
+Real IndexCdsOptionBaseEngine::fep() const {
+
+    // Exercise date
+    const Date& exerciseDate = arguments_.exercise->dates().front();
+
+    // Realised FEP
+    results_.additionalResults["realisedFEP"] = arguments_.realisedFep;
+
+    // Unrealised FEP
+    Real fep = 0.0;
+    for (Size i = 0; i < probabilities_.size(); ++i) {
+        fep += (1 - recoveries_[i]) * probabilities_[i]->defaultProbability(exerciseDate) * notionals_[i];
+    }
+    results_.additionalResults["UnrealisedFEP"] = fep;
+
+    // Total FEP
+    fep += arguments_.realisedFep;
+    results_.additionalResults["FEP"] = fep;
+
+    // Discounted FEP
+    Real discount = discount_->discount(exerciseDate);
+    fep *= discount;
+    results_.additionalResults["discountedFEP"] = fep;
+
+    return fep;
+}
+
+void IndexCdsOptionBaseEngine::registerWithMarket() {
+    for (const auto& p : probabilities_)
+        registerWith(p);
+    registerWith(discount_);
+    registerWith(volatility_);
+}
+
+} // namespace QuantExt
