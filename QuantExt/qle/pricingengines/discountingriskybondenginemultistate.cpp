a262dfd1b88b692d5c02ab3d638673972f5b7e16
Peter Caspers <peter.caspers@acadia.inc>
2023-04-06 16:08:25 +0200

    QPR-8566 add engine builders

--- QuantExt/qle/pricingengines/discountingriskybondenginemultistate.cpp
+++ QuantExt/qle/pricingengines/discountingriskybondenginemultistate.cpp
@@ -32,9 +32,9 @@ DiscountingRiskyBondEngineMultiState::DiscountingRiskyBondEngineMultiState(
     const std::vector<Handle<DefaultProbabilityTermStructure>>& defaultCurves,
     const std::vector<Handle<Quote>>& recoveryRates, const Size mainResultState, const Handle<Quote>& securitySpread,
     Period timestepPeriod, boost::optional<bool> includeSettlementDateFlows)
-    : DiscountingRiskyBondEngineMultiState(discountCurve, Handle<DefaultProbabilityTermStructure>(), Handle<Quote>(),
-                                           securitySpread, timestepPeriod, includeSettlementDateFlows),
-      defaultCurves_(defaultCurves), mainResultState_(mainResultState), recoveryRates_(recoveryRates) {
+    : DiscountingRiskyBondEngine(discountCurve, Handle<DefaultProbabilityTermStructure>(), Handle<Quote>(),
+                                 securitySpread, timestepPeriod, includeSettlementDateFlows),
+      defaultCurves_(defaultCurves), recoveryRates_(recoveryRates), mainResultState_(mainResultState) {
     QL_REQUIRE(!defaultCurves.empty(), "DiscountingRiskBondEngineMultiState: no default curves / recovery rates given");
     for (auto const& h : defaultCurves_) {
         registerWith(h);
