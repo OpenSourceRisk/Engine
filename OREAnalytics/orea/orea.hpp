ea8eb29a6dcf7bb1c6ab70ae30bc00f9a7c592e1
Peter Caspers <peter.caspers@acadia.inc>
2023-04-13 17:23:20 +0200

    QPR-8566 add multi-state npv calculator

--- OREAnalytics/orea/orea.hpp
+++ OREAnalytics/orea/orea.hpp
@@ -51,6 +51,7 @@
 #include <orea/engine/historicalpnlgenerator.hpp>
 #include <orea/engine/historicalsensipnlcalculator.hpp>
 #include <orea/engine/mporcalculator.hpp>
+#include <orea/engine/multistatenpvcalculator.hpp>
 #include <orea/engine/multithreadedvaluationengine.hpp>
 #include <orea/engine/npvrecord.hpp>
 #include <orea/engine/observationmode.hpp>
