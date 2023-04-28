e91ef5a8bb38fbed04c7c056f70eb1dd5362ebb9
Roland Lichters <roland.lichters@acadia.inc>
2023-04-28 12:06:56 +0200

    update example 43, bond/CDS

--- Examples/Example_43/run.py
+++ Examples/Example_43/run.py
@@ -6,6 +6,22 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
+oreex.print_headline("Run ORE for Bond and CDS")
+orexml="Input/ore4.xml"
+oreex.run(orexml)
+case="bond_cds"
+targetFileName = "credit_migration_" + case + "_11.csv"
+oreex.setup_plot("cdf_" + case)
+oreex.plot(targetFileName, 0, 2, 'b', label="Loss")
+oreex.decorate_plot(title="Example 43 - Loss distribution", ylabel="CDF", xlabel="Loss", y_format_as_int=False)
+oreex.save_plot_to_file()
+oreex.setup_plot("pdf_" + case)
+oreex.plot(targetFileName, 0, 1, 'b', label="Loss")
+oreex.decorate_plot(title="Example 43 - Loss distribution", ylabel="PDF", xlabel="Loss", y_format_as_int=False)
+oreex.save_plot_to_file()
+
+sys.exit("Done")
+
 oreex.print_headline("Run ORE for a single Bond")
 orexml="Input/ore.xml"
 oreex.run(orexml)
@@ -62,19 +78,6 @@ oreex.plot(targetFileName, 0, 1, 'b', label="Loss")
 oreex.decorate_plot(title="Example 43 - Loss distribution", ylabel="PDF", xlabel="Loss", y_format_as_int=False)
 oreex.save_plot_to_file()
 
-oreex.print_headline("Run ORE for Bond and CDS")
-orexml="Input/ore4.xml"
-oreex.run(orexml)
-case="bond_cds"
-targetFileName = "credit_migration_" + case + "_11.csv"
-oreex.setup_plot("cdf_" + case)
-oreex.plot(targetFileName, 0, 2, 'b', label="Loss")
-oreex.decorate_plot(title="Example 43 - Loss distribution", ylabel="CDF", xlabel="Loss", y_format_as_int=False)
-oreex.save_plot_to_file()
-oreex.setup_plot("pdf_" + case)
-oreex.plot(targetFileName, 0, 1, 'b', label="Loss")
-oreex.decorate_plot(title="Example 43 - Loss distribution", ylabel="PDF", xlabel="Loss", y_format_as_int=False)
-oreex.save_plot_to_file()
 
 #oreex.print_headline("Run ORE for 100 Bonds")
 #orexml="Input/ore100.xml"
