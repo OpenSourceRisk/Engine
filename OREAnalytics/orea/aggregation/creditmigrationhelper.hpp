a0f8d4bcbef52dcc10bf3a0901fcf51f0eae6502
Peter Caspers <peter.caspers@acadia.inc>
2023-05-09 11:57:52 +0200

    QPR-11706 clean up

--- OREAnalytics/orea/aggregation/creditmigrationhelper.hpp
+++ OREAnalytics/orea/aggregation/creditmigrationhelper.hpp
@@ -47,10 +47,7 @@ namespace analytics {
      - n correlated global factors \f$ G_j \f$
      - entity specific factor loadings \f$ \beta_{ij} \f$
      - idiosyncratic part \f$ dZ_i = \sigma_i dW_i \f$
-     - independent  Wiener processes W, i.e. \f$ dW_k dW_l = 0 \f$ and \f$ dW_k dG_j = 0 \f$
-
-   \warning Evaluation modes ForwardSimulationA and ForwardSimulationB are untested and need review
-*/
+     - independent  Wiener processes W, i.e. \f$ dW_k dW_l = 0 \f$ and \f$ dW_k dG_j = 0 \f$ */
 class CreditMigrationHelper {
 public:
     enum class CreditMode { Migration, Default };
@@ -68,71 +65,44 @@ public:
     void build(const std::map<std::string, boost::shared_ptr<Trade>>& trades);
 
     const std::vector<Real>& upperBucketBound() const { return bucketing_.upperBucketBound(); }
-    // 
+    //
     Array pnlDistribution(const Size date);
 
-    //! Correlation of factor1/factor2 moves from today to date
-    void stateCorrelations(const Size date, Size entity1, Size entity2, string index1, Real initialIndexValue1,
-                           string index2, Real initialIndexValue2);
-
 private:
-    /*! Get the transition matrix from today to date by entity, 
-      sanitise the annual transition matrix input, 
-      rescale to the desired horizon/date using the generator, 
-      cache the result so that we do the sanitising/rescaling only once
-    */
+    /*! Get the transition matrix from today to date by entity,
+      sanitise the annual transition matrix input,
+      rescale to the desired horizon/date using the generator,
+      cache the result so that we do the sanitising/rescaling only once */
     std::map<string, Matrix> rescaledTransitionMatrices(const Size date);
 
-    /*! Initialise 
+    /*! Initialise
       - the variance of the global part Y_i of entity state X_i, for all entities
       - the global part Y_i of entity i's state X_i by date index, entity index and sample number
-        using the simulated global state paths stored in the aggregation scenario data object
-     */
+        using the simulated global state paths stored in the aggregation scenario data object */
     void init();
 
     //! Allocate 3d storage for the simulated idiosyncratic factors by entity, date and sample
     void initEntityStateSimulation();
 
-    /*!
-      Initialise the entity state simulationn for a given date and global path and return transition
-      matrices by simulation date along the path and by entity 
-      Case Evaluation::TerminalSimulation:
-        - return transition matrix for each entity for one date (the terminal date) only, 
-          conditional on the global terminal state on the given path     
-      Case Evaluation::ForwardSimulationB:
-        - return forward transition matrices M(t_{j-1},t_j) for each cube date and each entity 
-          conditional on the terminal date, across all cube dates on the given path
-          such that the product of these matrices along the path matches the terminal 
-          conditional transition matrix
-        - M(t_{j-1},t_j) = M^{-1}(0, t_{j-1}) * M(0, t_j)
-      Case Evaluation::ForwardSimulationA:
-        - return forward transition matrices M_i(t_{j-1},t_j) where each of the underlying
-          transition matrices M_i(0,t_j) is conditional on the global state at t_j 
-     */
-    std::vector<std::vector<Matrix>> initEntityStateSimulation(const Size date, const Size path);
+    /*! Initialise the entity state simulationn for a given date for
+        Evaluation = TerminalSimulation:
+        Return transition matrix for each entity for the given date,
+        conditional on the global terminal state on the given path */
+    std::vector<Matrix> initEntityStateSimulation(const Size date, const Size path);
 
-    /*! 
-      Generate one entity state sample path for all entities given the global state path
-      and given the forward transition matrices
-    */
-    void simulateEntityStates(const std::vector<std::vector<Matrix>>& cond, const Size path,
-                              const MersenneTwisterUniformRng& mt, const Size date);
+    /*! Generate one entity state sample path for all entities given the global state path
+        and given the conditional transition matrices for all entities at the terminal date. */
+    void simulateEntityStates(const std::vector<Matrix>& cond, const Size path, const MersenneTwisterUniformRng& mt);
 
     //! Look up the simulated entity credit state for the given entity, date and path
-    Size simulatedEntityState(const Size i, const Size date, const Size path) const;
+    Size simulatedEntityState(const Size i, const Size path) const;
 
-    //! Return true if both A and B default on the given path and A defaults before B
-    bool simulatedDefaultOrder(const Size entityA, const Size entityB, const Size date, const Size path,
-                               const Size n) const;
-
-    /*! Return a single PnL impact due to credit migration or default of Bond/CDS issuers and default of 
-      netting set counterparties on the given global path 
-    */
+    /*! Return a single PnL impact due to credit migration or default of Bond/CDS issuers and default of
+      netting set counterparties on the given global path */
     Real generateMigrationPnl(const Size date, const Size path, const Size n) const;
 
     /*! Return a vector of PnL impacts and associated conditional probabilities for the specified global path,
-      due to credit migration or default of Bond/CDS issuers and default of netting set counterparties
-    */
+      due to credit migration or default of Bond/CDS issuers and default of netting set counterparties */
     void generateConditionalMigrationPnl(const Size date, const Size path, const std::map<string, Matrix>& transMat,
                                          std::vector<Array>& condProbs, std::vector<Array>& pnl) const;
 
@@ -161,10 +131,10 @@ private:
     // Transition matrix rows
     Size n_;
     std::vector<std::map<string, Matrix>> rescaledTransitionMatrices_;
-    // Variance of the systemic part (Y_i) of entity state X_i 
+    // Variance of the systemic part (Y_i) of entity state X_i
     std::vector<Real> globalVar_;
-    // Storage for the simulated idiosyncratic factors Z by entity, date, sample number
-    std::vector<std::vector<std::vector<Size>>> simulatedEntityState_;
+    // Storage for the simulated idiosyncratic factors Z by entity, sample number
+    std::vector<std::vector<Size>> simulatedEntityState_;
 
     std::vector<std::vector<Matrix>> entityStateSimulationMatrices_;
     // Systemic part (Y_i) of entity state X_i by date index, entity index, sample number
