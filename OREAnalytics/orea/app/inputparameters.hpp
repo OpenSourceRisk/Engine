e616fa2361ee6e1bf5ffb872d083069dd2cc60b7
Peter Caspers <peter.caspers@acadia.inc>
2023-05-17 10:54:08 +0200

    QPR-11719 remove analytic fx sensi "hack"

--- OREAnalytics/orea/app/inputparameters.hpp
+++ OREAnalytics/orea/app/inputparameters.hpp
@@ -107,7 +107,6 @@ public:
 
     // Setters for sensi analytics
     void setXbsParConversion(bool b) { xbsParConversion_ = b; }
-    void setAnalyticFxSensis(bool b) { analyticFxSensis_ = b; }
     void setParSensi(bool b) { parSensi_ = b; }
     void setAlignPillars(bool b) { alignPillars_ = b; }
     void setOutputJacobi(bool b) { outputJacobi_ = b; }
@@ -315,7 +314,6 @@ public:
      * Getters for sensi analytics
      *****************************/
     bool xbsParConversion() { return xbsParConversion_; }
-    bool analyticFxSensis() { return analyticFxSensis_; }
     bool parSensi() const { return parSensi_; };
     bool alignPillars() const { return alignPillars_; };
     bool outputJacobi() const { return outputJacobi_; };
@@ -520,7 +518,6 @@ protected:
      * SENSITIVITY analytic
      **********************/
     bool xbsParConversion_ = false;
-    bool analyticFxSensis_ = true;
     bool parSensi_ = false;
     bool outputJacobi_ = false;
     bool alignPillars_ = false;
