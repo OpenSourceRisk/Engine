f0f4a2654b01ec122ab3eee56299b22f605c1696
henning <henning.segger@acadia.inc>
2023-06-05 11:22:48 +0200

    QPR-11764 move CBO QLE and ORED classes

---
+++ OREData/ored/portfolio/bondbasket.hpp
@@ -0,0 +1,75 @@
+/*
+ Copyright (C) 2020 Quaternion Risk Management Ltd
+ All rights reserved.
+*/
+
+/*! \file portfolio/bondbasket.hpp
+    \brief credit bond basket data model and serialization
+    \ingroup portfolio
+*/
+
+#pragma once
+
+#include <qle/instruments/bondbasket.hpp>
+
+#include <ored/portfolio/bond.hpp>
+#include <ored/portfolio/enginefactory.hpp>
+#include <ored/portfolio/schedule.hpp>
+#include <ored/portfolio/trade.hpp>
+#include <ored/utilities/parsers.hpp>
+
+#include <vector>
+
+namespace oreplus {
+namespace data {
+using namespace QuantLib;
+using namespace ore::data;
+using ore::data::XMLSerializable;
+using std::string;
+using std::vector;
+
+//! Serializable Bond-Basket Data
+/*!
+  \ingroup tradedata
+*/
+
+class BondBasket : public XMLSerializable {
+public:
+    //! Default constructor
+    BondBasket() {}
+
+    //! \name Serialisation
+    //@{
+    virtual void fromXML(XMLNode* node) override;
+    virtual XMLNode* toXML(ore::data::XMLDocument& doc) override;
+
+    //! Add underlying Bond names
+    std::map<AssetClass, std::set<std::string>>
+        underlyingIndices(const boost::shared_ptr<ReferenceDataManager>& referenceDataManager = nullptr) const;
+
+    bool empty() { return bonds_.empty(); }
+    void clear();
+
+    boost::shared_ptr<QuantExt::BondBasket> build(const boost::shared_ptr<EngineFactory>& engineFactory,
+                                                  const QuantLib::Currency& ccy,
+                                                  const std::string& reinvestmentEndDate);
+    //@}
+
+    const std::vector<boost::shared_ptr<ore::data::Bond>>& bonds() const { return bonds_; }
+
+    const RequiredFixings& requiredFixings() const { return requiredFixings_; }
+private:
+
+    bool isFeeFlow(const ext::shared_ptr<QuantLib::CashFlow>& cf, const std::string& name);
+    void setReinvestmentScalar();
+
+    vector<boost::shared_ptr<ore::data::Bond>> bonds_;
+    std::map <string, boost::shared_ptr<QuantExt::FxIndex>> fxIndexMap_;
+    RequiredFixings requiredFixings_;
+    QuantLib::Date reinvestment_;
+    std::map<std::string, std::vector<double> > reinvestmentScalar_;
+    std::map<std::string, std::vector<std::string> > flowType_;
+
+};
+} // namespace data
+} // namespace oreplus
---
+++ OREData/ored/portfolio/builders/cbo.hpp
@@ -0,0 +1,30 @@
+/*
+ Copyright (C) 2020 Quaternion Risk Management Ltd
+ All rights reserved.
+*/
+
+/*! \file portfolio/builders/cbo.hpp
+\brief
+\ingroup portfolio
+*/
+
+#pragma once
+
+#include <ored/portfolio/enginefactory.hpp>
+#include <ql/experimental/credit/pool.hpp>
+
+namespace oreplus {
+namespace data {
+
+using namespace ore::data;
+
+class CboMCEngineBuilder : public EngineBuilder {
+public:
+    //! constructor that builds a usual pricing engine
+    CboMCEngineBuilder() : EngineBuilder("OneFactorCopula", "MonteCarloCBOEngine", {"CBO"}) {};
+
+    boost::shared_ptr<QuantLib::PricingEngine> engine(const boost::shared_ptr<QuantLib::Pool>& pool);
+};
+
+} // namespace data
+} // namespace oreplus
