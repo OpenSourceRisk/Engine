985b2eee611f3c028406d1eb61b2a61152e46514
Matthias Groncki <matthias.groncki@acadia.inc>
2023-07-10 15:42:30 +0200

    QPR-11749 bugfix

--- QuantExt/qle/cashflows/blackovernightindexedcouponpricer.cpp
+++ QuantExt/qle/cashflows/blackovernightindexedcouponpricer.cpp
@@ -156,9 +156,8 @@ Real BlackOvernightIndexedCouponPricer::optionletRateLocal(Option::Type optionTy
     if (i < n && fixingDates[std::min(i, nCutoff)] == today) {
         // might have been fixed
         try {
-            Rate pastFixing =
-                index->fixing(fixingDates[std::min(i, nCutoff)]);
-            if (pastFixing != Null<Real>()) {
+            if (index->hasHistoricalFixing(today)){
+                Rate pastFixing = index->fixing(fixingDates[std::min(i, nCutoff)]);
                 if (coupon_->underlying()->includeSpread()) {
                     pastFixing += coupon_->spread();
                 }
