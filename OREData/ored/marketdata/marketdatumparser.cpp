f14a7a20c1a96a8f7cadda98febc3e713cc15b5b
Roland Lichters <roland.lichters@acadia.inc>
2023-09-28 09:46:58 +0200

    QPR-12056 add expected results, cleanup

--- OREData/ored/marketdata/marketdatumparser.cpp
+++ OREData/ored/marketdata/marketdatumparser.cpp
@@ -318,14 +318,23 @@ boost::shared_ptr<MarketDatum> parseMarketDatum(const Date& asof, const string& 
             offset = 1;
         }
         Period tenor = parsePeriod(tokens[4 + offset]);
-        try {
-            Period fwdStart = parsePeriod(tokens[3 + offset]);
-            Period term = parsePeriod(tokens[5 + offset]);
+        boost::variant<QuantLib::Date, QuantLib::Period> start = parseDateOrPeriod(tokens[3 + offset]);
+        boost::variant<QuantLib::Date, QuantLib::Period> end = parseDateOrPeriod(tokens[5 + offset]);
+
+        if (start.type() == typeid(QuantLib::Period) &&
+            end.type() == typeid(QuantLib::Period)) {
+            Period fwdStart = boost::get<QuantLib::Period>(start);
+            Period term = boost::get<QuantLib::Period>(end);
             return boost::make_shared<SwapQuote>(value, asof, datumName, quoteType, ccy, fwdStart, term, tenor, indexName);
-        } catch(...) {
-            Date startDate = parseDate(tokens[3 + offset]);
-            Date maturityDate = parseDate(tokens[5 + offset]);
+        }
+        else if (start.type() == typeid(QuantLib::Date) &&
+                 end.type() == typeid(QuantLib::Date)) {
+            Date startDate = boost::get<QuantLib::Date>(start);
+            Date maturityDate = boost::get<QuantLib::Date>(end);
             return boost::make_shared<SwapQuote>(value, asof, datumName, quoteType, ccy, startDate, maturityDate, tenor, indexName);
+        }
+        else {
+            QL_FAIL("Expect swap quote with start/end as either periods or dates");
         }
     }
 
