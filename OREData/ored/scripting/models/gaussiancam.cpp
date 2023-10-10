23c6bb81942371602316f6056f887f5f86fcb877
Peter Caspers <peter.caspers@acadia.inc>
2023-10-10 12:18:14 +0200

    QPR-12093 fix underlying paths training, invalidate cache when switching paths

--- OREData/ored/scripting/models/gaussiancam.cpp
+++ OREData/ored/scripting/models/gaussiancam.cpp
@@ -155,11 +155,17 @@ void GaussianCam::performCalculations() const {
         irStates_[d] = std::vector<RandomVariable>(currencies_.size(), RandomVariable(size(), 0.0));
         infStates_[d] = std::vector<std::pair<RandomVariable, RandomVariable>>(
             infIndices_.size(), std::make_pair(RandomVariable(size(), 0.0), RandomVariable(size(), 0.0)));
+
         if(trainingSamples() != Null<Size>() && injectedPathTimes_ == nullptr) {
-            irStatesTraining_[d] = std::vector<RandomVariable>(currencies_.size(), RandomVariable(size(), 0.0));
+            underlyingPathsTraining_[d] =
+                std::vector<RandomVariable>(indices_.size(), RandomVariable(trainingSamples(), 0.0));
+            irStatesTraining_[d] =
+                std::vector<RandomVariable>(currencies_.size(), RandomVariable(trainingSamples(), 0.0));
             infStatesTraining_[d] = std::vector<std::pair<RandomVariable, RandomVariable>>(
-                infIndices_.size(), std::make_pair(RandomVariable(size(), 0.0), RandomVariable(size(), 0.0)));
+                infIndices_.size(),
+                std::make_pair(RandomVariable(trainingSamples(), 0.0), RandomVariable(trainingSamples(), 0.0)));
         }
+
     }
 
     // populate index mappings
@@ -629,6 +635,7 @@ void GaussianCam::toggleTrainingPaths() const {
     std::swap(irStates_, irStatesTraining_);
     std::swap(infStates_, infStatesTraining_);
     inTrainingPhase_ = !inTrainingPhase_;
+    irIndexValueCache_.clear();
 }
 
 Size GaussianCam::trainingSamples() const { return mcParams_.trainingSamples; }
