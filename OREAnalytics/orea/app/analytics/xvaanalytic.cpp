3f31e6e87ea851b3e2e1be3b8f06499eb6f9824e
Peter Caspers <peter.caspers@acadia.inc>
2023-11-16 13:06:32 +0100

    QPR-12028 fix parameters

--- OREAnalytics/orea/app/analytics/xvaanalytic.cpp
+++ OREAnalytics/orea/app/analytics/xvaanalytic.cpp
@@ -589,7 +589,7 @@ void XvaAnalyticImpl::runAnalytic(const boost::shared_ptr<ore::data::InMemoryLoa
         LOG("XVA analytic is running with amc cg engine (experimental).");
         // note: market configs both set to simulation, see note in xvaenginecg, we'd need inccy config in sim market there...
         XvaEngineCG engine(
-            inputs_->nThreads(), inputs_->asof(), analytic()->loader(), inputs_->curveConfigs().get(),
+            inputs_->nThreads(), inputs_->asof(), loader, inputs_->curveConfigs().get(),
             analytic()->configurations().todaysMarketParams, analytic()->configurations().simMarketParams,
             inputs_->amcPricingEngine(), inputs_->crossAssetModelData(), inputs_->scenarioGeneratorData(),
             inputs_->portfolio(), inputs_->marketConfig("simulation"), inputs_->marketConfig("simulation"),
