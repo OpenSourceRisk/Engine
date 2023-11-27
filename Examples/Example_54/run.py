5b352b1c0f9fa64d1a564bc08e8731784325c3a6
Peter Caspers <peter.caspers@acadia.inc>
2023-11-27 09:22:59 +0100

    QPR-12028 expected results for example 54

--- Examples/Example_54/run.py
+++ Examples/Example_54/run.py
@@ -10,10 +10,3 @@ oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
 oreex.print_headline("Run ORE to produce AMC CG exposure")
 oreex.run("Input/ore.xml")
-
-oreex.print_headline("Plot results: Simulated exposure")
-
-oreex.setup_plot("amc_vanillaswap_eur")
-oreex.plot("exposure_trade_Swap_EUR.csv", 2, 3, 'b', "AMC Vanilla Swap EUR EPE")
-oreex.decorate_plot(title="Example 39 - Simulated exposure for 20y EUR Payer Swap")
-oreex.save_plot_to_file()
