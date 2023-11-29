df897de6617fdc0a73468201ba2474d59f00e31e
Peter Caspers <peter.caspers@acadia.inc>
2023-11-29 15:49:26 +0100

    QPR-12028 back to original settings, add hw dynamic

--- OREData/ored/scripting/models/lgmcg.cpp
+++ OREData/ored/scripting/models/lgmcg.cpp
@@ -37,14 +37,14 @@ std::size_t LgmCG::numeraire(const Date& d, const std::size_t x, const Handle<Yi
     std::size_t n;
     if (n = cg_var(g_, id, ComputationGraph::VarDoesntExist::Nan); n == ComputationGraph::nan) {
         auto p(p_);
-        Date ds = getSloppyDate(d, sloppySimDates_, effSimDates_);
+        // Date ds = getSloppyDate(d, sloppySimDates_, effSimDates_);
         Real t = p()->termStructure()->timeFromReference(d);
-        Real ts = p()->termStructure()->timeFromReference(ds);
-        std::string id_P0t = "__dsc_" + ore::data::to_string(ds) + "_" + discountCurveId;
-        std::string id_H = "__lgm_" + qualifier_ + "_H_" + ore::data::to_string(ds);
-        std::string id_zeta = "__lgm_" + qualifier_ + "_zeta_" + ore::data::to_string(ds);
-        std::size_t H = addModelParameter(g_, modelParameters_, id_H, [p, ts] { return p()->H(ts); });
-        std::size_t zeta = addModelParameter(g_, modelParameters_, id_zeta, [p, ts] { return p()->zeta(ts); });
+        // Real ts = p()->termStructure()->timeFromReference(ds);
+        std::string id_P0t = "__dsc_" + ore::data::to_string(d) + "_" + discountCurveId;
+        std::string id_H = "__lgm_" + qualifier_ + "_H_" + ore::data::to_string(d);
+        std::string id_zeta = "__lgm_" + qualifier_ + "_zeta_" + ore::data::to_string(d);
+        std::size_t H = addModelParameter(g_, modelParameters_, id_H, [p, t] { return p()->H(t); });
+        std::size_t zeta = addModelParameter(g_, modelParameters_, id_zeta, [p, t] { return p()->zeta(t); });
         std::size_t P0t = addModelParameter(g_, modelParameters_, id_P0t, [p, discountCurve, t] {
             return (discountCurve.empty() ? p()->termStructure() : discountCurve)->discount(t);
         });
@@ -79,17 +79,17 @@ std::size_t LgmCG::reducedDiscountBond(const Date& d, Date e, const std::size_t 
     std::size_t n;
     if (n = cg_var(g_, id, ComputationGraph::VarDoesntExist::Nan), n == ComputationGraph::nan) {
         auto p = p_;
-        Date ds = getSloppyDate(d, sloppySimDates_, effSimDates_);
+        // Date ds = getSloppyDate(d, sloppySimDates_, effSimDates_);
         Date es = getSloppyDate(e, sloppySimDates_, effSimDates_);
-        // Real t = p()->termStructure()->timeFromReference(d);
+        Real t = p()->termStructure()->timeFromReference(d);
         Real T = p()->termStructure()->timeFromReference(e);
-        Real ts = p()->termStructure()->timeFromReference(ds);
-        Real Ts = p()->termStructure()->timeFromReference(es);
+        // Real ts = p()->termStructure()->timeFromReference(ds);
+        // Real Ts = p()->termStructure()->timeFromReference(es);
         std::string id_P0T = "__dsc_" + ore::data::to_string(es) + "_" + discountCurveId;
-        std::string id_H = "__lgm_" + qualifier_ + "_H_" + ore::data::to_string(es);
-        std::string id_zeta = "__lgm_" + qualifier_ + "_zeta_" + ore::data::to_string(ds);
-        std::size_t H = addModelParameter(g_, modelParameters_, id_H, [p, Ts] { return p()->H(Ts); });
-        std::size_t zeta = addModelParameter(g_, modelParameters_, id_zeta, [p, ts] { return p()->zeta(ts); });
+        std::string id_H = "__lgm_" + qualifier_ + "_H_" + ore::data::to_string(e);
+        std::string id_zeta = "__lgm_" + qualifier_ + "_zeta_" + ore::data::to_string(d);
+        std::size_t H = addModelParameter(g_, modelParameters_, id_H, [p, T] { return p()->H(T); });
+        std::size_t zeta = addModelParameter(g_, modelParameters_, id_zeta, [p, t] { return p()->zeta(t); });
         std::size_t P0T = addModelParameter(g_, modelParameters_, id_P0T, [p, discountCurve, T] {
             return (discountCurve.empty() ? p()->termStructure() : discountCurve)->discount(T);
         });
@@ -129,10 +129,8 @@ std::size_t LgmCG::fixing(const boost::shared_ptr<InterestRateIndex>& index, con
 
             Time dt = ibor->dayCounter().yearFraction(d1, d2);
 
-            std::size_t disc1 =
-                reducedDiscountBond(t, d1, x, ibor->forwardingTermStructure(), "fwd_" + index->name());
-            std::size_t disc2 =
-                reducedDiscountBond(t, d2, x, ibor->forwardingTermStructure(), "fwd_" + index->name());
+            std::size_t disc1 = reducedDiscountBond(t, d1, x, ibor->forwardingTermStructure(), "fwd_" + index->name());
+            std::size_t disc2 = reducedDiscountBond(t, d2, x, ibor->forwardingTermStructure(), "fwd_" + index->name());
 
             n = cg_div(g_, cg_subtract(g_, cg_div(g_, disc1, disc2), cg_const(g_, 1.0)), cg_const(g_, dt));
 
