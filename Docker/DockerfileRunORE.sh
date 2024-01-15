6bd231614660203a9a031881885d5b29cde4c1bb
Damien Barker <damien.barker@acadia.inc>
2024-01-15 13:05:50 +0000

    use ninja

--- Docker/DockerfileRunORE.sh
+++ Docker/DockerfileRunORE.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-quantlib.yml build || exit 1
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore2.yml build || exit 1
