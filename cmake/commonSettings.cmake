e2f19e836795f13cd47f655c6b368db449e1984d
Matthias Groncki <matthias.groncki@acadia.inc>
2024-01-16 15:57:49 +0100

    fix abi mismatch between boost log > 1.72 and ORE

--- cmake/commonSettings.cmake
+++ cmake/commonSettings.cmake
@@ -62,7 +62,9 @@ endif()
 
 if(MSVC)
     set(BUILD_SHARED_LIBS OFF)
-
+    add_compile_definitions(_WINVER=0x0601)
+    add_compile_definitions(_WIN32_WINNT=0x0601)
+    add_compile_definitions(BOOST_USE_WINAPI_VERSION=0x0601)
     # build static libs always
     set(CMAKE_MSVC_RUNTIME_LIBRARY
         "MultiThreaded$<$<CONFIG:Debug>:Debug>$<$<BOOL:${MSVC_LINK_DYNAMIC_RUNTIME}>:DLL>")
