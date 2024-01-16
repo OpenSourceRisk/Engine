619950c8119191b34a33cec09bd1f5ba94ba0bb3
Roland Lichters <roland.lichters@acadia.inc>
2024-01-16 09:31:08 +0100

    update expected results with large number of samples

--- Examples/Example_55/run.py
+++ Examples/Example_55/run.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+import os
 import sys
 sys.path.append('../')
 from ore_examples_helper import OreExample
+
+samples1=os.environ["OVERWRITE_SCENARIOGENERATOR_SAMPLES"]
+print("samples1 =", samples1)
+os.environ["OVERWRITE_SCENARIOGENERATOR_SAMPLES"]=""
+samples2=os.environ["OVERWRITE_SCENARIOGENERATOR_SAMPLES"]
+print("samples2 =", samples2)
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
@@ -17,3 +24,5 @@ oreex.plot("exposure_trade_SCRIPTED_FX_TARF.csv", 2, 3, 'b', "TaRF EPE")
 oreex.plot("exposure_trade_SCRIPTED_FX_TARF.csv", 2, 4, 'r', "TaRF ENE")
 oreex.decorate_plot(title="Example Scripting / AMC - Simulated exposure for TaRF")
 oreex.save_plot_to_file()
+
+os.environ["OVERWRITE_SCENARIOGENERATOR_SAMPLES"]=samples1
