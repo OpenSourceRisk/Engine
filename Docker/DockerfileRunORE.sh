a843459f921710825fcd42b82737aae0feab66cc
Peter Caspers <peter.caspers@lseg.com>
2024-01-18 09:55:07 +0100

    Revert "tmp disable cache"
    This reverts commit be25eab417fc371dba7eeddcf637b9e47a0fd848.

--- Docker/DockerfileRunORE.sh
+++ Docker/DockerfileRunORE.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore.yml build --no-cache || exit 1
-docker-compose --env-file Docker/.env -f Docker/docker-compose-test.yml build --no-cache || exit 1
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-app.yml build --no-cache || exit 1
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore.yml --no-cache build || exit 1
+docker-compose --env-file Docker/.env -f Docker/docker-compose-test.yml --no-cache build || exit 1
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-app.yml --no-cache build || exit 1
