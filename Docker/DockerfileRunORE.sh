f8e7230e152a0874bb3473beb42b9a7683f9f83b
Peter Caspers <peter.caspers@lseg.com>
2024-01-18 09:15:43 +0100

    tmp disable cache

--- Docker/DockerfileRunORE.sh
+++ Docker/DockerfileRunORE.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore.yml build || exit 1
-docker-compose --env-file Docker/.env -f Docker/docker-compose-test.yml build || exit 1
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-app.yml build || exit 1
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore.yml --no-cache build || exit 1
+docker-compose --env-file Docker/.env -f Docker/docker-compose-test.yml --no-cache build || exit 1
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-app.yml --no-cache build || exit 1
