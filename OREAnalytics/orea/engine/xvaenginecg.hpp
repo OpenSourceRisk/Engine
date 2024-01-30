3f050e6b2af27d9fae5a73a4cea584066d90b6d4
Peter Caspers <peter.caspers@acadia.inc>
2024-01-30 09:49:00 +0000

    Resolve QPR-12253 parSensitivity TBS

--- OREAnalytics/orea/engine/xvaenginecg.hpp
+++ OREAnalytics/orea/engine/xvaenginecg.hpp
@@ -70,7 +70,6 @@ private:
                                  const std::vector<std::pair<std::size_t, double>>& modelParameters) const;
 
     // input parameters
-    Size nThreads_;
     Date asof_;
     boost::shared_ptr<ore::data::Loader> loader_;
     boost::shared_ptr<ore::data::CurveConfigurations> curveConfigs_;
