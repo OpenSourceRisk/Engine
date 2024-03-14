774a0bf3869fe6d3dc4e3531bf97964202b19569
Roland Lichters <roland.lichters@lseg.com>
2024-03-14 17:20:33 +0000

    QPR-12370 fix

--- Examples/Example_44/run.py
+++ Examples/Example_44/run.py
@@ -18,4 +18,5 @@ for orexml in orexmls:
 	oreex.print_headline(f"Run ORE SIMM; version={orexml[1]}; MPOR days={orexml[2]}")
 	oreex.run(orexml[0])
 
+oreex.print_headline(f"Run ORE for IM Schedule")
 oreex.run("Input/ore_schedule.xml")
