3499e342101aac3e6a3225fef26be5760905d58b
Peter Caspers <peter.caspers@lseg.com>
2024-04-03 17:02:49 +0200

    QPR-12417 revert for the time being

--- OREAnalytics/orea/app/reportwriter.cpp
+++ OREAnalytics/orea/app/reportwriter.cpp
@@ -580,8 +580,10 @@ void ReportWriter::writeCashflow(ore::data::Report& report, const std::string& b
                     if (cf.effectiveCapVolatility != Null<Real>())
                         capVolatility = cf.effectiveCapVolatility;
 
-                    if (!includePastCashflows && cf.payDate <= asof)
-                        continue;
+                    // to be consistent with the leg-based cf report we should do this:
+                    // if (!includePastCashflows && cf.payDate <= asof)
+                    //     continue;
+                    // however, this changes a lot of results, so we output all cfs for the time being
 
                     report.next()
                         .add(trade->id())
