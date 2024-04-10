b974bd022aab2cbeb416070b1f384cb554cc3050
Peter Caspers <peter.caspers@acadia.inc>
2024-04-10 07:44:47 +0000

    Resolve QPR-12419 add optional pca-instructed coordinate transform to regression analysis (AMC and scripted trades, comes up in BNY xva-project)

--- QuantExt/qle/pricingengines/mcmultilegoptionengine.hpp
+++ QuantExt/qle/pricingengines/mcmultilegoptionengine.hpp
@@ -47,7 +47,8 @@ public:
         const std::vector<Handle<YieldTermStructure>>& discountCurves = std::vector<Handle<YieldTermStructure>>(),
         const std::vector<Date>& simulationDates = std::vector<Date>(),
         const std::vector<Size>& externalModelIndices = std::vector<Size>(), const bool minimalObsDate = true,
-        const RegressorModel regressorModel = RegressorModel::Simple);
+        const RegressorModel regressorModel = RegressorModel::Simple,
+        const Real regressionVarianceCutoff = Null<Real>());
     McMultiLegOptionEngine(const boost::shared_ptr<LinearGaussMarkovModel>& model,
                            const SequenceType calibrationPathGenerator, const SequenceType pricingPathGenerator,
                            const Size calibrationSamples, const Size pricingSamples, const Size calibrationSeed,
@@ -59,7 +60,8 @@ public:
                            const std::vector<Date>& simulationDates = std::vector<Date>(),
                            const std::vector<Size>& externalModelIndices = std::vector<Size>(),
                            const bool minimalObsDate = true,
-                           const RegressorModel regressorModel = RegressorModel::Simple);
+                           const RegressorModel regressorModel = RegressorModel::Simple,
+                           const Real regressionVarianceCutoff = Null<Real>());
 
     void calculate() const override;
     const Handle<CrossAssetModel>& model() const { return model_; }
