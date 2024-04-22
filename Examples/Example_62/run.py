d9311e6700323bf878b9e4858ab7c2b70654a8ed
Roland Lichters <roland.lichters@lseg.com>
2024-04-22 11:17:41 +0200

    QPR-12455 add Jupyter notebook

--- Examples/Example_62/run.py
+++ Examples/Example_62/run.py
@@ -7,7 +7,11 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE")
-oreex.run("Input/ore.xml")
-oreex.get_times("Output/log.txt")
+oreex.print_headline("Run ORE Bump")
+oreex.run("Input/ore_bump.xml")
+oreex.get_times("Output/bump/log.txt")
+
+oreex.print_headline("Run ORE AD")
+oreex.run("Input/ore_ad.xml")
+oreex.get_times("Output/ad/log.txt")
 
