d747f14896bccb3b6eb5a0aa51b14bb2f67ed53b
Roland Lichters <roland.lichters@lseg.com>
2024-04-23 12:49:51 +0200

    QPR-12455 compare performance and sensis, classic - cg - ad - gpu

--- Examples/Example_62/run.py
+++ Examples/Example_62/run.py
@@ -7,11 +7,19 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE Bump")
-oreex.run("Input/ore_bump.xml")
+oreex.print_headline("Run ORE Bump Sensitivities")
+oreex.run("Input/ore.xml")
 oreex.get_times("Output/bump/log.txt")
+
+oreex.print_headline("Run ORE with CG")
+oreex.run("Input/ore_cg.xml")
+oreex.get_times("Output/cg/log.txt")
 
 oreex.print_headline("Run ORE AD")
 oreex.run("Input/ore_ad.xml")
 oreex.get_times("Output/ad/log.txt")
+
+oreex.print_headline("Run ORE on GPU")
+oreex.run("Input/ore_gpu.xml")
+oreex.get_times("Output/gpu/log.txt")
 
