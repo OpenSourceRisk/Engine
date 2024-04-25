4c8d7e6bc2fa86e5394423851001b7e1f7c370d6
Sebastien Bouvard <sebastien.bouvard@acadia.inc>
2024-04-25 12:34:24 +0200

    Merge branch 'master' into QPR-12223

--- OREData/ored/utilities/initbuilders.cpp
+++ OREData/ored/utilities/initbuilders.cpp
@@ -254,6 +254,7 @@ void initBuilders() {
     ORE_REGISTER_REFERENCE_DATUM("CreditIndex", CreditIndexReferenceDatum, false)
     ORE_REGISTER_REFERENCE_DATUM("EquityIndex", EquityIndexReferenceDatum, false)
     ORE_REGISTER_REFERENCE_DATUM("CurrencyHedgedEquityIndex", CurrencyHedgedEquityIndexReferenceDatum, false)
+    ORE_REGISTER_REFERENCE_DATUM("PortfolioBasket", PortfolioBasketReferenceDatum, false)
     ORE_REGISTER_REFERENCE_DATUM("CommodityIndex", CommodityIndexReferenceDatum, false)
     ORE_REGISTER_REFERENCE_DATUM("Credit", CreditReferenceDatum, false)
     ORE_REGISTER_REFERENCE_DATUM("Equity", EquityReferenceDatum, false)
