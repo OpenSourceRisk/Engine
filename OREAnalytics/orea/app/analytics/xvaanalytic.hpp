4026d7de95897d273f1447b1e6a7fd9e0d4e17a7
Peter Caspers <peter.caspers@lseg.com>
2024-05-02 15:26:39 +0200

    QPR-12376 update analytic

--- OREAnalytics/orea/app/analytics/xvaanalytic.hpp
+++ OREAnalytics/orea/app/analytics/xvaanalytic.hpp
@@ -31,7 +31,9 @@ class XvaAnalyticImpl : public Analytic::Impl {
 public:
     static constexpr const char* LABEL = "XVA";
 
-    XvaAnalyticImpl(const QuantLib::ext::shared_ptr<InputParameters>& inputs) : Analytic::Impl(inputs) { setLabel(LABEL); }
+    explicit XvaAnalyticImpl(const QuantLib::ext::shared_ptr<InputParameters>& inputs) : Analytic::Impl(inputs) {
+        setLabel(LABEL);
+    }
     virtual void runAnalytic(const QuantLib::ext::shared_ptr<ore::data::InMemoryLoader>& loader,
                              const std::set<std::string>& runTypes = {}) override;
     void setUpConfigurations() override;
@@ -81,8 +83,9 @@ protected:
 
 class XvaAnalytic : public Analytic {
 public:
-    XvaAnalytic(const QuantLib::ext::shared_ptr<InputParameters>& inputs)
-        : Analytic(std::make_unique<XvaAnalyticImpl>(inputs), {"XVA", "EXPOSURE"}, inputs, false, false, false, false) {}
+    explicit XvaAnalytic(const QuantLib::ext::shared_ptr<InputParameters>& inputs)
+        : Analytic(std::make_unique<XvaAnalyticImpl>(inputs), {"XVA", "EXPOSURE"}, inputs, false, false, false, false) {
+    }
 };
 
 } // namespace analytics
