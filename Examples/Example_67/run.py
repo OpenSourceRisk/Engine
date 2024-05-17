e676907e9ea9eb94c6e028ee0f4deb8e16803a21
Matthias Groncki <matthias.groncki@acadia.inc>
2024-05-17 18:11:30 +0200

    QPR-12376 update example

--- Examples/Example_67/run.py
+++ Examples/Example_67/run.py
@@ -16,7 +16,7 @@ oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
 oreex.print_headline("Run ORE to produce XVA Stresstest with AMC")
 oreex.run("Input/ore_amc.xml")
-oreex.get_times("Output/log.txt")
+#oreex.get_times("Output/log.txt")
 
 if "OVERWRITE_SCENARIOGENERATOR_SAMPLES" in os.environ.keys():
     os.environ["OVERWRITE_SCENARIOGENERATOR_SAMPLES"]=samples1
