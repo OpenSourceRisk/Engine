1a172d91e760e10ac0cfb4c6e774624003910444
Peter Caspers <peter.caspers@lseg.com>
2024-06-03 10:52:34 +0200

    QPR-12228 add skeleton for function adding non-st to cg

---
+++ OREAnalytics/orea/engine/addnonscriptedtradetocg.hpp
@@ -0,0 +1,35 @@
+/*
+ Copyright (C) 2017 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+/*! \file engine/addnonscriptedtradetocg.hpp
+    \brief convert non-scripted trade to computation graph
+    \ingroup engine
+*/
+
+#pragma once
+
+#include <>
+
+namespace ore {
+namespace analytics {
+
+void addNonScriptedTradeToCG(const QuantLib::ext::shared_ptr<Trade>& trade,
+                             const QuantLib::ext::shared_ptr<GaussianCamCG>& model);
+
+} // namespace analytics
+} // namespace ore
