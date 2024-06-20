03ce448ac9de2d578f9472a519d6ec30ed318c89
Peter Caspers <peter.caspers@acadia.inc>
2024-06-20 14:05:04 +0000

    Merge branch 'QPR-12517_3' into 'master'
    QPR-12517 - InMemoryReport - use temp dir / delete temp files
    
    Closes QPR-12517
    
    See merge request qs/oreplus!2103

--- OREData/ored/report/inmemoryreport.hpp
+++ OREData/ored/report/inmemoryreport.hpp
@@ -42,6 +42,7 @@ using std::vector;
 class InMemoryReport : public Report {
 public:
     explicit InMemoryReport(Size bufferSize=100000) : i_(0), bufferSize_(bufferSize) {}
+    ~InMemoryReport() override;
 
     Report& addColumn(const string& name, const ReportType& rt, Size precision = 0) override;
     Report& next() override;
