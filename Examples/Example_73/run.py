c24d1ac3b3aada1ba66fabbf69ca486cf8bafceb
Damien Barker <damien.barker@acadia.inc>
2024-07-11 10:15:20 +0100

    QPR-12646 failing tests

--- Examples/Example_73/run.py
+++ Examples/Example_73/run.py
@@ -6,11 +6,6 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE PnL")
-oreex.run("Input/ore_pnl.xml")
+oreex.print_headline("Run ORE Portfolio details")
+oreex.run("Input/ore.xml")
 
-oreex.print_headline("Run ORE PNL Explain")
-oreex.run("Input/ore_explain.xml")
-
-oreex.print_headline("Run ORE PNL Explain with Par Rates")
-oreex.run("Input/ore_explain_par.xml")
