5a8d76798448e49934b491590e41c38a442951d3
Peter Caspers <peter.caspers@acadia.inc>
2024-08-13 17:44:31 +0100

    QPR-12228 fix

--- OREAnalytics/orea/engine/xvaenginecg.cpp
+++ OREAnalytics/orea/engine/xvaenginecg.cpp
@@ -771,7 +771,7 @@ void XvaEngineCG::calculateSensitivities() {
 
 void XvaEngineCG::generateSensiReports() {
     DLOG("XvaEngineCG: write sensi report.");
-    if (!sensitivityData_) {
+    if (!sensitivityData_)
         return;
     sensiReport_ = QuantLib::ext::make_shared<InMemoryReport>();
     auto sensiCube = QuantLib::ext::make_shared<SensitivityCube>(
