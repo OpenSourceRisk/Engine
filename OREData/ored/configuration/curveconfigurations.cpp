2cbe687b49208d70ef460002eda869ce1899d312
Eric Ehlers <eric.ehlers@acadia.inc>
2024-08-21 14:26:27 +0100

    QPR-12678: Pass the ReportConfig node from the input to the output (e.g. to curveconfig.xml)

--- OREData/ored/configuration/curveconfigurations.cpp
+++ OREData/ored/configuration/curveconfigurations.cpp
@@ -590,8 +590,42 @@ XMLNode* CurveConfigurations::toXML(XMLDocument& doc) const {
     addNodes(doc, parent, "CommodityCurves");
     addNodes(doc, parent, "CommodityVolatilities");
     addNodes(doc, parent, "Correlations");
+    addReportConfigurationNode(doc, parent);
 
     return parent;
+}
+
+// Append to the parent node a ReportConfiguration node generated from member variables
+void CurveConfigurations::addReportConfigurationNode(XMLDocument& doc, XMLNode* parent) const {
+    // Allocate a node to contain the ReportConfiguration data
+    XMLNode* node = doc.allocNode("ReportConfiguration");
+    // A flag to indicate whether or not the generated ReportConfiguration node contains any data
+    bool haveData = false;
+    // A function to append data from member variables (ReportConfig objects) to the ReportConfiguration node
+    auto f = [&doc, node, &haveData](const ReportConfig& rc, const string& label) {
+        // From the ReportConfig object, generate a (possibly null) node.  If it exists it will have the label "Report".
+        if (const auto& xml = rc.toXML(doc)) {
+            // If the generated Report node has no children then exit
+            if (XMLUtils::getChildrenNodes(xml, "").empty())
+                return;
+            // Tell the surrounding context that we have some data
+            haveData = true;
+            // Append the data from the Report node to a new child node called "label" in the ReportConfiguration node
+            XMLNode* child = doc.allocNode(label);
+            XMLUtils::appendNode(node, child);
+            XMLUtils::appendNode(child, xml);
+        }
+    };
+    // Call the above function for each of the ReportConfig members
+    f(reportConfigEqVols_, "EquityVolatilities");
+    f(reportConfigFxVols_, "FXVolatilities");
+    f(reportConfigCommVols_, "CommodityVolatilities");
+    f(reportConfigIrCapFloorVols_, "IRCapFloorVolatilities");
+    f(reportConfigIrSwaptionVols_, "IRSwaptionVolatilities");
+    f(reportConfigYieldCurves_, "YieldCurves");
+    // If the newly generated ReportConfiguration node contains any data then append it to the parent node
+    if (haveData)
+        XMLUtils::appendNode(parent, node);
 }
 
 void CurveConfigurations::addAdditionalCurveConfigs(const CurveConfigurations& c) {
