d2a3030975e8e1aabe6773f905a8d56d466b15cc
Peter Caspers <peter.caspers@lseg.com>
2024-09-06 16:07:49 +0200

    QPR-12793 restore behaviour of ex 67

--- Examples/run_examples_testsuite.py
+++ Examples/run_examples_testsuite.py
@@ -8,7 +8,7 @@ import nose
 import collections
 #collections.Callable = collections.abc.Callable
 
-examples_exempt_from_scenariogenerator_samples_overwrite = ['Example_54', 'Example_55', 'Example_56', 'Example_60', 'Example_67', 'Example_68', 'Example_70', 'Example_72']
+examples_exempt_from_scenariogenerator_samples_overwrite = ['Example_54', 'Example_55', 'Example_56', 'Example_60', 'Example_68', 'Example_70', 'Example_72']
 
 # Pull in some shared utilities
 script_dir = Path(__file__).parents[0]
