794be25883f7dd30f027241260bb05154f337915
Peter Caspers <peter.caspers@acadia.inc>
2024-09-11 20:17:20 +0000

    QPR-12791 this has been moved to crif generation

--- OREData/ored/portfolio/builders/forwardbond.cpp
+++ OREData/ored/portfolio/builders/forwardbond.cpp
@@ -86,10 +86,6 @@ void FwdBondEngineBuilder::setCurves(const string& id, const Currency& ccy, cons
         if (!creditCurveId.empty())
             recovery_ = market_->recoveryRate(creditCurveId, configuration(MarketContext::pricing));
     }
-
-    if (!hasCreditRisk) {
-        dpts_ = Handle<DefaultProbabilityTermStructure>();
-    }
 };
 
 QuantLib::ext::shared_ptr<PricingEngine> CamAmcFwdBondEngineBuilder::buildMcEngine(
