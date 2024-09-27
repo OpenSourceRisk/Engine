b55fc4f8b594628d2e4e260237b12f9200f66a10
Matthias Groncki <matthias.groncki@acadia.inc>
2024-09-27 07:42:10 +0000

    Update run.py

--- Examples/Example_78/run.py
+++ Examples/Example_78/run.py
@@ -6,6 +6,6 @@ from ore_examples_helper import OreExample
 
 oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
 
-oreex.print_headline("Run ORE to produce Bond NPV with BondYieldShifted curve setup")
+oreex.print_headline("Run ORE to produce Bond NPV and Cashflow Report including past cashflows")
 oreex.run("Input/ore.xml")
 oreex.get_times("Output/log.txt")
