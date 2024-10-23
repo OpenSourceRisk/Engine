fd8bcc18178c6b66d855557b9fd06a523df816f1
Bouvard <sebastien.bouvard@lseg.com>
2024-10-23 13:09:49 +0200

    QPR-12911 Handle empty numericCode Node for currencies config

--- OREData/ored/configuration/currencyconfig.cpp
+++ OREData/ored/configuration/currencyconfig.cpp
@@ -53,11 +53,14 @@ void CurrencyConfig::fromXML(XMLNode* baseNode) {
     for (auto node : XMLUtils::getChildrenNodes(baseNode, "Currency")) {
         string name = XMLUtils::getChildValue(node, "Name", false);
         string isoCode = XMLUtils::getChildValue(node, "ISOCode", false);
-	auto tmp = parseListOfValues(XMLUtils::getChildValue(node, "MinorUnitCodes", false));
+        auto tmp = parseListOfValues(XMLUtils::getChildValue(node, "MinorUnitCodes", false));
         std::set<std::string> minorUnitCodes(tmp.begin(), tmp.end());
         try {
             DLOG("Loading external currency configuration for " << isoCode);
-            Integer numericCode = parseInteger(XMLUtils::getChildValue(node, "NumericCode", false));
+            Integer numericCode = parseInteger("999");
+            if (XMLUtils::getChildValue(node, "NumericCode", false) != "") {
+                numericCode = parseInteger(XMLUtils::getChildValue(node, "NumericCode", false));
+            } 
             string symbol = XMLUtils::getChildValue(node, "Symbol", false);
             string fractionSymbol = XMLUtils::getChildValue(node, "Symbol", false);
             Integer fractionsPerUnit = parseInteger(XMLUtils::getChildValue(node, "FractionsPerUnit", false));
