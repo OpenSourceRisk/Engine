ffe7dc585b900c1044723b7f8c540bcaabdaa14d
Peter Caspers <peter.caspers@acadia.inc>
2024-10-28 15:54:23 +0000

    Merge branch 'QPR-12902' into 'master'
    Resolve QPR-12902 offset xva sensi in base scenario
    
    Closes QPR-12902
    
    See merge request qs/oreplus!2382

--- OREAnalytics/orea/app/analytics/xvasensitivityanalytic.cpp
+++ OREAnalytics/orea/app/analytics/xvasensitivityanalytic.cpp
@@ -140,8 +140,8 @@ void XvaSensitivityAnalyticImpl::runSensitivity(
             DLOG("Calculate XVA for scenario " << label);
             CONSOLE("XVA_SENSITIVITY: Apply scenario " << label);
             auto newAnalytic = ext::make_shared<XvaAnalytic>(
-                inputs_, (label == "BASE" ? nullptr : scenario),
-                (label == "BASE" ? nullptr : analytic()->configurations().simMarketParams));
+                inputs_, scenario,
+                analytic()->configurations().simMarketParams);
             CONSOLE("XVA_SENSITIVITY: Calculate Exposure and XVA")
             newAnalytic->runAnalytic(loader, {"EXPOSURE", "XVA"});
             // Collect exposure and xva reports
