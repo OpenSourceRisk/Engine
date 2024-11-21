b0017a24145212934b7a47cc1076d4f19abbec4b
Alex Chow <alex.chow@acadia.inc>
2024-11-21 11:59:41 +0000

    QPR-12977 Refactor InMemoryCube classes to make them more memory efficient

--- OREAnalytics/orea/cube/inmemorycube.hpp
+++ OREAnalytics/orea/cube/inmemorycube.hpp
@@ -51,12 +51,13 @@ template <typename T> class InMemoryCubeBase : public NPVCube {
 public:
     //! default ctor
     InMemoryCubeBase(const Date& asof, const std::set<std::string>& ids, const vector<Date>& dates, Size samples,
-                     const T& t = T())
-        : asof_(asof), dates_(dates), samples_(samples), t0Data_(ids.size(), t),
-          data_(ids.size(), vector<vector<T>>(dates.size(), vector<T>(samples, t))) {
+                     Size depth, const T& t = T())
+        : asof_(asof), dates_(dates), samples_(samples), depth_(depth), t0Data_(depth, vector<T>(ids.size(), t)),
+          data_(depth, vector<vector<vector<T>>>(dates.size(), vector(ids.size(), vector<T>(samples, t)))) {
         QL_REQUIRE(ids.size() > 0, "InMemoryCube::InMemoryCube no ids specified");
         QL_REQUIRE(dates.size() > 0, "InMemoryCube::InMemoryCube no dates specified");
         QL_REQUIRE(samples > 0, "InMemoryCube::InMemoryCube samples must be > 0");
+        QL_REQUIRE(depth > 0, "InMemoryCube::InMemoryCube depth must be > 0");
         size_t pos = 0;
         for (const auto& id : ids) {
             idIdx_[id] = pos++;
@@ -70,7 +71,8 @@ public:
     //! Return the length of each dimension
     Size numIds() const override { return idIdx_.size(); }
     Size numDates() const override { return dates_.size(); }
-    virtual Size samples() const override { return samples_; }
+    Size samples() const override { return samples_; }
+    Size depth() const override { return depth_; }
 
     //! Return a map of all ids and their position in the cube
     const std::map<std::string, Size>& idsAndIndexes() const override { return idIdx_; }
@@ -91,93 +93,61 @@ protected:
     QuantLib::Date asof_;
     vector<QuantLib::Date> dates_;
     Size samples_;
-    vector<T> t0Data_;
-    vector<vector<vector<T>>> data_;
+    Size depth_;
+    vector<vector<T>> t0Data_;
+    vector<vector<vector<vector<T>>>> data_;
 
     std::map<std::string, Size> idIdx_;
-};
-
-//! InMemoryCube of fixed depth 1
-/*! This implementation stores the type directly in an InMemoryCubeBase to avoid us having
- *  nested vectors all of length 1
- */
-template <typename T> class InMemoryCube1 : public InMemoryCubeBase<T> {
-public:
-    //! ctor
-    InMemoryCube1(const Date& asof, const std::set<std::string>& ids, const vector<Date>& dates, Size samples,
-                  const T& t = T())
-        : InMemoryCubeBase<T>(asof, ids, dates, samples, t) {}
-
-    //! default
-    InMemoryCube1() {}
-
-    //! Fixed depth
-    Size depth() const override { return 1; }
-
-    //! Get a T0 value from the cube
-    virtual Real getT0(Size i, Size d) const override {
-        this->check(i, 0, 0, d);
-        return this->t0Data_[i];
-    }
-
-    //! Set a value in the cube
-    virtual void setT0(Real value, Size i, Size d) override {
-        this->check(i, 0, 0, d);
-        this->t0Data_[i] = static_cast<T>(value);
-    }
-
-    //! Get a value from the cube
-    Real get(Size i, Size j, Size k, Size d) const override {
-        this->check(i, j, k, d);
-        return this->data_[i][j][k];
-    }
-
-    //! Set a value in the cube
-    void set(Real value, Size i, Size j, Size k, Size d) override {
-        this->check(i, j, k, d);
-        this->data_[i][j][k] = static_cast<T>(value);
-    }
 };
 
 //! InMemoryCube of variable depth
 /*! This implementation stores a vector an InMemoryCubeBase
  */
-template <typename T> class InMemoryCubeN : public InMemoryCubeBase<vector<T>> {
+template <typename T> class InMemoryCubeN : public InMemoryCubeBase<T> {
 public:
     //! ctor
     InMemoryCubeN(const Date& asof, const std::set<std::string>& ids, const vector<Date>& dates, Size samples, Size depth,
                   const T& t = T())
-        : InMemoryCubeBase<vector<T>>(asof, ids, dates, samples, vector<T>(depth, t)) {}
+        : InMemoryCubeBase<T>(asof, ids, dates, samples, depth, t) {}
 
     //! default
     InMemoryCubeN() {}
 
-    //! Depth
-    Size depth() const override { return this->data_[0][0][0].size(); } // we don't want any members in this class
-
     //! Get a T0 value from the cube
     virtual Real getT0(Size i, Size d) const override {
         this->check(i, 0, 0, d);
-        return this->t0Data_[i][d];
+        return this->t0Data_[d][i];
     }
 
     //! Set a value in the cube
     virtual void setT0(Real value, Size i, Size d) override {
         this->check(i, 0, 0, d);
-        this->t0Data_[i][d] = static_cast<T>(value);
+        this->t0Data_[d][i] = static_cast<T>(value);
     }
 
     //! Get a value from the cube
     Real get(Size i, Size j, Size k, Size d) const override {
         this->check(i, j, k, d);
-        return this->data_[i][j][k][d];
+        return this->data_[d][j][i][k];
     }
 
     //! Set a value in the cube
     void set(Real value, Size i, Size j, Size k, Size d) override {
         this->check(i, j, k, d);
-        this->data_[i][j][k][d] = static_cast<T>(value);
+        this->data_[d][j][i][k] = static_cast<T>(value);
     }
+};
+
+//! InMemoryCube of fixed depth 1
+template <typename T> class InMemoryCube1 : public InMemoryCubeN<T> {
+public:
+    //! ctor
+    InMemoryCube1(const Date& asof, const std::set<std::string>& ids, const vector<Date>& dates, Size samples,
+                  const T& t = T())
+        : InMemoryCubeN<T>(asof, ids, dates, samples, 1, t) {}
+
+    //! default
+    InMemoryCube1() {}
 };
 
 //! InMemoryCube of depth 1 with single precision floating point numbers.
