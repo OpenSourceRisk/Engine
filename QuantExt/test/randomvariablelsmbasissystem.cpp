9fc8f206f82fbee3ac93d9686b102cf0f1384409
Peter Caspers <peter.caspers@lseg.com>
2025-01-03 11:56:20 +0100

    QPR-12228 add unit test for var groups

--- QuantExt/test/randomvariablelsmbasissystem.cpp
+++ QuantExt/test/randomvariablelsmbasissystem.cpp
@@ -18,8 +18,8 @@
 
 #include <qle/math/randomvariablelsmbasissystem.hpp>
 
-#include <ql/time/date.hpp>
 #include <ql/math/randomnumbers/haltonrsg.hpp>
+#include <ql/time/date.hpp>
 
 #include <boost/test/unit_test.hpp>
 
@@ -86,9 +86,71 @@ BOOST_AUTO_TEST_CASE(testBasisSystem) {
                          ++itRv, ++itRef) {
                         BOOST_CHECK(QuantLib::close_enough(*itRv, *itRef));
                     }
-
                 }
             }
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(testVarGroups) {
+
+    BOOST_TEST_MESSAGE("Testing lsm basis system for random variables with var groups...");
+
+    auto bs = multiPathBasisSystem(5, 5, QuantLib::LsmBasisSystem::PolynomialType::Monomial, {{0, 1}, {2, 3, 4}});
+
+    auto bs1 = multiPathBasisSystem(2, 5, QuantLib::LsmBasisSystem::PolynomialType::Monomial);
+    auto bs2 = multiPathBasisSystem(3, 5, QuantLib::LsmBasisSystem::PolynomialType::Monomial);
+    bs2.erase(bs2.begin()); // constant term is in bs1 and bs2, so we remove the one in bs2
+
+    BOOST_REQUIRE(bs.size() == bs1.size() + bs2.size());
+
+    Size nSamplePoints = 20;
+
+    Array samplePoint;
+    std::vector<RandomVariable> samplePointRv(5);
+    std::vector<RandomVariable> samplePointRv1(2);
+    std::vector<RandomVariable> samplePointRv2(3);
+    std::vector<const RandomVariable*> samplePointRvPtr(5);
+    std::vector<const RandomVariable*> samplePointRvPtr1(2);
+    std::vector<const RandomVariable*> samplePointRvPtr2(3);
+
+    HaltonRsg rsg(5, 42);
+    for (Size i = 0; i < nSamplePoints; ++i) {
+        auto tmp = rsg.nextSequence().value;
+        samplePoint = Array(tmp.begin(), tmp.end());
+
+        for (Size d = 0; d < 5; ++d) {
+            samplePointRv[d] = RandomVariable(1, samplePoint[d]);
+            samplePointRvPtr[d] = &samplePointRv[d];
+        }
+
+        for (Size d = 0; d < 2; ++d) {
+            samplePointRv1[d] = RandomVariable(1, samplePoint[d]);
+            samplePointRvPtr1[d] = &samplePointRv[d];
+        }
+
+        for (Size d = 0; d < 3; ++d) {
+            samplePointRv2[d] = RandomVariable(1, samplePoint[d + 2]);
+            samplePointRvPtr2[d] = &samplePointRv2[d];
+        }
+
+        std::set<Real> valuesRv;
+        std::set<Real> valuesRv12;
+
+        for (auto const& f : bs) {
+            valuesRv.insert(f(samplePointRvPtr).at(0));
+        }
+
+        for (auto const& f : bs1) {
+            valuesRv12.insert(f(samplePointRvPtr1).at(0));
+        }
+        for (auto const& f : bs2) {
+            valuesRv12.insert(f(samplePointRvPtr2).at(0));
+        }
+
+        BOOST_REQUIRE(valuesRv.size() == valuesRv12.size());
+        for (auto itRv = valuesRv.begin(), itRv12 = valuesRv12.begin(); itRv != valuesRv.end(); ++itRv, ++itRv12) {
+            BOOST_CHECK(QuantLib::close_enough(*itRv, *itRv12));
         }
     }
 }
