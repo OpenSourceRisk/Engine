afbe55b7665ccdc08642a3a59f14faa9c3d13d3e
cfdickmann <cfdickmann@googlemail.com>
2025-01-20 22:48:54 +0100

    QPR-12827: Start Black'76 Test

--- QuantExt/test/lgmpricing.cpp
+++ QuantExt/test/lgmpricing.cpp
@@ -108,6 +108,8 @@
 #include <ql/time/daycounters/actual360.hpp>
 #include <ql/time/daycounters/thirty360.hpp>
 
+#include <iostream>
+#include <cmath>
 #include <ql/indexes/ibor/usdlibor.hpp>
 #include <boost/make_shared.hpp>
 // fix for boost 1.64, see https://lists.boost.org/Archives/boost/2016/11/231756.php
@@ -141,8 +143,112 @@ using std::vector;
 
 BOOST_FIXTURE_TEST_SUITE(QuantExtTestSuite, qle::test::TopLevelFixture)
 
-BOOST_AUTO_TEST_SUITE(CrossAssetModelTest)
+BOOST_AUTO_TEST_SUITE(LgmPricingCases)
 
+// Define helper functions for Black'76 test case. The source code needs
+// this additional implementation to ensure an independent testing.
+double blackCallPrice(double F, double K, double vol, double T, double r) {
+    double d1 = (std::log(F / K) + 0.5 * vol * vol * T) / (vol * std::sqrt(T));
+    double d2 = d1 - vol * std::sqrt(T);
+    return std::exp(-r * T) * (F * 0.5 * erfc(-d1 / std::sqrt(2.0)) - K * 0.5 * erfc(-d2 / std::sqrt(2.0)));
+}
+
+double blackPutPrice(double F, double K, double vol, double T, double r) {
+    double d1 = (std::log(F / K) + 0.5 * vol * vol * T) / (vol * std::sqrt(T));
+    double d2 = d1 - vol * std::sqrt(T);
+    return std::exp(-r * T) * (K *  0.5 * erfc(d2 / std::sqrt(2.0)) - F *  0.5 * erfc(d1 / std::sqrt(2.0)));
+}
+
+BOOST_AUTO_TEST_CASE(testBlackCase) {
+    // The LGM model converes to the famous Black'76 model approach in case of a zero mean-reversion rate.
+    // The dynamics defined via the underlying SDE lead to that relationship between the two models.
+    
+    BOOST_TEST_MESSAGE("Testing LGM pricing in cases equal to the Black76 model ...");
+
+    Calendar calendar = TARGET();
+    Date settlementDate(15, July, 2015);
+    boost::shared_ptr<Exercise> exercise = boost::make_shared<EuropeanExercise>(Date(10, July, 2016)); // T=1
+    double T = 1.0;
+    double vol = 0.005;
+    Date startDate(15, July, 2016);
+    Settings::instance().evaluationDate() = settlementDate;    
+    Date maturityDate = calendar.advance(settlementDate, 5, Years);
+    Real notional = 1.0;
+    Rate fixedRate = 0.02;
+
+    auto eurYts= QuantLib::ext::make_shared<FlatForward>(settlementDate, fixedRate, Actual365Fixed());
+   
+    std::vector<Date> volstepdates;
+    volstepdates.push_back(Date(15, July, 2016));
+    volstepdates.push_back(Date(15, July, 2017));
+    volstepdates.push_back(Date(15, July, 2018));
+    volstepdates.push_back(Date(15, July, 2019));
+    volstepdates.push_back(Date(15, July, 2020));
+
+    std::vector<Real> eurVols;
+
+    auto  volsteptimes_a = Array(volstepdates.size());
+
+    for (Size i = 0; i < volstepdates.size(); ++i) 
+        volsteptimes_a[i] = eurYts->timeFromReference(volstepdates[i]);
+    
+    for (Size i = 0; i < volstepdates.size() + 1; ++i) 
+        eurVols.push_back(vol); // Alpha
+
+    Array eurVols_a;
+    Array notimes_a, eurKappa_a;
+    eurVols_a = Array(eurVols.begin(), eurVols.end());
+    notimes_a = Array(0);
+    eurKappa_a = Array(1, 0.00); // No mean reversion 
+
+    Handle<YieldTermStructure> eurYtsHandle(eurYts);
+    auto model = QuantLib::ext::make_shared<IrLgm1fPiecewiseConstantParametrization>(EURCurrency(), eurYtsHandle, volsteptimes_a, eurVols_a, notimes_a, eurKappa_a);
+
+    QuantLib::ext::shared_ptr<PricingEngine> eurSwEng1 = QuantLib::ext::make_shared<AnalyticLgmSwaptionEngine>(model);
+    boost::shared_ptr<IborIndex> EURIBOR6m = boost::make_shared<Euribor6M>(eurYtsHandle);
+    Schedule schedule(startDate, maturityDate, Period(Semiannual), calendar, Unadjusted, Unadjusted, DateGeneration::Backward, false);
+
+    double Annuity=0.0;
+
+    for (int i=2;i<10;++i)
+        Annuity += 0.5 * exp(-(double) i * 0.5 *  fixedRate);
+
+    // Starting 1% above market rate level of 2%
+    BOOST_TEST_MESSAGE("Checking Receiver Swaps ...");
+    for (double strike = 0.03; strike < 0.07; strike += 0.005)
+    {
+        boost::shared_ptr<VanillaSwap> swap = boost::make_shared<VanillaSwap>(VanillaSwap::Receiver, notional, schedule, strike, Actual365Fixed(), schedule, EURIBOR6m,  0.0, Actual365Fixed());        
+        boost::shared_ptr<Swaption> swaption = boost::make_shared<Swaption>(swap, exercise);
+
+        swaption->setPricingEngine(eurSwEng1);
+        Real npv = swaption->NPV();
+        double limitValue = Annuity * blackPutPrice(fixedRate, strike, vol, T, fixedRate);
+        BOOST_TEST_MESSAGE("Receiver Swaption (Strike = " << strike * 100.0 << "%): " << npv * 10000.00 << " bp. ");
+        BOOST_TEST_MESSAGE("Limit Value: " << limitValue * 10000.0 << " bp., " << "Annuity: " << Annuity);
+        BOOST_TEST_MESSAGE("------------");
+        BOOST_CHECK_CLOSE(npv, limitValue, 1.0); // Tolerance of 1%
+    }
+
+    // Starting 1% below market rate level of 2%
+    BOOST_TEST_MESSAGE("Checking Payer Swaps ...");
+    for (double strike = 0.01; strike > 0.0; strike -= 0.005) //TODO go lower
+    {
+        boost::shared_ptr<VanillaSwap> swap = boost::make_shared<VanillaSwap>(VanillaSwap::Payer, notional, schedule, strike, Actual365Fixed(), schedule, EURIBOR6m,  0.0, Actual365Fixed());
+        boost::shared_ptr<Swaption> swaption = boost::make_shared<Swaption>(swap, exercise);
+
+        swaption->setPricingEngine(eurSwEng1);
+        Real npv = swaption->NPV();
+        double limitValue = Annuity * blackCallPrice(fixedRate, strike, vol, T, fixedRate);
+        BOOST_TEST_MESSAGE("Payer Swaption (Strike = " << strike * 100.0 << "%): " << npv * 10000.00 << " bp. ");
+        BOOST_TEST_MESSAGE("Limit Value: " << limitValue * 10000.0 << " bp., " << "Annuity: " << Annuity);
+        BOOST_TEST_MESSAGE("------------");
+        BOOST_CHECK_CLOSE(npv, limitValue, 3.0); // Tolerance of 3%
+    }
+    
+    BOOST_TEST_MESSAGE(" T = 1: Model - "<< model->printParameters(1));    
+}
+
+/* 
 BOOST_AUTO_TEST_CASE(testLgmPricingVola) {
 
     BOOST_TEST_MESSAGE("Testing pricing with different volas ...");
@@ -316,7 +422,7 @@ BOOST_AUTO_TEST_CASE(testLgmPricingBachelier) {
     }
     
     BOOST_TEST_MESSAGE(" T = 1: Model - "<< model->printParameters(1));    
-}
+} */
 
 BOOST_AUTO_TEST_SUITE_END()
 
