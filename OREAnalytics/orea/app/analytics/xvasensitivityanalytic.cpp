8aec970ca7fe0bde1ebf628e9fbfd177765f1c86
Barker <damien.barker@lseg.com>
2025-02-01 23:32:24 +0000

    QPR-13022 fix xva sensi analytics

--- OREAnalytics/orea/app/analytics/xvasensitivityanalytic.cpp
+++ OREAnalytics/orea/app/analytics/xvasensitivityanalytic.cpp
@@ -338,7 +338,7 @@ void XvaSensitivityAnalyticImpl::computeXvaUnderScenarios(std::map<size_t, ext::
         try {
             DLOG("Calculate XVA for scenario " << label);
             CONSOLE("XVA_SENSITIVITY: Apply scenario " << label);
-            auto xvaAnalytic = AnalyticFactory::instance().build("XVA", inputs_, analytic()->analyticsManager()).second;
+            auto xvaAnalytic = AnalyticFactory::instance().build("XVA", inputs_, analytic()->analyticsManager(), false).second;
             XvaAnalyticImpl* xvaImpl = static_cast<XvaAnalyticImpl*>(xvaAnalytic->impl().get());
             xvaImpl->setOffsetScenario(scenario);
             xvaImpl->setOffsetSimMarketParams(simMarketParams);
