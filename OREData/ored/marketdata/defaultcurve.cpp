dfb26b35ffb14d9efa3ff5c855dd8f7077268408
Matthias Groncki <matthias.groncki@acadia.inc>
2025-02-04 13:15:29 +0100

    QPR-13083 QPR-13056 bugfix edge case in default curve building and accrual rebate calculation

--- OREData/ored/marketdata/defaultcurve.cpp
+++ OREData/ored/marketdata/defaultcurve.cpp
@@ -377,22 +377,26 @@ void DefaultCurve::buildCdsCurve(const std::string& curveID, const DefaultCurveC
             QuantLib::ext::shared_ptr<SpreadCdsHelper> tmp;
 
             try {
-
+                bool cdsDateRule = cdsConv->rule() == DateGeneration::CDS ||
+                                   cdsConv->rule() == DateGeneration::CDS2015 ||
+                                   cdsConv->rule() == DateGeneration::OldCDS;
+                if (cdsDateRule && cdsMaturity(asof, quote.term, cdsConv->rule()) <= asof) {
+                    WLOG("DefaultCurve:: SKIP cds with term "
+                         << quote.term << " cds maturity ("
+                         << io::iso_date(cdsMaturity(asof, quote.term, cdsConv->rule())) << ") is before T ("
+                         << io::iso_date(asof) << "), the first CDS payment is the next IMM payment date after T+1");
+                    continue;
+                };
                 tmp = QuantLib::ext::make_shared<SpreadCdsHelper>(
                     quote.value, quote.term, cdsConv->settlementDays(), cdsConv->calendar(), cdsConv->frequency(),
                     cdsConv->paymentConvention(), cdsConv->rule(), cdsConv->dayCounter(), recoveryRate_, discountCurve,
                     cdsConv->settlesAccrual(), ppt, config.startDate(), cdsConv->lastPeriodDayCounter());
 
             } catch (exception& e) {
-                bool cdsDateRule = cdsConv->rule() == DateGeneration::CDS || cdsConv->rule() == DateGeneration::CDS2015 ||
-                    cdsConv->rule() == DateGeneration::OldCDS;
+
                 if (quote.term == Period(0, Months)) {
                     WLOG("DefaultCurve:: Cannot add quote of term 0M to CDS curve " << curveID << " for asof date "
                                                                                     << asof);
-                } else if (cdsDateRule && cdsMaturity(asof, quote.term, cdsConv->rule()) >= (asof +1)) {
-                    WLOG("DefaultCurve:: SKIP cds with term " << quote.term << " cds maturity (" <<
-                        io::iso_date(cdsMaturity(asof, quote.term, cdsConv->rule())) <<
-                        ")at or before T + 1 ("<< io::iso_date(asof +1)<<")");
                 } else {
                     QL_FAIL("DefaultCurve:: Failed to add quote of term " << quote.term << " to CDS curve " << curveID
                                                                           << " for asof date " << asof
