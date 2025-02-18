610cbf146dbce042979da02e399bcfb55ab59225
cfdickmann <cfdickmann@googlemail.com>
2025-02-18 16:21:59 +0100

    QPR-12367: Add American Nonstandard Case

--- OREData/ored/portfolio/builders/swaption.hpp
+++ OREData/ored/portfolio/builders/swaption.hpp
@@ -69,7 +69,7 @@ private:
 class LGMSwaptionEngineBuilder : public SwaptionEngineBuilder {
 public:
     LGMSwaptionEngineBuilder(const string& engine)
-        : SwaptionEngineBuilder("LGM", engine, {"EuropeanSwaption", "BermudanSwaption", "BermudanSwaption_NonStandard", "AmericanSwaption"}) {}
+        : SwaptionEngineBuilder("LGM", engine, {"EuropeanSwaption", "BermudanSwaption", "BermudanSwaption_NonStandard", "AmericanSwaption", "AmericanSwaption_NonStandard"}) {}
 
 protected:
     QuantLib::ext::shared_ptr<QuantExt::LGM> model(const string& id, const string& key, const std::vector<Date>& dates,
