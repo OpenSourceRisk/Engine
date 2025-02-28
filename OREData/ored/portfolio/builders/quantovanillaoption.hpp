2ba0d5948ca4e83b07d11821863b451d9aef212e
Alex Solounias <alex.solounias@lseg.com>
2025-02-28 14:03:50 -0500

    QPR-13105 Next update in logic

--- OREData/ored/portfolio/builders/quantovanillaoption.hpp
+++ OREData/ored/portfolio/builders/quantovanillaoption.hpp
@@ -27,7 +27,7 @@
 #include <ored/portfolio/builders/vanillaoption.hpp>
 #include <qle/termstructures/flatcorrelation.hpp>
 #include <ql/pricingengines/quanto/quantoengine.hpp>
-#include <qle/methods/fdmquantohelper.hpp>
+#include <ql/methods/finitedifferences/utilities/fdmquantohelper.hpp>
 
 namespace ore {
 namespace data{
@@ -133,6 +133,10 @@ protected:
         // We follow the way FdBlackScholesBarrierEngine determines maturity for time grid generation
         Handle<YieldTermStructure> riskFreeRate =
             market_->discountCurve(underlyingCcy.code(), configuration(ore::data::MarketContext::pricing));
+
+        Handle<YieldTermStructure> payCcyRate =
+            market_->discountCurve(payCcy.code(), configuration(ore::data::MarketContext::pricing));
+
         Time expiry = riskFreeRate->dayCounter().yearFraction(riskFreeRate->referenceDate(),
                                                               std::max(riskFreeRate->referenceDate(), expiryDate));
 
@@ -145,13 +149,6 @@ protected:
         tGrid = std::max(tGridMin, tGrid);
 
         QuantLib::ext::shared_ptr<QuantLib::GeneralizedBlackScholesProcess> gbsp;
-
-        Real quantoCorr = quantoCorrelationMultiplier_ * getCorrelation()[0][1];
-        QuantLib::ext::shared_ptr<QuantExt::FdmQuantoHelper> quantoHelper;
-        quantoHelper = QuantLib::ext::make_shared<QuantExt::FdmQuantoHelper>(
-            *curves_[quantoTargetCcyIndex_], *curves_[quantoSourceCcyIndex_],
-            *model_->processes()[1]->blackVolatility(), quantoCorr, Null<Real>(), model_->processes()[1]->x0(), false,
-            true);
 
         if (monotoneVar) {
             // Replicate the construction of time grid in FiniteDifferenceModel::rollbackImpl
@@ -168,8 +165,52 @@ protected:
         } else {
             gbsp = getBlackScholesProcess(assetName, underlyingCcy, assetClassUnderlying);
         }
-        // Target:  return QuantLib::ext::make_shared<FdBlackScholesVanillaEngine>(gbsp, quantoHelper, tGrid, xGrid, dampingSteps, scheme);
-        return QuantLib::ext::make_shared<FdBlackScholesVanillaEngine>(gbsp, tGrid, xGrid, dampingSteps, scheme);
+
+        Handle<BlackVolTermStructure> fxVolatility =
+            market_->fxVol(underlyingCcy.code() + payCcy.code(), configuration(MarketContext::pricing));
+
+        std::string fxSource = engineParameter("FXSource");
+        std::string fxIndex = "FX-" + fxSource + "-" + underlyingCcy.code() + "-" + payCcy.code();
+        std::string underlyingIndex;
+        switch (assetClassUnderlying) {
+        case AssetClass::EQ:
+            underlyingIndex = "EQ-" + assetName;
+            break;
+        case AssetClass::COM:
+            underlyingIndex = "COMM-" + assetName;
+            break;
+        default:
+            QL_FAIL("Asset class " << assetClassUnderlying << " not supported for quanto vanilla option.");
+        }
+
+        QuantLib::Handle<QuantExt::CorrelationTermStructure> quantoCorr(
+            QuantLib::ext::make_shared<QuantExt::FlatCorrelation>(0, NullCalendar(), 0.0, Actual365Fixed()));
+        try {
+            quantoCorr = market_->correlationCurve(fxIndex, underlyingIndex, configuration(MarketContext::pricing));
+        } catch (...) {
+            WLOG("no correlation curve for " << fxIndex << ", " << underlyingIndex
+                                                << " found, fall back to zero correlation");
+        }
+
+        Handle<Quote> fxSpot =
+            market_->fxSpot(underlyingCcy.code() + payCcy.code(), configuration(MarketContext::pricing));
+        Real fxStrike = fxSpot->value() * riskFreeRate->discount(expiryDate) / payCcyRate->discount(expiryDate);
+
+        QuantLib::ext::shared_ptr<FdmQuantoHelper> quantoHelper;
+
+        quantoHelper = QuantLib::ext::make_shared<FdmQuantoHelper>(
+            *payCcyRate, *riskFreeRate, 
+            *gbsp->blackVolatility(), 
+            quantoCorr->correlation(expiryDate), 
+            fxStrike);
+        
+        // return approach like above but fails
+        // return QuantLib::ext::make_shared<QuantLib::QuantoEngine<QuantLib::VanillaOption,
+        // FdBlackScholesVanillaEngine>>(
+        //            gbsp, quantoHelper, tGrid, xGrid, dampingSteps, scheme);
+        
+        return QuantLib::ext::make_shared<QuantLib::FdBlackScholesVanillaEngine>(gbsp, quantoHelper, tGrid, xGrid,
+                                                                               dampingSteps, scheme);
     }
 };
 
