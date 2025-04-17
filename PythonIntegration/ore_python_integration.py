44f8e30709fcc241a66fb8a2f17e9bb3296ff63e
Peter Caspers <peter.caspers@lseg.com>
2025-04-17 13:14:27 +0200

    QPR-12228 whitespace

--- PythonIntegration/ore_python_integration.py
+++ PythonIntegration/ore_python_integration.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures  
+from sklearn.preprocessing import PolynomialFeatures
 
 def conditional_expectation(x,y):
     X = np.array(x)
     Y = np.array(y)
-    poly = PolynomialFeatures(degree=4)  
+    poly = PolynomialFeatures(degree=4)
     X_pre = poly.fit_transform(X)
     model = LinearRegression()
     model.fit(X_pre, Y)
     return model.predict(X_pre).tolist()
-
