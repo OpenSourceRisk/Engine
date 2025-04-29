cf7c3d45aaecc9cac9db8af9f57aff23e88a3c66
chris <christopher.smith@acadia.inc>
2025-04-29 15:45:53 +0200

    Refactor FX Digital Option, FX European Barrier Option
    FX Digital Option previously has no capacity to handle payment dates, changed this to use either:
     - CashSettledEuropeanOption if no payment date
     - VanillaForwardOption if payment date provided
    
    FX European Barrier Option replication process made no distinction between physical and cash settled delivery. Updated to use:
     - CashSettledEuropeanOption for FX Calls, and VanillaForwardOption for the digital and rebate, if payment date provided AND cash settled
     - VanillaForwardOption for FX Calls, digital and rebate if payment date provided AND physically settled
     - VanillaOption for FX calls, digital and rebate if no payment date provided

--- OREData/ored/portfolio/fxdigitaloption.cpp
+++ OREData/ored/portfolio/fxdigitaloption.cpp
@@ -25,8 +25,10 @@
 #include <ql/instruments/compositeinstrument.hpp>
 #include <ql/instruments/payoffs.hpp>
 #include <ql/instruments/vanillaoption.hpp>
+#include <qle/instruments/cashsettledeuropeanoption.hpp>
 
 using namespace QuantLib;
+using namespace QuantExt;
 
 namespace ore {
 namespace data {
@@ -81,33 +83,83 @@ void FxDigitalOption::build(const QuantLib::ext::shared_ptr<EngineFactory>& engi
     // Exercise
     Date expiryDate = parseDate(option_.exerciseDates().front());
     QuantLib::ext::shared_ptr<Exercise> exercise = QuantLib::ext::make_shared<EuropeanExercise>(expiryDate);
-    maturity_ = std::max(option_.premiumData().latestPremiumDate(), expiryDate);
+    
+    Date paymentDate = expiryDate;
+    const boost::optional<OptionPaymentData>& opd = option_.paymentData();
+    
+    if (opd) {
+        if (opd->rulesBased()) {
+            const Calendar& cal = opd->calendar();
+            QL_REQUIRE(cal != Calendar(), "Need a non-empty calendar for rules based payment date.");
+            paymentDate = cal.advance(expiryDate, opd->lag(), Days, opd->convention());
+        } else {
+            const vector<Date>& dates = opd->dates();
+            QL_REQUIRE(dates.size() == 1, "Need exactly one payment date for cash settled European option.");
+            paymentDate = dates[0];
+        }
+        QL_REQUIRE(paymentDate >= expiryDate, "Payment date must be greater than or equal to expiry date.");
+    }
+    maturity_ = std::max(option_.premiumData().latestPremiumDate(), paymentDate);
     maturityType_ = maturity_ == expiryDate ? "Expiry Date" : "Option's Latest Premium Date";
+    QuantLib::ext::shared_ptr<Instrument> vanilla;
+    Real exercisePrice = Null<Real>();
+    bool exercised = false;
+    QuantLib::ext::shared_ptr<FxIndex> fxIndex;
+    if (paymentDate == expiryDate) {
+        const boost::optional<OptionExerciseData>& oed = option_.exerciseData();
+        if (oed) {
+            QL_REQUIRE(oed->date() == expiryDate, "The supplied exercise date ("
+                                                        << io::iso_date(oed->date())
+                                                        << ") should equal the option's expiry date ("
+                                                        << io::iso_date(expiryDate) << ").");
+            exercised = true;
+            exercisePrice = oed->price();
+        }
+        if (option_.isAutomaticExercise()) {
 
-    // QL does not have an FXDigitalOption, so we add a vanilla one here and wrap
-    // it in a composite.
-    QuantLib::ext::shared_ptr<Instrument> vanilla = QuantLib::ext::make_shared<VanillaOption>(payoff, exercise);
+            fxIndex = buildFxIndex(fxIndex_, domCcy.code(), forCcy.code(), engineFactory->market(),
+                                    engineFactory->configuration(MarketContext::pricing));
+            requiredFixings_.addFixingDate(expiryDate, fxIndex_, paymentDate);
+        }
 
-    // set pricing engines
-    QuantLib::ext::shared_ptr<EngineBuilder> builder = engineFactory->builder(tradeType_);
-    QL_REQUIRE(builder, "No builder found for " << tradeType_);
-    QuantLib::ext::shared_ptr<FxDigitalOptionEngineBuilder> fxOptBuilder =
-        QuantLib::ext::dynamic_pointer_cast<FxDigitalOptionEngineBuilder>(builder);
-    vanilla->setPricingEngine(fxOptBuilder->engine(forCcy, domCcy, flipResults));
-    setSensitivityTemplate(*fxOptBuilder);
-    addProductModelEngine(*fxOptBuilder);
-
-    Position::Type positionType = parsePositionType(option_.longShort());
-    Real bsInd = (positionType == QuantLib::Position::Long ? 1.0 : -1.0);
-    Real mult = bsInd;
-
-    std::vector<QuantLib::ext::shared_ptr<Instrument>> additionalInstruments;
-    std::vector<Real> additionalMultipliers;
-    addPremiums(additionalInstruments, additionalMultipliers, mult, option_.premiumData(), -bsInd, domCcy,
-                engineFactory, fxOptBuilder->configuration(MarketContext::pricing));
-
-    instrument_ = QuantLib::ext::shared_ptr<InstrumentWrapper>(
-        new VanillaInstrument(vanilla, mult, additionalInstruments, additionalMultipliers));
+        vanilla = QuantLib::ext::make_shared<CashSettledEuropeanOption>(type, strike, payoffAmount_, expiryDate,
+                                                                        paymentDate, option_.isAutomaticExercise(),
+                                                                        fxIndex, exercised, exercisePrice);
+        // set pricing engines
+        QuantLib::ext::shared_ptr<EngineBuilder> builder = engineFactory->builder("FxDigitalOptionEuropeanCS");
+        QL_REQUIRE(builder, "No builder found for " << tradeType_);
+        QuantLib::ext::shared_ptr<FxDigitalCSOptionEngineBuilder> fxOptBuilder =
+            QuantLib::ext::dynamic_pointer_cast<FxDigitalCSOptionEngineBuilder>(builder);
+        vanilla->setPricingEngine(fxOptBuilder->engine(forCcy, domCcy));
+        setSensitivityTemplate(*fxOptBuilder);
+        addProductModelEngine(*fxOptBuilder);
+        Position::Type positionType = parsePositionType(option_.longShort());
+        Real bsInd = (positionType == QuantLib::Position::Long ? 1.0 : -1.0);
+        Real mult = bsInd;
+        std::vector<QuantLib::ext::shared_ptr<Instrument>> additionalInstruments;
+        std::vector<Real> additionalMultipliers;
+        addPremiums(additionalInstruments, additionalMultipliers, mult, option_.premiumData(), -bsInd, domCcy,
+                    engineFactory, fxOptBuilder->configuration(MarketContext::pricing));
+        instrument_ = QuantLib::ext::shared_ptr<InstrumentWrapper>(
+            new VanillaInstrument(vanilla, mult, additionalInstruments, additionalMultipliers));
+    } else {
+        QuantLib::ext::shared_ptr<EngineBuilder> builder = engineFactory->builder("FxOptionForward");
+        vanilla = QuantLib::ext::make_shared<QuantExt::VanillaForwardOption>(payoff, exercise, paymentDate, paymentDate);
+        QuantLib::ext::shared_ptr<VanillaOptionEngineBuilder> fxOptBuilder =
+            QuantLib::ext::dynamic_pointer_cast<VanillaOptionEngineBuilder>(builder);
+        vanilla->setPricingEngine(fxOptBuilder->engine(forCcy, domCcy, paymentDate));
+        setSensitivityTemplate(*fxOptBuilder);
+        addProductModelEngine(*fxOptBuilder);
+        Position::Type positionType = parsePositionType(option_.longShort());
+        Real bsInd = (positionType == QuantLib::Position::Long ? 1.0 : -1.0);
+        Real mult = bsInd;
+        std::vector<QuantLib::ext::shared_ptr<Instrument>> additionalInstruments;
+        std::vector<Real> additionalMultipliers;
+        addPremiums(additionalInstruments, additionalMultipliers, mult, option_.premiumData(), -bsInd, domCcy,
+                    engineFactory, fxOptBuilder->configuration(MarketContext::pricing));
+        instrument_ = QuantLib::ext::shared_ptr<InstrumentWrapper>(
+            new VanillaInstrument(vanilla, mult, additionalInstruments, additionalMultipliers));
+    }
 }
 
 void FxDigitalOption::fromXML(XMLNode* node) {
@@ -126,7 +178,6 @@ XMLNode* FxDigitalOption::toXML(XMLDocument& doc) const {
     XMLNode* node = Trade::toXML(doc);
     XMLNode* fxNode = doc.allocNode("FxDigitalOptionData");
     XMLUtils::appendNode(node, fxNode);
-
     XMLUtils::appendNode(fxNode, option_.toXML(doc));
     XMLUtils::addChild(doc, fxNode, "Strike", strike_);
     XMLUtils::addChild(doc, fxNode, "PayoffCurrency", payoffCurrency_);
