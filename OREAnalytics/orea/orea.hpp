302376f30d2b144eedb758b35d5d59fd753cd470
Peter Caspers <peter.caspers@lseg.com>
2025-05-23 18:24:16 +0200

    cleanup

--- OREAnalytics/orea/orea.hpp
+++ OREAnalytics/orea/orea.hpp
@@ -148,7 +148,6 @@
 #include <orea/scenario/deltascenariofactory.hpp>
 #include <orea/scenario/historicalscenariogenerator.hpp>
 #include <orea/scenario/lgmscenariogenerator.hpp>
-#include <orea/scenario/scenario.hpp>
 #include <orea/scenario/scenariofactory.hpp>
 #include <orea/scenario/scenariofilereader.hpp>
 #include <orea/scenario/scenariofilter.hpp>
