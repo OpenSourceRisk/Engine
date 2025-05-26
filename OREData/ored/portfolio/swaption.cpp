0d90004acc1fd4e1476968112b969dc46338d6ec
cfdickmann <cfdickmann@googlemail.com>
2025-05-26 22:23:42 +0200

    QPR-12824: Parameter Parsing

--- OREData/ored/portfolio/swaption.cpp
+++ OREData/ored/portfolio/swaption.cpp
@@ -424,7 +424,7 @@ void Swaption::build(const QuantLib::ext::shared_ptr<EngineFactory>& engineFacto
     // 9.4 get engine and set it
     cpu_timer timer;
 
-    auto calibrationStrategy = parseCalibrationStrategy(swaptionBuilder->modelParameter("CalibrationStrategy"));
+    auto calibrationStrategy = parseCalibrationStrategy(swaptionBuilder->modelParameter("CalibrationStrategy", {}, false));
     QuantLib::ext::shared_ptr<PricingEngine> swaptionEngine; 
     std::vector<Date> maturitiesEngine;
     std::vector<Rate> strikesEngine;
