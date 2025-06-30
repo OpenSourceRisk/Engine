c0a4c96b8776d219bc982c82f96f56a42bd1731b
Bouvard <sebastien.bouvard@lseg.com>
2025-06-30 14:16:25 +0100

    QPR-13370 Strict Comparison for accumulator, KOSwap, ad to scriptlibrary

--- OREData/ored/portfolio/knockoutswap.cpp
+++ OREData/ored/portfolio/knockoutswap.cpp
@@ -125,6 +125,7 @@ void KnockOutSwap::build(const QuantLib::ext::shared_ptr<EngineFactory>& factory
     QL_REQUIRE(barrierData_.levels().size() == 1, "Expected exactly one barrier level");
     QL_REQUIRE(barrierData_.levels().front().value() != Null<Real>(), "No barrier level specified.");
 
+    numbers_.emplace_back("Number", "BarrierStrictComparison", barrierData_.strictComparison().value());
     numbers_.emplace_back("Number", "KnockOutLevel", std::to_string(barrierData_.levels().front().value()));
 
     events_.emplace_back("BarrierStartDate", barrierStartDate_);
@@ -174,12 +175,22 @@ void KnockOutSwap::build(const QuantLib::ext::shared_ptr<EngineFactory>& factory
 
       "   IF d < SIZE(FloatFixingSchedule) THEN\n"
       "     fix = FloatIndex(FloatFixingSchedule[d]);\n"
-      "     IF FloatFixingSchedule[d] >= BarrierStartDate AND\n"
-      "        {{KnockOutType == 3 AND fix <= KnockOutLevel} OR\n"
-      "         {KnockOutType == 4 AND fix >= KnockOutLevel}} THEN\n"
-      "       aliveInd = 0;\n"
+      "     IF {BarrierStrictComparison == 0} THEN\n"
+      "        IF FloatFixingSchedule[d] >= BarrierStartDate AND\n"
+      "          {{KnockOutType == 3 AND fix <= KnockOutLevel} OR\n"
+      "          {KnockOutType == 4 AND fix >= KnockOutLevel}} THEN\n"
+      "          aliveInd = 0;\n"
+      "        END;\n"
+      "        Alive[d] = aliveInd;\n"
       "     END;\n"
-      "     Alive[d] = aliveInd;\n"
+      "     IF {BarrierStrictComparison == 1} THEN\n"
+      "        IF FloatFixingSchedule[d] >= BarrierStartDate AND\n"
+      "          {{KnockOutType == 3 AND fix < KnockOutLevel} OR\n"
+      "          {KnockOutType == 4 AND fix > KnockOutLevel}} THEN\n"
+      "          aliveInd = 0;\n"
+      "        END;\n"
+      "        Alive[d] = aliveInd;\n"
+      "     END;\n"
       "   END;\n"
 
       "END;\n");
