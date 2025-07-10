cb7bc94c4028fd1b58d956f9a9ab294c4ddc4198
Peter Caspers <peter.caspers@lseg.com>
2025-07-10 15:00:41 +0200

    QPR-12753 introduce interface for forward bond calculation

---
+++ QuantExt/qle/pricingengines/forwardenabledbondengine.hpp
@@ -0,0 +1,30 @@
+/*
+ Copyright (C) 2025 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
+#pragma once
+
+#include <ql/qldefines.hpp>
+#include <ql/time/date.hpp>
+
+namespace QuantExt {
+
+struct ForwardEnabledBondEngine {
+    QuantLib::Real forwardNpv(const QuantLib::Date& forwardDate) const = 0;
+};
+
+} // namespace QuantExt
