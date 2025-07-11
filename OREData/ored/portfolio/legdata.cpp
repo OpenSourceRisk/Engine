f3e8580e91fc2d714e71832624832504dc834c8d
Peter Caspers <peter.caspers@lseg.com>
2025-07-11 17:15:00 +0200

    QPR-13395 use default params

--- OREData/ored/portfolio/legdata.cpp
+++ OREData/ored/portfolio/legdata.cpp
@@ -1390,10 +1390,7 @@ Leg makeIborLeg(const LegData& data, const QuantLib::ext::shared_ptr<IborIndex>&
         if (productModelEngines)
             productModelEngines->insert(std::make_tuple(builder->tradeTypes(), builder->model(), builder->engine()));
     } else {
-        setCouponPricer(tmpLeg,
-                        QuantLib::ext::make_shared<BlackIborCouponPricer>(
-                            Handle<OptionletVolatilityStructure>(), BlackIborCouponPricer::TimingAdjustment::Black76,
-                            Handle<Quote>(ext::make_shared<SimpleQuote>(1.0))));
+        setCouponPricer(tmpLeg, QuantLib::ext::make_shared<BlackIborCouponPricer>());
     }
 
     // build naked option leg if required
