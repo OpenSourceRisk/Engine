a6d868376a29c758948b4af4e368ae8a1ef295a3
Acar <sarp.kaya.acar@acadia.inc>
2025-07-15 10:24:18 +0100

    QPR-13435 fixing regression tests

--- QuantExt/qle/pricingengines/analyticcashsettledeuropeanengine.cpp
+++ QuantExt/qle/pricingengines/analyticcashsettledeuropeanengine.cpp
@@ -195,12 +195,12 @@ void AnalyticCashSettledEuropeanEngine::calculate() const {
         cfResults.back().payDate = arguments_.paymentDate;
         cfResults.back().legNumber = 0;
         cfResults.back().type = "ExpectedFlow";
-        cfResults.back().fixingDate = fixingDate;
-        cfResults.back().fixingValue = fxRate;
-
+        if (fixingDate != Date()){
+            cfResults.back().fixingDate = fixingDate;
+            cfResults.back().fixingValue = fxRate;
+        }
         results_.additionalResults["cashFlowResults"] = cfResults;
         results_.additionalResults["discountFactorTeTp"] = df_te_tp;
-        results_.additionalResults["settlementFxFwdDate"] = fixingDate;
         results_.additionalResults["settlementFxFwd"] = fxRate;
     }
 
