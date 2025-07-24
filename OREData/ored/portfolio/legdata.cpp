e53c721ef8a6a86c473b2c313296d75c528c6643
Bouvard <sebastien.bouvard@lseg.com>
2025-07-24 21:44:03 +0100

    QPR-13456 Fix toXml to match xsd

--- OREData/ored/portfolio/legdata.cpp
+++ OREData/ored/portfolio/legdata.cpp
@@ -923,9 +923,8 @@ XMLNode* LegData::toXML(XMLDocument& doc) const {
         XMLNode* resetNode = doc.allocNode("FXReset");
         if (!resetStartDate_.empty()) {
             XMLUtils::addChild(doc, resetNode, "StartDate", resetStartDate_);
-        } else {
-            XMLUtils::addChild(doc, resetNode, "ForeignCurrency", foreignCurrency_);
         }
+        XMLUtils::addChild(doc, resetNode, "ForeignCurrency", foreignCurrency_);
         XMLUtils::addChild(doc, resetNode, "ForeignAmount", foreignAmount_);
         XMLUtils::addChild(doc, resetNode, "FXIndex", fxIndex_);
         XMLUtils::appendNode(notionalsNodePtr, resetNode);
