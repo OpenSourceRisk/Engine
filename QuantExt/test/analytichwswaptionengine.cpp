7e242b75b7daa597c6d70b1a4727300cc86f1b3e
Oriol Ripalta <oriol.ripaltaimaso@lseg.com>
2025-07-28 12:53:01 -0400

    add manual swaption price calc

--- QuantExt/test/analytichwswaptionengine.cpp
+++ QuantExt/test/analytichwswaptionengine.cpp
@@ -127,7 +127,7 @@ BOOST_AUTO_TEST_CASE(testPrintProcess){
     // Swap params
     Real maturity = 1.0;
     Real tenor = 5.0;
-    Real strike = 0.025;
+    Real strike = 0.02;
     // Simulation params
     Size nPaths = 1;
     Size nSteps = 100;
@@ -187,12 +187,317 @@ BOOST_AUTO_TEST_CASE(testPrintProcess){
     std::cout << "The expected payoff is: " << expectedPayoff << endl;
 };
 
+BOOST_AUTO_TEST_CASE(testAlternativeMC) {
+    Date today(10, July, 2025);
+    Settings::instance().evaluationDate() = today;
+
+    Array kappa(1, 0.01);
+    Matrix sigma(1, 1, 0.01);
+    Real strike = 0.02;
+    
+    //Handle<YieldTermStructure> ts(ext::make_shared<FlatForward>(today, 0.02, Actual365Fixed())); 
+    Handle<YieldTermStructure> ts(QuantLib::ext::make_shared<FlatForward>(0, NullCalendar(), 0.02, Actual365Fixed()));
+
+    auto params = ext::make_shared<IrHwConstantParametrization>(EURCurrency(), ts, sigma, kappa);
+
+    //ext::shared_ptr<HwModel> model = ext::make_shared<HwModel>(params);
+    ext::shared_ptr<HwModel> model =
+        ext::make_shared<HwModel>(params, IrModel::Measure::BA, HwModel::Discretization::Euler, false);
+
+
+    // Option setup
+    auto index = ext::make_shared<EuriborSwapIsdaFixA>(5 * Years, ts);
+    Swaption swap = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
+    Swaption swpLgm = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
+
+    // MC settings
+    Size paths = 10000;
+    Time optionTime = 2.0; // two years of option
+    // NOTE: most accurate result takes place when dt = 0.004
+    Time dt = 0.05; // equals to 20 steps x year (maturity / dt = steps)
+    Size steps = optionTime / dt;
+
+    mt19937_64 rng(42);
+    normal_distribution<> norm(0.0, 1.0);
+
+    Real sumPayoffs = 0.0;
+
+    // Pre-compute swap payments
+    const auto& fixedLeg = swap.underlying()->fixedLeg();
+    const auto& schedule = swap.underlying()->fixedSchedule();
+    vector<Time> payTimes;
+    vector<Real> accruals; // deltas (time between payments)
+    for (auto const& cf : fixedLeg) {
+        auto cpn = ext::dynamic_pointer_cast<FixedRateCoupon>(cf);
+        if (!cpn) continue;
+        payTimes.push_back(ts->timeFromReference(cpn->date()));
+        accruals.push_back(cpn->accrualPeriod());
+    }
+
+    Time T0 = ts->timeFromReference(schedule.startDate());
+    Time TN = ts->timeFromReference(schedule.endDate());
+    Real notional = swap.underlying()->nominal();
+    auto process = model->stateProcess();
+    //Size nDim = process->size();
+    Size nFactors = process->factors();
+    cout << "Start date (T0): " << schedule.startDate() << endl;
+    cout << "End date (TN): " << schedule.endDate() << endl;
+
+    std::ofstream csv("paths.csv");
+    csv << "path,t,x0\n";
+
+    for (Size p = 0; p < paths; ++p) {
+        Array state = process->initialValues();
+        // print initial state
+        if (p == 0) {
+            cout << "Array size: " << state.size() << endl;
+            cout << "Array: " << state << endl;
+        }
+        Time t = 0.0;
+        for (Size step = 0; step < steps; ++step) {
+            Array dw(nFactors);
+            for (Size k = 0; k < nFactors; ++k) {
+                dw[k] = sqrt(dt) * norm(rng);
+            }
+            state = process->evolve(t, state, dt, dw);
+            t += dt;
+            if (p == 0) {
+                csv << p << "," << t << "," << state[0] << "\n";
+            }
+        }
+
+        // Compute swap PV at option expiry
+        // Floating leg PV: P(t, T0) - P(t, TN)
+        Real P_t_T0 = model->discountBond(optionTime, T0, state, ts);
+        Real P_t_TN = model->discountBond(optionTime, TN, state, ts);
+        Real floatingPV = notional * (P_t_T0 - P_t_TN);
+        // I belive the floatingPV should be aprox. 1
+        if (p == 0) {
+            cout << "floatingPV: " << floatingPV << endl;
+        }
+
+        // Compute fixed leg PV
+        Real fixedPv = 0.0;
+        for (Size i = 0; i < payTimes.size(); ++i) {
+            if (payTimes[i] <= optionTime)
+                continue;
+            Real P_t_Ti = model->discountBond(optionTime, payTimes[i], state, ts);
+            fixedPv += notional * strike * accruals[i] * P_t_Ti;
+        }
+
+        Real swapPV = floatingPV - fixedPv;
+        Real payoff = max(swapPV, 0.0);
+        Real num = model->numeraire(optionTime, state, ts);
+        Real discountedPayoff = payoff / num;
+        sumPayoffs += discountedPayoff;
+    }
+    csv.close();
+
+
+    Real mcPrice = sumPayoffs / static_cast<Real>(paths);
+    cout << "MC price: " << mcPrice << endl;
+
+    // Quickly compare against the analytical engine
+    auto analyticEngine = ext::make_shared<AnalyticHwSwaptionEngine>(swap, model);
+    swap.setPricingEngine(analyticEngine);
+    Real analyticPrice = swap.NPV();
+    cout << "Analytic price: " << analyticPrice << endl;
+    Array sigmaDates;
+    Array kappaDates;
+    Array sigmaLgm = {0.01}; // LGM model takes a array sigma (not a matrix)
+
+    auto hwAdaptor = QuantLib::ext::make_shared<IrLgm1fPiecewiseConstantHullWhiteAdaptor>(EURCurrency(), ts, sigmaDates,
+                                                                                          sigmaLgm, kappaDates, kappa);
+    QuantLib::ext::shared_ptr<PricingEngine> lgmEngine =
+        QuantLib::ext::make_shared<AnalyticLgmSwaptionEngine>(hwAdaptor);
+    swpLgm.setPricingEngine(lgmEngine);
+    Real lgmPrice = swpLgm.NPV();
+    cout << "LGM adaptor price: " << lgmPrice << endl;
+}
+
+double get_df(const vector<double>& path, int t0, int t1, Time dt){
+    return exp(-accumulate(path.begin() + t0, path.begin() + t1, 0.0) * dt);
+}
+
+BOOST_AUTO_TEST_CASE(testFullPath) {
+    Date today(10, July, 2025);
+    Settings::instance().evaluationDate() = today;
+
+    Array kappa(1, 0.01);
+    Matrix sigma(1, 1, 0.01);
+    Real strike = 0.02;
+
+    // Handle<YieldTermStructure> ts(ext::make_shared<FlatForward>(today, 0.02, Actual365Fixed()));
+    Handle<YieldTermStructure> ts(QuantLib::ext::make_shared<FlatForward>(0, NullCalendar(), 0.02, Actual365Fixed()));
+
+    auto params = ext::make_shared<IrHwConstantParametrization>(EURCurrency(), ts, sigma, kappa);
+
+    // ext::shared_ptr<HwModel> model = ext::make_shared<HwModel>(params);
+    ext::shared_ptr<HwModel> model =
+        ext::make_shared<HwModel>(params, IrModel::Measure::BA, HwModel::Discretization::Euler, false);
+
+    // Option setup
+    auto index = ext::make_shared<EuriborSwapIsdaFixA>(5 * Years, ts);
+    Swaption swap = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
+    Swaption swpLgm = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
+
+    // MC settings
+    Size paths = 10000;
+    Time optionTime = 2; // two years of option
+    Time maturityTime = 7;
+    // NOTE: most accurate result takes place when dt = 0.004
+    Time dt = 0.005; // equals to 20 steps x year (maturity / dt = steps)
+    Size steps = maturityTime / dt;
+    Real delta = 1.0;
+
+    mt19937_64 rng(42);
+    normal_distribution<> norm(0.0, 1.0);
+
+    using Path = vector<double>;
+    using PathSet = vector<Path>;
+    PathSet pathSet;
+    pathSet.reserve(paths);
+
+    Real sumPayoffs = 0.0;
+    Real sumAltPayoffs = 0.0;
+
+    // Pre-compute swap payments
+    const auto& fixedLeg = swap.underlying()->fixedLeg();
+    const auto& schedule = swap.underlying()->fixedSchedule();
+    vector<Time> payTimes;
+    vector<Real> accruals; // deltas (time between payments)
+    for (auto const& cf : fixedLeg) {
+        auto cpn = ext::dynamic_pointer_cast<FixedRateCoupon>(cf);
+        if (!cpn)
+            continue;
+        payTimes.push_back(ts->timeFromReference(cpn->date()));
+        accruals.push_back(cpn->accrualPeriod());
+    }
+
+    //Time T0 = ts->timeFromReference(schedule.startDate());
+    //Time TN = ts->timeFromReference(schedule.endDate());
+    //Real notional = swap.underlying()->nominal();
+    auto process = model->stateProcess();
+    // Size nDim = process->size();
+    Size nFactors = process->factors();
+    cout << "Start date (T0): " << schedule.startDate() << endl;
+    cout << "End date (TN): " << schedule.endDate() << endl;
+
+
+    for (Size p = 0; p < paths; ++p) {
+        Path singlePath;
+        singlePath.reserve(steps + 1);
+        Array state = process->initialValues();
+        singlePath.push_back(state[0]);
+        // print initial state
+        if (p == 0) {
+            cout << "Array size: " << state.size() << endl;
+            cout << "Array: " << state << endl;
+        }
+        Time t = 0.0;
+        for (Size step = 0; step < steps; ++step) {
+            Array dw(nFactors);
+            for (Size k = 0; k < nFactors; ++k) {
+                dw[k] = sqrt(dt) * norm(rng);
+            }
+            state = process->evolve(t, state, dt, dw);
+            t += dt;
+            singlePath.push_back(state[0]);
+        }
+        pathSet.push_back(move(singlePath));
+    }
+
+    // Compute swap value
+    vector<Time> paymentDates;
+    for (Time t = optionTime + 1; t <= maturityTime; t += 1) {
+        paymentDates.push_back(t);
+        cout << t << endl;
+    }
+    cout << paymentDates.size() << endl;
+    int idxExp = static_cast<int>(optionTime / dt);
+    vector<int> idxPay;
+    for (Time t : paymentDates) {
+        idxPay.push_back(static_cast<int>(t / dt));
+        cout << static_cast<int>(t / dt) << endl;
+    }
+
+    int counter = 0;
+    for (const auto& path : pathSet) {
+        //double df_0_Tepx = exp(-accumulate(path.begin(), path.begin() + idxExp, 0.0) * dt);
+        vector<double> dfs;
+        double SumPTs = 0.0;
+        //double swapVal = 0.0;
+        for (int idxTi : idxPay) {
+            double df_T_Ti = exp(-accumulate(path.begin() + idxExp, path.begin() + idxTi, 0.0) * dt);
+            //double df_T_Ti = get_df(path, idxExp, idxTi, dt);
+            dfs.push_back(df_T_Ti);
+            SumPTs += delta * df_T_Ti;
+        }
+
+        double P_T_T0 = dfs.front();
+        double P_T_TN = dfs.back();
+        //double SwapRate = (P_T_T0 - P_T_TN) / SumPTs;
+   
+        double fixedPV = SumPTs * strike;
+        double floatPV = 1 - P_T_TN;
+        double SwapVal = floatPV - fixedPV;
+        Real payoff = max(SwapVal, 0.0);
+        if (counter < 100 && payoff > 0) {
+            cout << "Positive Payoff" << endl;
+        }
+        Real df_0_T0 = get_df(path, 0, idxExp, dt);
+
+        sumPayoffs += (payoff * df_0_T0);
+
+        if (counter == 0) {
+            cout << "Printing in priceing" << endl;
+            cout << "P_T_T0: " << P_T_T0 << endl;
+            cout << "P_T_TN: " << P_T_TN << endl;
+            cout << "fixedPV: " << fixedPV << endl;
+            cout << "SumPTs: " << SumPTs << endl;
+            cout << "df_0_T0: " << df_0_T0 << endl;
+        }
+
+        Real altSwapVal = 1 - fixedPV;
+        Real altPayoff = max(altSwapVal, 0.0);
+        sumAltPayoffs += (altPayoff * df_0_T0);
+        counter += 1;
+    }
+
+    Real mcPrice = sumPayoffs / static_cast<Real>(paths);
+    Real altMcPrice = sumAltPayoffs / static_cast<Real>(paths);
+    cout << "MC price: " << mcPrice << endl;
+    cout << "MC Alt price: " << altMcPrice << endl;
+
+    // Quickly compare against the analytical engine
+    auto analyticEngine = ext::make_shared<AnalyticHwSwaptionEngine>(swap, model);
+    swap.setPricingEngine(analyticEngine);
+    Real analyticPrice = swap.NPV();
+    cout << "Analytic price: " << analyticPrice << endl;
+    Array sigmaDates;
+    Array kappaDates;
+    Array sigmaLgm = {0.01}; // LGM model takes a array sigma (not a matrix)
+
+    auto hwAdaptor = QuantLib::ext::make_shared<IrLgm1fPiecewiseConstantHullWhiteAdaptor>(EURCurrency(), ts, sigmaDates,
+                                                                                          sigmaLgm, kappaDates, kappa);
+    QuantLib::ext::shared_ptr<PricingEngine> lgmEngine =
+        QuantLib::ext::make_shared<AnalyticLgmSwaptionEngine>(hwAdaptor);
+    swpLgm.setPricingEngine(lgmEngine);
+    Real lgmPrice = swpLgm.NPV();
+    cout << "LGM adaptor price: " << lgmPrice << endl;
+    cout << pathSet.size() << endl;
+    cout << pathSet[0].size() << endl;
+    cout << pathSet[0][0] << endl;
+}
+
 BOOST_AUTO_TEST_CASE(testAnalyticalvsSimulation) {
     Date today(10, July, 2025);
     Settings::instance().evaluationDate() = today;
     BOOST_TEST_MESSAGE("Initializing Analytical Soltuion");
 
-    Handle<YieldTermStructure> ts(boost::make_shared<FlatForward>(today, 0.02, Actual360()));
+    //Handle<YieldTermStructure> ts(boost::make_shared<FlatForward>(today, 0.02, Actual360()));
+    Handle<YieldTermStructure> ts(QuantLib::ext::make_shared<FlatForward>(0, NullCalendar(), 0.02, Actual365Fixed()));
+
 
     Array kappa = {0.01};
     QuantLib::Matrix sigma = {{0.01}};
@@ -214,8 +519,11 @@ BOOST_AUTO_TEST_CASE(testAnalyticalvsSimulation) {
     Swaption swp = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
 
 
-    // Create another swap for the LGM model (same as prev swap)
+    // Create another swap for the LGM model(s) (same as prev swap)
+    //IrLgm1fParametrization
     Swaption swpLgm = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
+    //LinearGaussMarkovModel
+    Swaption swpLgmTwo = MakeSwaption(index, 2 * Years, strike).withUnderlyingType(VanillaSwap::Payer);
    
     // Define price engine
     ext::shared_ptr<PricingEngine> hw_engine = boost::make_shared<AnalyticHwSwaptionEngine>(swp, model, ts);
@@ -237,15 +545,22 @@ BOOST_AUTO_TEST_CASE(testAnalyticalvsSimulation) {
     auto hwAdaptor = QuantLib::ext::make_shared<IrLgm1fPiecewiseConstantHullWhiteAdaptor>(EURCurrency(), ts, sigmaDates,sigmaLgm, kappaDates, kappa);
     QuantLib::ext::shared_ptr<PricingEngine> lgmEngine =
          QuantLib::ext::make_shared<AnalyticLgmSwaptionEngine>(hwAdaptor);
+    // This is an additional way I found where one cass pass in the HW adaptor to the LGM engine
+    auto lgmTwo = ext::make_shared<LinearGaussMarkovModel>(hwAdaptor);
+    QuantLib::ext::shared_ptr<PricingEngine> lgmEngine2 = QuantLib::ext::make_shared<AnalyticLgmSwaptionEngine>(lgmTwo);
 
     // Pass pricing engines
     swp.setPricingEngine(hw_engine);
     swpLgm.setPricingEngine(lgmEngine);
+    swpLgmTwo.setPricingEngine(lgmEngine2);
 
     Real analyticalPrice = swp.NPV();
     Real analyticalLgmPrice = swpLgm.NPV();
-    std::cout << "Analytical Price: " << analyticalPrice << std::endl;
-    std::cout << "The LGM adaptor price: " << analyticalLgmPrice << std::endl;
+    Real analyticalLgmPriceTwo = swpLgmTwo.NPV();
+
+    cout << "Analytical Price: " << analyticalPrice << endl;
+    cout << "The LGM adaptor price: " << analyticalLgmPrice << endl;
+    cout << "The alternative LGM adaptor price: " << analyticalLgmPriceTwo << endl;
 
 
     BOOST_TEST_MESSAGE("Initializing MC Simulation");
@@ -260,7 +575,7 @@ BOOST_AUTO_TEST_CASE(testAnalyticalvsSimulation) {
 
     // Simulation params
     Size nPaths = 1000;
-    Size nSteps = 100;
+    Size nSteps = 400;
     Time dt = maturity / nSteps;
     Real sumPayoffs = 0.0;
 
@@ -360,9 +675,8 @@ BOOST_AUTO_TEST_CASE(testAnalyticalvsSimulation) {
         Real swapPV = floatingPV - fixedPV;
         Real payoff = max(swapPV, 0.0);
 
-        Real num_t = model->numeraire(maturity, factors, ts); // TODO this numeraire might be wrong
-        Real num_0 = 1.0;
-        sumPayoffs += payoff * num_0 / num_t;
+        Real num = model->numeraire(maturity, factors, ts); // TODO this numeraire might be wrong
+        sumPayoffs += payoff / num;
     }
 
     Real expectedPayoff = sumPayoffs / nPaths;
