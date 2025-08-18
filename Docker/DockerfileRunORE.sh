59262116dbf6197a630856ac67453b98e617ed17
Damien Barker <damien.barker@lseg.com>
2025-08-18 14:49:31 +0100

    QPR-13475 remove nexus and move to ORE

--- Docker/DockerfileRunORE.sh
+++ Docker/DockerfileRunORE.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-dependencies.yml build || exit 1
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore.yml build || exit 1
 docker-compose --env-file Docker/.env -f Docker/docker-compose-test.yml build || exit 1
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose --env-file Docker/.env -f Docker/docker-compose-ore-app.yml build || exit 1
