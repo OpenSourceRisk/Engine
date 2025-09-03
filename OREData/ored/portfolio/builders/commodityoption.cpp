11ac3a7029e33936b265940aaad76c97c664d57e
Shen <zeyu.shen@lseg.com>
2025-09-03 15:24:20 -0400

    Fix error

--- OREData/ored/portfolio/builders/commodityoption.cpp
+++ OREData/ored/portfolio/builders/commodityoption.cpp
@@ -28,14 +28,16 @@ namespace data {
     "   NUMBER Payoff, d, currentNotional;\n"
     "   \n"
     "   currentNotional = Quantity * Strike;\n"
-    "   FOR d IN(SIZE(ExerciseDates), 1, -1) DO Option = NPV(Option, ExerciseDates[d]);\n"
-    "       Payoff = NPV(OptionType * (Underlying(ExerciseDates[d]) - Strike), ExerciseDates[d]);\n"
+    "   FOR d IN(SIZE(ExerciseDates), 1, -1) DO\n"
+    "       Option = NPV(Option, ExerciseDates[d]);\n" 
+    "       Payoff = NPV(OptionType * (Underlying(ExerciseDates[d]) - Strike), ExerciseDates[d]);\n "
+    //"       Payoff = PAY(OptionType * (Underlying(ExerciseDates[d]) - Strike), ExerciseDates[d], ExerciseDates[d], PayCcy);\n"
     "       IF Payoff > Option THEN\n"
     "           Option = Payoff;\n"
     "       END;\n"
     "   END;\n"
-    "   Option = Option * Quantity * LongShort;\n"
-    "   Option = Option - LongShort * PremiumAmount;\n";
+    "   Option = Option * Quantity * LongShort;\n";
+    //"   Option = Option - LongShort * PremiumAmount;\n";
 
 QuantLib::ext::shared_ptr<ore::data::Trade>
     CommodityAmericanFDScriptedEngineBuilder::build(const Trade* trade,
@@ -48,19 +50,29 @@ QuantLib::ext::shared_ptr<ore::data::Trade>
 
     // Build a ScriptedTrade with inline scripts
     Date today = Settings::instance().evaluationDate();
-    std::vector<ScriptedTradeEventData> events = {ScriptedTradeEventData(
-        "ExerciseDates", ScheduleData(ScheduleRules(
-                             to_string(today), commodityOption->option().exerciseDates()[0],
-                                                    "1D", commodityOption->currency(), "F", "F", "Forward")))};
+    Date expiry = parseDate(commodityOption->option().exerciseDates()[0]);
+    std::vector<ScriptedTradeEventData> events;
+    if (today < expiry)
+        events.push_back(ScriptedTradeEventData(
+            "ExerciseDates", ScheduleData(ScheduleRules(to_string(today), commodityOption->option().exerciseDates()[0],
+                                                        "1D", "WeekendsOnly", "F", "F", "Forward"))));
+    else
+        events.push_back(ScriptedTradeEventData(
+            "ExerciseDates", ScheduleData(ScheduleRules(commodityOption->option().exerciseDates()[0],
+                                                        commodityOption->option().exerciseDates()[0], "1D",
+                                                        "WeekendsOnly", "F", "F", "Forward"))));
 
-    std::vector<ScriptedTradeValueTypeData> numbers =
-    { 
-        ScriptedTradeValueTypeData("Number", "Strike", std::to_string(commodityOption->strike().value())), 
+    //double premiumAmount = commodityOption->option().premiumData().premiumData()[0].amount;
+
+    std::vector<ScriptedTradeValueTypeData> numbers = {
+        ScriptedTradeValueTypeData("Number", "Strike", std::to_string(commodityOption->strike().value())),
         ScriptedTradeValueTypeData("Number", "Quantity", std::to_string(commodityOption->quantity())),
         ScriptedTradeValueTypeData("Number", "LongShort",
-                                    commodityOption->option().longShort() == "Long" ? "1.0" : "-1.0"),
+                                   commodityOption->option().longShort() == "Long" ? "1.0" : "-1.0"),
         ScriptedTradeValueTypeData("Number", "OptionType",
-                                    commodityOption->option().callPut() == "Call" ? "1.0" : "-1.0")
+                                   commodityOption->option().callPut() == "Call" ? "1.0" : "-1.0"),
+        ScriptedTradeValueTypeData("Number", "PremiumAmount",
+                                   std::to_string(commodityOption->option().premiumData().premiumData()[0].amount))
     };
 
     std::vector<ScriptedTradeValueTypeData> indices = {
@@ -73,21 +85,19 @@ QuantLib::ext::shared_ptr<ore::data::Trade>
 
     std::vector<ScriptedTradeValueTypeData> dayCounter;
 
+    std::vector<ScriptedTradeScriptData::CalibrationData> calibrationSpec = {
+        ScriptedTradeScriptData::CalibrationData("Underlying", std::vector<std::string>{"Strike"})};
+
     std::map<std::string, ScriptedTradeScriptData> script = {
         {"", ScriptedTradeScriptData(commodity_american_fd_script, "Option",
-                                    {{"currentNotional", "currentNotional"}, {"notionalCurrency", "PayCcy"}}, {})}
-    };
+                                     {{"currentNotional", "currentNotional"}, {"notionalCurrency", "PayCcy"}}, {}, {},
+                                     calibrationSpec)}};
 
     auto scriptedTrade =
         QuantLib::ext::make_shared<ScriptedTrade>(commodityOption->envelope(), events, numbers, indices, currencies,
                                                   dayCounter, script, "CommodityOptionAmerican", "ScriptedTrade");
 
-
-
-
-
-
-    const auto longShortPremium = commodityOption->option().longShort() == "Long" ? -1 : 1;
+    auto longShortPremium = commodityOption->option().longShort() == "Long" ? -1 : 1;
     scriptedTrade->build(engineFactory, commodityOption->option().premiumData(), longShortPremium);
 
     return scriptedTrade;
