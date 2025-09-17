6352da3195a11bbc41f26b308f417c7f8ecd5131
Matthias Groncki <matthias.groncki@acadia.inc>
2025-09-17 18:58:14 +0200

    QPR-13526 bachelier model for commodity options (WIP)

--- QuantExt/qle/pricingengines/analyticeuropeanengine.hpp
+++ QuantExt/qle/pricingengines/analyticeuropeanengine.hpp
@@ -36,13 +36,18 @@ using namespace QuantLib;
 class AnalyticEuropeanEngine : public QuantLib::AnalyticEuropeanEngine {
 public:
     explicit AnalyticEuropeanEngine(ext::shared_ptr<GeneralizedBlackScholesProcess> gbsp,
-                                    const bool flipResults = false)
-        : QuantLib::AnalyticEuropeanEngine(gbsp), flipResults_(flipResults) {}
+                                    const bool flipResults = false,
+                                    QuantLib::BlackBachelierModel model = QuantLib::BlackBachelierMode::AsVolTypel,
+                                    const double displacement = 0.0)
+        : QuantLib::AnalyticEuropeanEngine(gbsp, model, displacement), flipResults_(flipResults) {}
 
     AnalyticEuropeanEngine(ext::shared_ptr<GeneralizedBlackScholesProcess> process,
                            Handle<YieldTermStructure> discountCurve, const bool flipResults = false,
-                           ext::optional<unsigned int> spotDays = {}, ext::optional<Calendar> spotCalendar = {})
-        : QuantLib::AnalyticEuropeanEngine(process, discountCurve, spotDays, spotCalendar), flipResults_(flipResults) {}
+                           ext::optional<unsigned int> spotDays = {}, ext::optional<Calendar> spotCalendar = {},
+                           QuantLib::BlackBachelierModel model = QuantLib::BlackBachelierMode::AsVolTypel,
+                           const double displacement = 0.0)
+        : QuantLib::AnalyticEuropeanEngine(process, discountCurve, spotDays, spotCalendar, model, displacement),
+          flipResults_(flipResults) {}
 
     void calculate() const override;
 
