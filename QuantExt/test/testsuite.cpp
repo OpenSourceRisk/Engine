32a734bad76f50d52e5741fba7d5546fd0212096
Damien Barker <damien.barker@lseg.com>
2025-10-23 21:41:21 +0100

    QPR-12983 fix run command

--- QuantExt/test/testsuite.cpp
+++ QuantExt/test/testsuite.cpp
@@ -36,8 +36,13 @@
 
 // Boost
 using namespace boost;
+#include <boost/timer/timer.hpp>
+using boost::timer::cpu_timer;
 using boost::unit_test::test_suite;
 using boost::unit_test::framework::master_test_suite;
+
+#include <oret/oret.hpp>
+using ore::test::setupTestLogging;
 
 #include "toplevelfixture.hpp"
 
@@ -52,3 +57,44 @@ using boost::unit_test::framework::master_test_suite;
 #include <boost/config/auto_link.hpp>
 #endif
 
+class QleGlobalFixture {
+public:
+    QleGlobalFixture() {
+        int argc = master_test_suite().argc;
+        char** argv = master_test_suite().argv;
+
+        // Set up test logging
+        setupTestLogging(argc, argv);
+    }
+
+    ~QleGlobalFixture() { stopTimer(); }
+
+    // Method called in destructor to log time taken
+    void stopTimer() {
+        t.stop();
+        double seconds = t.elapsed().wall * 1e-9;
+        int hours = int(seconds / 3600);
+        seconds -= hours * 3600;
+        int minutes = int(seconds / 60);
+        seconds -= minutes * 60;
+        std::cout << std::endl << "QuantExt tests completed in ";
+        if (hours > 0)
+            std::cout << hours << " h ";
+        if (hours > 0 || minutes > 0)
+            std::cout << minutes << " m ";
+        std::cout << std::fixed << std::setprecision(0) << seconds << " s" << std::endl;
+    }
+
+private:
+    // Timing the test run
+    cpu_timer t;
+};
+
+// Breaking change in 1.65.0
+// https://www.boost.org/doc/libs/1_65_0/libs/test/doc/html/boost_test/change_log.html
+// Deprecating BOOST_GLOBAL_FIXTURE in favor of BOOST_TEST_GLOBAL_FIXTURE
+#if BOOST_VERSION < 106500
+BOOST_GLOBAL_FIXTURE(OreaGlobalFixture);
+#else
+BOOST_TEST_GLOBAL_FIXTURE(QleGlobalFixture);
+#endif
