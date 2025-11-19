795d9037b117986a401e1067d5445120081fe80d
Damien Barker <damien.barker@lseg.com>
2025-11-19 15:33:48 +0000

    QPR-12959 fix test copy

--- Tools/PythonTools/update_expected_output.py
+++ Tools/PythonTools/update_expected_output.py
@@ -2,9 +2,7 @@ from pathlib import Path
 import shutil
 
 
-def copy_existing_files(test_path):
-    output_dir = Path(test_path) / "Output"
-    expected_dir = Path(test_path) / "ExpectedOutput"
+def copy_existing_output(output_dir, expected_dir):
 
     if not output_dir.exists() or not expected_dir.exists():
         print("One of the directories does not exist.")
@@ -18,3 +16,9 @@ def copy_existing_files(test_path):
                 print(f"Copied {file.name} to ExpectedOutput")
             else:
                 print(f"Skipped {file.name} (not in ExpectedOutput)")
+
+
+def copy_existing_files(test_path):
+    output_dir = Path(test_path) / "Output"
+    expected_dir = Path(test_path) / "ExpectedOutput"
+    copy_existing_output(output_dir, expected_dir)
