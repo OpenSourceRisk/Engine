ceca90eaecde9371b5ba122addf8744a2f441f84
Damien Barker <damien.barker@lseg.com>
2025-11-26 12:18:08 +0000

    QPR-12959 update with master and resolve conflicts

---
+++ Examples/conftest.py
@@ -0,0 +1,34 @@
+# conftest.py
+import pytest, os, sys
+from pathlib import Path
+script_dir = Path(__file__).parents[0]
+sys.path.append(os.path.join(script_dir, '../Tools/PythonTools'))
+from update_expected_output import copy_existing_files
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    # Only proceed during the "call" phase
+    if report.when != "call":
+        return
+
+    # Read environment variables
+    copy_all_output = os.getenv("UPDATE_ALL_EXPECTED_OUTPUT", "").upper() == "TRUE"
+    copy_failed_output = os.getenv("UPDATE_FAILED_EXPECTED_OUTPUT", "").upper() == "TRUE"
+
+    # Logic:
+    # - UPDATE_ALL_EXPECTED_OUTPUT → always copy
+    # - UPDATE_FAILED_EXPECTED_OUTPUT → copy only if test failed
+    if copy_all_output or (copy_failed_output and report.failed):
+        # Build test path
+        test_name_props = item.name.split("_", 1)
+        test_name = test_name_props[1]
+        test_path = Path(item.config.rootdir) / test_name
+
+        copy_existing_files(test_path)
+
+
+
