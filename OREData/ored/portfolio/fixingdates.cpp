9d3a0b406f9bd3964441b3a29c9cab247c91501f
Damien Barker <damien.barker@lseg.com>
2025-11-27 09:35:25 +0000

    QPR-13614 make indexed coupon fixing optional when fixingDate==valuationDate

--- OREData/ored/portfolio/fixingdates.cpp
+++ OREData/ored/portfolio/fixingdates.cpp
@@ -695,9 +695,10 @@ void FixingDateGetter::visit(QuantExt::SubPeriodsCoupon1& c) {
 
 void FixingDateGetter::visit(IndexedCoupon& c) {
     // the coupon's index might be null if an initial fixing is provided
-    if (c.index())
-        requiredFixings_.addFixingDate(c.fixingDate(), IndexNameTranslator::instance().oreName(c.index()->name()),
-                                       c.date());
+    if (c.index()) {
+        bool isTodaysFixing = Settings::instance().evaluationDate() == c.fixingDate();
+        requiredFixings_.addFixingDate(c.fixingDate(), IndexNameTranslator::instance().oreName(c.index()->name()), c.date(), false, isTodaysFixing);
+    }
     QL_REQUIRE(c.underlying(), "FixingDateGetter::visit(IndexedCoupon): underlying() is null");
     c.underlying()->accept(*this);
 }
