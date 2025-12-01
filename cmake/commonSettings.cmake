ef207294326e66b4483d71d45ab9339b7cee6a67
francis <fdfrancisduffy@gmail.com>
2025-12-01 16:03:04 +0000

    Remove duplicate.

--- cmake/commonSettings.cmake
+++ cmake/commonSettings.cmake
@@ -276,7 +276,6 @@ get_filename_component(ORETEST_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../ORETest"
 get_filename_component(RAPIDXML_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../ThirdPartyLibs/rapidxml-1.13" ABSOLUTE)
 
 # parallel unit test runner
-option(ORE_ENABLE_PARALLEL_UNIT_TEST_RUNNER "Enable the parallel unit test runner" OFF)
 if (ORE_ENABLE_PARALLEL_UNIT_TEST_RUNNER)
     add_definitions(-DORE_ENABLE_PARALLEL_UNIT_TEST_RUNNER)
 endif()
