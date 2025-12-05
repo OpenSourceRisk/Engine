a9a321673bc70fa15d0eddaac7e99aa7480475b0
Matthias Groncki <matthias.groncki@acadia.inc>
2025-12-05 13:05:36 +0100

    QPR-13659 cleanup

--- OREData/ored/utilities/inflationstartdate.hpp
+++ OREData/ored/utilities/inflationstartdate.hpp
@@ -34,9 +34,7 @@ namespace data {
 // example, this is more complicated and we need to account for this here if the inflation swap conventions provide
 // us with a publication schedule and tell us to roll on that schedule.
 std::pair<QuantLib::Date, QuantLib::Period>
-getStartAndLag(const QuantLib::Date& asof, const InflationSwapConvention& conv, const int startDelay = 0,
-               const Calendar& cal = Calendar(),
-               const BusinessDayConvention startDelayConvention = BusinessDayConvention::Unadjusted);
+getStartAndLag(const QuantLib::Date& asof, const InflationSwapConvention& conv);
 
 QuantLib::Date getInflationSwapStart(const QuantLib::Date& asof, const InflationSwapConvention& conv);
 
