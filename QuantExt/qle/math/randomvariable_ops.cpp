46b41efb6596c6fa59b6f2c815420569db834f4b
sebastien.bouvard <sebastien.bouvard@acadia.inc>
2026-01-05 18:01:03 +0000

    QPR-13698 Remove round frac function

--- QuantExt/qle/math/randomvariable_ops.cpp
+++ QuantExt/qle/math/randomvariable_ops.cpp
@@ -310,11 +310,11 @@ std::vector<RandomVariableGrad> getRandomVariableGradients(const Size size, cons
 
     // Frac = 19
     grads.push_back([size](const std::vector<const RandomVariable*>& args, const RandomVariable* v,
-                       const Size node) -> std::vector<RandomVariable> { return {QuantExt::frac(RandomVariable(size, 1.0))}; });
+                       const Size node) -> std::vector<RandomVariable> { return {RandomVariable(size, 1.0)}; });
 
     // Round = 20
     grads.push_back([size](const std::vector<const RandomVariable*>& args, const RandomVariable* v,
-                       const Size node) -> std::vector<RandomVariable> { return {QuantExt::round(RandomVariable(size, 0.0), RandomVariable(size, 0.0))}; });
+                       const Size node) -> std::vector<RandomVariable> { return {RandomVariable(size, 0.0), RandomVariable(size, 0.0)}; });
 
     return grads;
 }
