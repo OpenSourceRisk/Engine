fd5235cdc350e0beb8d25b0d5cfeb95588ed9400
Matthias Groncki <matthias.groncki@acadia.inc>
2026-01-09 11:55:44 +0100

    QPR-11988 bugfix

--- OREData/ored/portfolio/builders/fxeuropeanbarrieroption.cpp
+++ OREData/ored/portfolio/builders/fxeuropeanbarrieroption.cpp
@@ -9,14 +9,14 @@
   under the terms of the Modified BSD License.  You should have received a
   copy of the license along with this program.
   The license is also available online at <http://opensourcerisk.org>
-  
+
   This program is distributed on the basis that it will form a useful
   contribution to risk analytics and model standardisation, but WITHOUT
   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
   FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
  */
 
-#include <ored/portfolio/builder/fxeuropeanbarrieroption.hpp>
+#include <ored/portfolio/builders/fxeuropeanbarrieroption.hpp>
 #include <ored/portfolio/fxeuropeanbarrieroption.hpp>
 #include <ored/portfolio/genericbarrieroption.hpp>
 #include <ored/portfolio/underlying.hpp>
@@ -25,26 +25,27 @@ namespace ore {
 namespace data {
 
 QuantLib::ext::shared_ptr<ore::data::Trade>
-FxEuropeanBarrierOptionEngineBuilder::build(const Trade* trade, const QuantLib::ext::shared_ptr<EngineFactory>& engineFactory) {
+FxEuropeanBarrierOptionEngineBuilder::build(const Trade* trade,
+                                            const QuantLib::ext::shared_ptr<EngineFactory>& engineFactory) {
 
     auto fxEuropeanBarrierOption = dynamic_cast<const ore::data::FxEuropeanBarrierOption*>(trade);
 
-    QL_REQUIRE(
-        fxEuropeanBarrierOption != nullptr,
-        "FxEuropeanBarrierOptionEngineBuilder: internal error, could not cast to ore::data::FxEuropeanBarrierOption. Contact dev.");
+    QL_REQUIRE(fxEuropeanBarrierOption != nullptr, "FxEuropeanBarrierOptionEngineBuilder: internal error, could not "
+                                                   "cast to ore::data::FxEuropeanBarrierOption. Contact dev.");
 
-    auto underlying = Quantib::ext::make_shared<FXUnderlying>("FX", fxEuropeanBarrierOption->fxIndex(), 1.0);
-    
-    auto optionData = fxEuropeanBarrierOption->optionData();
+    QuantLib::ext::shared_ptr<Underlying> underlying =
+        QuantLib::ext::make_shared<FXUnderlying>("FX", fxEuropeanBarrierOption->fxIndex(), 1.0);
 
-    std::string exerciseDate = optionData->exerciseDates().begin();
-    ScheduleDates monitoringDates("NullCalendar", "", "0D", {exerciseDate} );
+    const auto& optionData = fxEuropeanBarrierOption->option();
+
+    std::string exerciseDate = optionData.exerciseDates().front();
+    ScheduleDates monitoringDates("NullCalendar", "", "0D", {exerciseDate});
     ScheduleData barrierMonitoringDates(monitoringDates);
 
     //! Empty transatlantic barrier
     auto transatlanticBarrier = BarrierData();
     //! Observation date for schedule monitoring dates only at the end
-    
+
     std::string domesticCurrency = fxEuropeanBarrierOption->soldCurrency();
 
     Date expiryDate = parseDate(exerciseDate);
@@ -64,14 +65,14 @@ FxEuropeanBarrierOptionEngineBuilder::build(const Trade* trade, const QuantLib::
     }
     auto qty = fxEuropeanBarrierOption->boughtAmount();
     auto strike = fxEuropeanBarrierOption->strike();
-
+    std::vector<BarrierData> barriers = {fxEuropeanBarrierOption->barrier()};
     auto barrierOption = QuantLib::ext::make_shared<GenericBarrierOption>(
-        underlying, optionData, {fxEuropeanBarrierOption->barrierData()}, barrierMonitoringDates, transatlanticBarrier,
-        domesticCurrency, to_string(paymentDate), to_string(qty), to_string(strike), "", "");
+        underlying, optionData, barriers, barrierMonitoringDates, transatlanticBarrier, domesticCurrency,
+        to_string(paymentDate), to_string(qty), to_string(strike), "", "");
 
     barrierOption->build(engineFactory);
     return barrierOption;
 }
 
 } // namespace data
-} // namespace oreplus
+} // namespace ore
