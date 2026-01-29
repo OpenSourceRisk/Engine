e0b478a6406667bf25c16a6a1f42b15e0b6bc8b8
Damien Barker <damien.barker@lseg.com>
2026-01-29 14:36:17 +0000

    QPR-12237 random missing from cube

--- OREAnalytics/test/cube.cpp
+++ OREAnalytics/test/cube.cpp
@@ -48,6 +48,7 @@
 #include <ored/utilities/log.hpp>
 #include <ored/utilities/fileio.hpp>
 #include <ored/utilities/osutils.hpp>
+#include <ql/math/randomnumbers/mt19937uniformrng.hpp>
 #include <ql/time/calendars/target.hpp>
 #include <ql/time/date.hpp>
 #include <ql/time/daycounters/actualactual.hpp>
@@ -57,12 +58,13 @@
 #include <orea/app/reportwriter.hpp>
 
 #include "testmarket.hpp"
-#include <random>
 
 using namespace ore::analytics;
 using namespace boost::unit_test_framework;
 using std::string;
 using std::vector;
+
+namespace {
 
 void initCube(NPVCube& cube) {
     // Set every (i,j,k,d) node to be i*1000000 + j + (k/1000000) + d*3
