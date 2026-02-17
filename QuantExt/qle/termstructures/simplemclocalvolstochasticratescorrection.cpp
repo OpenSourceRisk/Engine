38a695a954f0514873d6e63b84e11e562fe45267
Peter Caspers <peter.caspers@lseg.com>
2026-02-17 12:13:03 +0100

    QPR-13825 draft correction class, untested

--- QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.cpp
+++ QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.cpp
@@ -18,13 +18,39 @@
 
 #include <qle/termstructures/simplemclocalvolstochasticratescorrection.hpp>
 
+#include <ql/math/interpolations/flatextrapolation2d.hpp>
+#include <ql/math/interpolations/bilinearinterpolation.hpp>
+#include <ql/pricingengines/blackformula.hpp>
+
+using namespace QuantLib;
+
+namespace {
+inline Array getProjectedArray(const Array& source, Size start, Size length) {
+    QL_REQUIRE(source.size() >= start + length, "getProjectedArray(): internal errors: source size "
+                                                    << source.size() << ", start" << start << ", length " << length);
+    return Array(std::next(source.begin(), start), std::next(source.begin(), start + length));
+}
+} // namespace
+
 namespace QuantExt {
 
 SimpleMcLocalVolStochasticRatesCorrection::SimpleMcLocalVolStochasticRatesCorrection(
-    Handle<LocalVolTermStructure> source, QuantLib::ext::shared_ptr<IrModel> r, QuantLib::ext::shared_ptr<IrModel> q,
-    QuantLib::ext::shared_ptr<FxModel> S, std::function<QuantLib::Array(QuantLib::Real, QuantLib::Real)> dwGenerator)
-    : source_(std::move(source)), r_(std::move(r)), q_(std::move(q)), S_(std::move(S)),
-      dwGenerator_(std::move(dwGenerator)) {}
+    Handle<BlackVolTermStructure> blackVol, Handle<LocalVolTermStructure> source, ext::shared_ptr<IrModel> r,
+    ext::shared_ptr<IrModel> q, ext::shared_ptr<FxModel> S, Handle<YieldTermStructure> r0,
+    Handle<YieldTermStructure> q0, std::function<Array(Real, Real)> dwGenerator, std::vector<Real> times,
+    std::vector<Real> logStrikes, Size nPaths)
+    : blackVol_(blackVol), source_(std::move(source)), r_(std::move(r)), q_(std::move(q)), S_(std::move(S)),
+      r0_(std::move(r0)), q0_(std::move(q0)), dwGenerator_(std::move(dwGenerator)), times_(std::move(times)),
+      logStrikes_(std::move(logStrikes)), nPaths_(nPaths) {
+
+    QL_REQUIRE(!times_.empty(), "SimpleMcLocalVolStochasticRatesCorrection: times are empty");
+    QL_REQUIRE(!logStrikes_.empty(), "SimpleMcLocalVolStochasticRatesCorrection: strikes are empty");
+
+    if (!close_enough(times_.front(), 0.0))
+        times_.insert(times_.begin(), 0.0);
+
+    correctionData_ = Matrix(logStrikes_.size(), times_.size(), 0.0);
+}
 
 void SimpleMcLocalVolStochasticRatesCorrection::update() {
     TermStructure::update();
@@ -33,10 +59,105 @@ void SimpleMcLocalVolStochasticRatesCorrection::update() {
 
 void SimpleMcLocalVolStochasticRatesCorrection::performCalculations() const {
 
+    Size m_r = r_->m() + r_->m_aux();
+    Size m_q = q_->m() + q_->m_aux();
+    Size m_s = S_->m() + S_->m_aux();
+
+    std::vector<Array> rState(nPaths_, r_->initialValue());
+    std::vector<Array> qState(nPaths_, q_->initialValue());
+    std::vector<Array> sState(nPaths_, S_->initialValue());
+
+    std::vector<Real> r(nPaths_);
+    std::vector<Real> q(nPaths_);
+    std::vector<Real> n(nPaths_);
+
+    correction_ = std::make_unique<FlatExtrapolator2D>(QuantLib::ext::make_shared<BilinearInterpolation>(
+        times_.begin(), times_.end(), logStrikes_.begin(), logStrikes_.end(), correctionData_));
+
+    for (Size i = 1; i < times_.size(); ++i) {
+
+        Real t0 = times_[i - 1];
+        Real t1 = times_[i];
+        Real dt = t1 - t0;
+
+        // generate path values
+
+        for (Size p = 0; p < nPaths_; ++p) {
+
+            Array dw = dwGenerator_(t0, dt);
+
+            rState[p] = r_->marginalStep(t0, rState[p], dt, getProjectedArray(dw, 0, m_r));
+            qState[p] = q_->marginalStep(t0, qState[p], dt, getProjectedArray(dw, m_r, m_q));
+            sState[p] = S_->marginalStep(t0, sState[p], dt, getProjectedArray(dw, m_r + m_q, m_s), r[p], q[p]);
+
+            r[p] = r_->shortRate(t0, rState[p], r0_);
+            q[p] = q_->shortRate(t0, qState[p], q0_);
+
+            n[p] = r_->numeraire(t0, getProjectedArray(rState[p], 0, m_r), r0_);
+        }
+
+        // sort path data by S
+
+        std::vector<Size> pm(nPaths_);
+        std::iota(pm.begin(), pm.end(), 0);
+        std::sort(pm.begin(), pm.end(), [&sState](Size i, Size j) { return sState[i][0] > sState[j][0]; });
+
+        // iterate over strikes and accumulate results
+
+        Real F = S_->fxSpotToday()->value() * q0_->discount(t1) / r0_->discount(t1);
+
+        Real r0 = r0_->forwardRate(t0, t0, Continuous);
+        Real q0 = q0_->forwardRate(t0, t0, Continuous);
+        Real dsc = r0_->discount(t0);
+
+        Real e1 = 0.0, e2 = 0.0;
+
+        Size p = 0;
+        Real K_prev;
+
+        for (int k = logStrikes_.size() - 1; k >= 0; --k) {
+
+            Real K = F * std::exp(logStrikes_[k]);
+
+            if (k < logStrikes_.size() - 1) {
+                e2 += K_prev - K;
+            }
+
+            Real n1 = 0.0, n2 = 0.0;
+
+            while (sState[pm[p]][0] > logStrikes_[k] + std::log(F)) {
+
+                n1 += (r[pm[p]] - q[pm[p]]) / n[pm[p]];
+                n2 += q[pm[p]] * (std::exp(sState[pm[p]][0]) - K) / n[pm[p]];
+
+                ++p;
+            }
+
+            e1 += static_cast<Real>(n1) / static_cast<Real>(nPaths_);
+            e2 += static_cast<Real>(n2) / static_cast<Real>(nPaths_);
+
+            K_prev = K;
+
+            constexpr Real eps = 1E-4;
+            Real volK = blackVol_->blackVol(t0, K);
+            Real volKm = blackVol_->blackVol(t0, K - eps);
+            Real volKp = blackVol_->blackVol(t0, K + eps);
+            Real C = blackFormula(Option::Type::Call, K, F, volK, dsc);
+            Real Cm = blackFormula(Option::Type::Call, K - eps, F, volKm, dsc);
+            Real Cp = blackFormula(Option::Type::Call, K + eps, F, volKp, dsc);
+            Real dCdK = (Cp - Cm) / (2.0 * eps);
+            Real d2CdK2 = (Cp - 2.0 * Cm + Cm) / (eps * eps);
+
+            correctionData_(k, i - 1) =
+                std::sqrt(((-K * e1 + e2) - (K * dCdK * (r0 - q0) + q0 * C)) / (0.5 * K * K * d2CdK2));
+        }
+
+        correction_->update();
+    }
 }
 
 Volatility SimpleMcLocalVolStochasticRatesCorrection::localVolImpl(Time t, Real strike) const {
-    return source_->localVol(t, strike);
+    return std::max(0.0, source_->localVol(t, strike) + correction_->operator()(t, strike));
 }
 
 } // namespace QuantExt
--- QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.hpp
+++ QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.hpp
@@ -22,32 +22,47 @@
 
 #pragma once
 
-#include <qle/models/irmodel.hpp>
 #include <qle/models/fxmodel.hpp>
+#include <qle/models/irmodel.hpp>
 
-#include <ql/termstructures/volatility/equityfx/localvoltermstructure.hpp>
 #include <ql/handle.hpp>
+#include <ql/termstructures/volatility/equityfx/localvoltermstructure.hpp>
+#include <ql/termstructures/volatility/equityfx/blackvoltermstructure.hpp>
+#include <ql/math/interpolations/interpolation2d.hpp>
 
 namespace QuantExt {
 
 class SimpleMcLocalVolStochasticRatesCorrection : public QuantLib::LocalVolTermStructure, public QuantLib::LazyObject {
 public:
-    /*! S should be linked to this local vol ts to facilitate the bootstrap of the correction */
+    /*! - S should be linked to this local vol ts to facilitate the bootstrap of the correction
+        - the log strike grid is defined as ln( K / F ) */
     SimpleMcLocalVolStochasticRatesCorrection(
+        QuantLib::Handle<QuantLib::BlackVolTermStructure> blackVol,
         QuantLib::Handle<QuantLib::LocalVolTermStructure> source, QuantLib::ext::shared_ptr<IrModel> r,
         QuantLib::ext::shared_ptr<IrModel> q, QuantLib::ext::shared_ptr<FxModel> S,
-        std::function<QuantLib::Array(QuantLib::Real, QuantLib::Real)> dwGenerator);
+        QuantLib::Handle<QuantLib::YieldTermStructure> r0, QuantLib::Handle<QuantLib::YieldTermStructure> q0,
+        std::function<QuantLib::Array(QuantLib::Real, QuantLib::Real)> dwGenerator, std::vector<Real> times,
+        std::vector<Real> logStrikes, Size nPaths);
 
 private:
     void performCalculations() const override;
     void update() override;
     QuantLib::Volatility localVolImpl(Time t, Real strike) const override final;
 
+    QuantLib::Handle<QuantLib::BlackVolTermStructure> blackVol_;
     QuantLib::Handle<QuantLib::LocalVolTermStructure> source_;
     QuantLib::ext::shared_ptr<IrModel> r_;
     QuantLib::ext::shared_ptr<IrModel> q_;
     QuantLib::ext::shared_ptr<FxModel> S_;
+    QuantLib::Handle<QuantLib::YieldTermStructure> r0_;
+    QuantLib::Handle<QuantLib::YieldTermStructure> q0_;
     std::function<QuantLib::Array(QuantLib::Real, QuantLib::Real)> dwGenerator_;
+    std::vector<Real> times_;
+    std::vector<Real> logStrikes_;
+    Size nPaths_;
+
+    mutable Matrix correctionData_;
+    mutable std::unique_ptr<Interpolation2D> correction_;
 };
 
 } // namespace QuantExt
