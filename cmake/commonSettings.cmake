12e559a837de4923f0fbcc65db4850e6f95e10a5
farahkhashman <farah.khashman@acadia.inc>
2026-02-19 11:00:33 +0000

    try c++20 in cmake common settings

--- cmake/commonSettings.cmake
+++ cmake/commonSettings.cmake
@@ -19,7 +19,7 @@ option(ORE_ENABLE_OPENCL "Enable OpenCL" OFF)
 option(ORE_ENABLE_CUDA "Enable CUDA" OFF)
 
 # define build type clang address sanitizer + undefined behaviour + LIBCPP assertions, but keep O2
-set(CMAKE_CXX_FLAGS_CLANG_ASAN_O2 "-fsanitize=address,undefined -fno-omit-frame-pointer -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_DEBUG -g -O2")
+set(CMAKE_CXX_FLAGS_CLANG_ASAN_O2 "-fsanitize=address,undefined -fno-omit-frame-pointer -D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_DEBUG -g -O2 -std=c++20")
 
 # add compiler flag, if not already present
 macro(add_compiler_flag flag supportsFlag)
