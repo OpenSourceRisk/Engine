a2801ad3bfeffe7101d46d17aafde363c2df54e7
Peter Caspers <peter.caspers@lseg.com>
2026-02-21 19:42:05 +0100

    QPR-12228 fixes

--- OREData/ored/scripting/engines/amccgbaseengine.cpp
+++ OREData/ored/scripting/engines/amccgbaseengine.cpp
@@ -168,9 +168,9 @@ AmcCgBaseEngine::CashflowInfo AmcCgBaseEngine::createCashflowInfo(QuantLib::ext:
 
         std::size_t effectiveRate;
         if (isCapFloored) {
-            std::size_t swapletRate = ComputationGraph::nan;
-            std::size_t floorletRate = ComputationGraph::nan;
-            std::size_t capletRate = ComputationGraph::nan;
+            std::size_t swapletRate = cg_const(g, 0.0);
+            std::size_t floorletRate = cg_const(g, 0.0);
+            std::size_t capletRate = cg_const(g, 0.0);
             if (!isNakedOption)
                 swapletRate = cg_add(g, cg_mult(g, cg_const(g, ibor->gearing()), fixing), cg_const(g, ibor->spread()));
             if (effFloor != Null<Real>())
@@ -182,16 +182,7 @@ AmcCgBaseEngine::CashflowInfo AmcCgBaseEngine::createCashflowInfo(QuantLib::ext:
             if (isNakedOption && effFloor == Null<Real>()) {
                 capletRate = cg_mult(g, capletRate, cg_const(g, -1.0));
             }
-            effectiveRate = swapletRate;
-            if (floorletRate != ComputationGraph::nan)
-                effectiveRate = cg_add(g, effectiveRate, floorletRate);
-            if (capletRate != ComputationGraph::nan) {
-                if (isNakedOption && effFloor == Null<Real>()) {
-                    effectiveRate = cg_subtract(g, effectiveRate, capletRate);
-                } else {
-                    effectiveRate = cg_add(g, effectiveRate, capletRate);
-                }
-            }
+            effectiveRate = cg_subtract(g, cg_add(g, swapletRate, floorletRate), capletRate);
         } else {
             effectiveRate = cg_add(g, cg_mult(g, cg_const(g, ibor->gearing()), fixing), cg_const(g, ibor->spread()));
         }
