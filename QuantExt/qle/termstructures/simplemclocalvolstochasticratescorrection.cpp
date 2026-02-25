952ff79b1040f25d915f88e63af88f250893f3bd
Peter Caspers <peter.caspers@lseg.com>
2026-02-25 13:16:39 +0100

    QPR-13825 allow for second pass with pc, possibly more for convergence

--- QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.cpp
+++ QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.cpp
@@ -19,9 +19,9 @@
 #include <qle/methods/multipathvariategenerator.hpp>
 #include <qle/termstructures/simplemclocalvolstochasticratescorrection.hpp>
 
-#include <ql/math/interpolations/linearinterpolation.hpp>
 #include <ql/math/interpolations/bilinearinterpolation.hpp>
 #include <ql/math/interpolations/flatextrapolation2d.hpp>
+#include <ql/math/interpolations/linearinterpolation.hpp>
 #include <ql/math/matrixutilities/pseudosqrt.hpp>
 #include <ql/pricingengines/blackformula.hpp>
 
@@ -42,11 +42,11 @@ SimpleMcLocalVolStochasticRatesCorrection::SimpleMcLocalVolStochasticRatesCorrec
     QuantLib::ext::shared_ptr<IrModel> r, QuantLib::ext::shared_ptr<IrModel> q, QuantLib::ext::shared_ptr<FxModel> S,
     QuantLib::Handle<QuantLib::YieldTermStructure> r0, QuantLib::Handle<QuantLib::YieldTermStructure> q0,
     Matrix correlation_r_q_S, Real maxTime, Real minStrike, Real maxStrike, Size timeStepsPerYear, Size nStrikes,
-    Size nPaths, Real d2CdK2Threshold)
+    Size nPaths, Real d2CdK2Threshold, Size nPasses)
     : blackVol_(std::move(blackVol)), source_(std::move(source)), r_(std::move(r)), q_(std::move(q)), S_(std::move(S)),
       r0_(std::move(r0)), q0_(std::move(q0)), correlation_r_q_S_(std::move(correlation_r_q_S)), maxTime_(maxTime),
       minStrike_(minStrike), maxStrike_(maxStrike), timeStepsPerYear_(timeStepsPerYear), nStrikes_(nStrikes),
-      nPaths_(nPaths), d2CdK2Threshold_(d2CdK2Threshold) {
+      nPaths_(nPaths), d2CdK2Threshold_(d2CdK2Threshold), nPasses_(nPasses) {
 
     timeGrid_ = TimeGrid(maxTime_, static_cast<Size>(0.5 + maxTime_ * static_cast<Real>(timeStepsPerYear_)));
 
@@ -101,124 +101,129 @@ void SimpleMcLocalVolStochasticRatesCorrection::performCalculations() const {
 
     Matrix sqrtCorrelation = pseudoSqrt(correlation_r_q_S_);
 
-    // mc simulation to determine correction term
+    for (Size pass = 0; pass < nPasses_; ++pass) {
 
-    std::vector<Array> rState(nPaths_, r_->initialValue());
-    std::vector<Array> qState(nPaths_, q_->initialValue());
-    std::vector<Array> sState(nPaths_, S_->initialValue());
-    std::vector<Array> rState_u(nPaths_, r_->initialValue());
-    std::vector<Array> qState_u(nPaths_, q_->initialValue());
-    std::vector<Array> sState_u(nPaths_, S_->initialValue());
+        FxModel::Discretization fxDisc = pass == 0 ? FxModel::Discretization::Euler : FxModel::Discretization::PC;
 
-    std::vector<Real> r(nPaths_);
-    std::vector<Real> q(nPaths_);
-    std::vector<Real> n(nPaths_);
-    std::vector<Real> r_u(nPaths_);
-    std::vector<Real> q_u(nPaths_);
-    std::vector<Real> n_u(nPaths_);
+        // mc simulation to determine correction term
 
-    for (Size i = 1; i < timeGrid_.size(); ++i) {
+        std::vector<Array> rState(nPaths_, r_->initialValue());
+        std::vector<Array> qState(nPaths_, q_->initialValue());
+        std::vector<Array> sState(nPaths_, S_->initialValue());
+        std::vector<Array> rState_u(nPaths_, r_->initialValue());
+        std::vector<Array> qState_u(nPaths_, q_->initialValue());
+        std::vector<Array> sState_u(nPaths_, S_->initialValue());
 
-        Real t0 = timeGrid_[i - 1];
-        Real t1 = timeGrid_[i];
-        Real dt = t1 - t0;
+        std::vector<Real> r(nPaths_);
+        std::vector<Real> q(nPaths_);
+        std::vector<Real> n(nPaths_);
+        std::vector<Real> r_u(nPaths_);
+        std::vector<Real> q_u(nPaths_);
+        std::vector<Real> n_u(nPaths_);
 
-        // generate path values
+        for (Size i = 1; i < timeGrid_.size(); ++i) {
 
-        for (Size p = 0; p < nPaths_; ++p) {
+            Real t0 = timeGrid_[i - 1];
+            Real t1 = timeGrid_[i];
+            Real dt = t1 - t0;
 
-            Array dw = variates[p][i - 1] * sqrtCorrelation;
-
-            Array rStateUpd = r_->marginalStep(t0, rState[p], dt, getProjectedArray(dw, 0, m_r));
-            Array qStateUpd = q_->marginalStep(t0, qState[p], dt, getProjectedArray(dw, m_r, m_q));
-
-            Real rs = -std::log(r_->discountBond(t0, t1, rStateUpd, r0_)) / dt;
-            Real qs = -std::log(q_->discountBond(t0, t1, qStateUpd, q0_)) / dt;
-
-            Array sStateUpd = S_->marginalStep(t0, sState[p], dt, getProjectedArray(dw, m_r + m_q, m_s), rs, qs,
-                                               FxModel::Discretization::Euler);
-
-            Array rStateUpd_u = r_->marginalStep(t0, rState_u[p], dt, Array(m_r, 0.0));
-            Array qStateUpd_u = q_->marginalStep(t0, qState_u[p], dt, Array(m_q, 0.0));
-
-            Real rs_u = -std::log(r_->discountBond(t0, t1, rStateUpd_u, r0_)) / dt;
-            Real qs_u = -std::log(q_->discountBond(t0, t1, qStateUpd_u, q0_)) / dt;
-
-            applyCorrection_ = false;
-            Array sStateUpd_u = S_->marginalStep(t0, sState_u[p], dt, getProjectedArray(dw, m_r + m_q, m_s), rs_u, qs_u,
-                                                 FxModel::Discretization::Euler);
-            applyCorrection_ = true;
-
-            rState[p] = rStateUpd;
-            qState[p] = qStateUpd;
-            sState[p] = sStateUpd;
-            r[p] = rs;
-            q[p] = qs;
-            n[p] = r_->numeraire(t1, getProjectedArray(rStateUpd, 0, m_r), r0_);
-
-            rState_u[p] = rStateUpd_u;
-            qState_u[p] = qStateUpd_u;
-            sState_u[p] = sStateUpd_u;
-            r_u[p] = rs_u;
-            q_u[p] = qs_u;
-            n_u[p] = r_->numeraire(t1, getProjectedArray(rStateUpd_u, 0, m_r), r0_);
-
-        }
-
-        // iterate over strikes and accumulate results
-
-        Real F = S_->fxSpotToday()->value() * q0_->discount(t1) / r0_->discount(t1);
-
-        Real e1 = 0.0, e2 = 0.0;
-        Real e1_u = 0.0, e2_u = 0.0;
-
-        Real K_prev = F * 2.0 * std::exp(logStrikes_.back() * std::sqrt(atmVarianceData_[i]));
-
-        for (int k = logStrikes_.size() - 1; k >= 0; --k) {
-
-            Real K = F * std::exp(logStrikes_[k] * std::sqrt(atmVarianceData_[i]));
-
-            Real n1 = 0.0, n2 = 0.0;
-
-            Real d2CdK2Count = 0.0;
+            // generate path values
 
             for (Size p = 0; p < nPaths_; ++p) {
-                if (sState[p][0] > std::log(K)) {
-                    n1 += (r[p] - q[p]) / n[p];
-                    n2 += q[p] * (std::exp(sState[p][0]) - K) / n[p];
-                    if (sState[p][0] < std::log(K_prev)) {
-                        d2CdK2Count += 1.0 / n[p];
+
+                Array dw = variates[p][i - 1] * sqrtCorrelation;
+
+                Array rStateUpd = r_->marginalStep(t0, rState[p], dt, getProjectedArray(dw, 0, m_r));
+                Array qStateUpd = q_->marginalStep(t0, qState[p], dt, getProjectedArray(dw, m_r, m_q));
+
+                Real rs = -std::log(r_->discountBond(t0, t1, rStateUpd, r0_)) / dt;
+                Real qs = -std::log(q_->discountBond(t0, t1, qStateUpd, q0_)) / dt;
+
+                Array sStateUpd =
+                    S_->marginalStep(t0, sState[p], dt, getProjectedArray(dw, m_r + m_q, m_s), rs, qs, fxDisc);
+
+                Array rStateUpd_u = r_->marginalStep(t0, rState_u[p], dt, Array(m_r, 0.0));
+                Array qStateUpd_u = q_->marginalStep(t0, qState_u[p], dt, Array(m_q, 0.0));
+
+                Real rs_u = -std::log(r_->discountBond(t0, t1, rStateUpd_u, r0_)) / dt;
+                Real qs_u = -std::log(q_->discountBond(t0, t1, qStateUpd_u, q0_)) / dt;
+
+                applyCorrection_ = true;
+                Array sStateUpd_u =
+                    S_->marginalStep(t0, sState_u[p], dt, getProjectedArray(dw, m_r + m_q, m_s), rs_u, qs_u, fxDisc);
+                applyCorrection_ = true;
+
+                rState[p] = rStateUpd;
+                qState[p] = qStateUpd;
+                sState[p] = sStateUpd;
+                r[p] = rs;
+                q[p] = qs;
+                n[p] = r_->numeraire(t1, getProjectedArray(rStateUpd, 0, m_r), r0_);
+
+                rState_u[p] = rStateUpd_u;
+                qState_u[p] = qStateUpd_u;
+                sState_u[p] = sStateUpd_u;
+                r_u[p] = rs_u;
+                q_u[p] = qs_u;
+                n_u[p] = r_->numeraire(t1, getProjectedArray(rStateUpd_u, 0, m_r), r0_);
+            }
+
+            // iterate over strikes and accumulate results
+
+            Real F = S_->fxSpotToday()->value() * q0_->discount(t1) / r0_->discount(t1);
+
+            Real e1 = 0.0, e2 = 0.0;
+            Real e1_u = 0.0, e2_u = 0.0;
+
+            Real K_prev = F * 2.0 * std::exp(logStrikes_.back() * std::sqrt(atmVarianceData_[i]));
+
+            for (int k = logStrikes_.size() - 1; k >= 0; --k) {
+
+                Real K = F * std::exp(logStrikes_[k] * std::sqrt(atmVarianceData_[i]));
+
+                Real n1 = 0.0, n2 = 0.0;
+
+                Real d2CdK2Count = 0.0;
+
+                for (Size p = 0; p < nPaths_; ++p) {
+                    if (sState[p][0] > std::log(K)) {
+                        n1 += (r[p] - q[p]) / n[p];
+                        n2 += q[p] * (std::exp(sState[p][0]) - K) / n[p];
+                        if (sState[p][0] < std::log(K_prev)) {
+                            d2CdK2Count += 1.0 / n[p];
+                        }
                     }
                 }
-            }
 
-            e1 = static_cast<Real>(n1) / static_cast<Real>(nPaths_);
-            e2 = static_cast<Real>(n2) / static_cast<Real>(nPaths_);
+                e1 = static_cast<Real>(n1) / static_cast<Real>(nPaths_);
+                e2 = static_cast<Real>(n2) / static_cast<Real>(nPaths_);
 
-            Real d2CdK2 = d2CdK2Count / static_cast<Real>(nPaths_) / (K_prev - K);
+                Real d2CdK2 = d2CdK2Count / static_cast<Real>(nPaths_) / (K_prev - K);
 
-            Real n1_u = 0.0, n2_u = 0.0;
+                Real n1_u = 0.0, n2_u = 0.0;
 
-            for (Size p = 0; p < nPaths_; ++p) {
-                if (sState_u[p][0] > std::log(K)) {
-                    n1_u += (r_u[p] - q_u[p]) / n_u[p];
-                    n2_u += q_u[p] * (std::exp(sState_u[p][0]) - K) / n_u[p];
+                for (Size p = 0; p < nPaths_; ++p) {
+                    if (sState_u[p][0] > std::log(K)) {
+                        n1_u += (r_u[p] - q_u[p]) / n_u[p];
+                        n2_u += q_u[p] * (std::exp(sState_u[p][0]) - K) / n_u[p];
+                    }
                 }
+
+                e1_u = static_cast<Real>(n1_u) / static_cast<Real>(nPaths_);
+                e2_u = static_cast<Real>(n2_u) / static_cast<Real>(nPaths_);
+
+                if (d2CdK2 * (K_prev - K) > d2CdK2Threshold_) {
+                    correctionData_(k, i - 1) = (-K * (e1 - e1_u) + (e2 - e2_u)) / (0.5 * K * K * d2CdK2);
+                }
+
+                K_prev = K;
             }
 
-            e1_u = static_cast<Real>(n1_u) / static_cast<Real>(nPaths_);
-            e2_u = static_cast<Real>(n2_u) / static_cast<Real>(nPaths_);
+            correction_->update();
 
-            if (d2CdK2 * (K_prev - K) > d2CdK2Threshold_) {
-                correctionData_(k, i - 1) = (-K * (e1 - e1_u) + (e2 - e2_u)) / (0.5 * K * K * d2CdK2);
-            }
+        } // loop over time grid
 
-            K_prev = K;
-        }
-
-        correction_->update();
-
-    } // loop over time grid
+    } // loop over passes
 }
 
 Volatility SimpleMcLocalVolStochasticRatesCorrection::localVolImpl(Time t, Real strike) const {
--- QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.hpp
+++ QuantExt/qle/termstructures/simplemclocalvolstochasticratescorrection.hpp
@@ -44,7 +44,7 @@ public:
         QuantLib::ext::shared_ptr<FxModel> S, QuantLib::Handle<QuantLib::YieldTermStructure> r0,
         QuantLib::Handle<QuantLib::YieldTermStructure> q0, Matrix correlation_r_q_S, Real maxTime = 10.0,
         Real minStrike = -3.0, Real maxStrike = 3.0, Size timeStepsPerYear = 24, Size nStrikes = 25,
-        Size nPaths = 10000, Real d2CdK2Threshold = 0.01);
+        Size nPaths = 10000, Real d2CdK2Threshold = 0.01, Size nPasses = 1);
 
     Date maxDate() const override { return source_->maxDate(); }
     Real minStrike() const override { return source_->minStrike(); }
@@ -74,6 +74,7 @@ private:
     Size nStrikes_;
     Size nPaths_;
     Real d2CdK2Threshold_;
+    Size nPasses_;
 
     mutable bool applyCorrection_ = true;
     mutable TimeGrid timeGrid_;
