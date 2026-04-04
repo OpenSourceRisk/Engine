193a0bd3cf3693cf095bfb81fb5140ae6fada147
francis duffy <francis.duffy@lseg.com>
2026-04-04 13:34:48 +0100

    Minor changes to IndexCreditDefaultSwapOption.
    Make the Trade::reset method virtual so that derived classes can reset
    elements that should only be populated during build().
    
    In this case, logic in build() relied upon checking if tradeDate and
    fepStartDate were equal to Date() and then went on to populate them in a
    default way if they held Date(). However, there are multiple passes of
    build() within a run. So, on first pass tradeDate and fepStartDate get
    populated with non-null Date instances and then on subsequent passes the
    logic changes because it sees that tradeDate and fepStartDate have
    non-null values. Better to store strings for these values, use those
    strings as the logic switch, populate the Date instances in that logic
    and reset them to Date() in the reset() call. That way on every pass of
    build(), the logic remains the same. Also the reset() avoids the need to
    call .clear(), = false, = Notionals() etc. during build().
    
    Initialise members in IndexCreditDefaultSwapOption that should be
    initialised e.g. enums, Reals. Also, best to be consistent with how we
    initialise members i.e. via the initialiser list or in the class body.
    Mixed approaches in the same class looks messy.

--- OREData/ored/portfolio/indexcreditdefaultswapoption.hpp
+++ OREData/ored/portfolio/indexcreditdefaultswapoption.hpp
@@ -36,12 +36,13 @@ public:
     IndexCreditDefaultSwapOption(const ore::data::Envelope& env, const IndexCreditDefaultSwapData& swap,
                                  const ore::data::OptionData& option, QuantLib::Real strike,
                                  const std::string& indexTerm = "", const std::string& strikeType = "Spread",
-                                 const QuantLib::Date& tradeDate = Date(), const QuantLib::Date& fepStartDate = Date());
+                                 const std::string& tradeDateStr = "", const std::string& fepStartDateStr = "");
 
     //! \name Trade
     //@{
     void build(const QuantLib::ext::shared_ptr<EngineFactory>&) override;
     QuantLib::Real notional() const override;
+    void reset() override;
     //@}
 
     //! \name Serialisation
@@ -58,10 +59,12 @@ public:
     QuantLib::Real strike() const;
     QuantLib::Option::Type callPut() const;
     const std::string& strikeType() const;
+    const std::string& tradeDateStr() const;
+    const std::string& fepStartDateStr() const;
+    // only available after build()
     const QuantLib::Date& tradeDate() const;
     const QuantLib::Date& fepStartDate() const;
     const CreditPortfolioSensitivityDecomposition sensitivityDecomposition() const;
-    // only available after build()
     QuantLib::Real effectiveStrike() const;
     const std::string& effectiveStrikeType() const;
     const QuantLib::Period& effectiveIndexTerm() const;
@@ -76,10 +79,12 @@ private:
     QuantLib::Real strike_;
     std::string indexTerm_;
     std::string strikeType_;
+    std::string tradeDateStr_;
+    std::string fepStartDateStr_;
+
     QuantLib::Date tradeDate_;
     QuantLib::Date fepStartDate_;
     CreditPortfolioSensitivityDecomposition sensitivityDecomposition_;
-
     QuantLib::Real effectiveStrike_;
     std::string effectiveStrikeType_;
     QuantLib::Period effectiveIndexTerm_;
@@ -99,7 +104,7 @@ private:
 
     //! Populated during trade building
     Notionals notionals_;
-    bool defaultHasOccured_ = false;
+    bool defaultHasOccured_;
 
     //! map of all the constituents to notionals
     map<string, Real> constituents_;
