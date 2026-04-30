e24bf8375587d25f5a806e116cc4c7bdcca34b67
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-04-30 16:31:08 +0100

    QPR-13716 remove restore input var not needed

--- OREAnalytics/orea/app/analytics/correlationanalytic.cpp
+++ OREAnalytics/orea/app/analytics/correlationanalytic.cpp
@@ -39,7 +39,7 @@ void CorrelationVariables::loadVariablesImpl(const QuantLib::ext::shared_ptr<Inp
     vector<string> correlationAnalytics = {"correlation", "xva"};
 
     inputs->loadParameterXML<ScenarioSimMarketParameters>(simMarketParams_, correlationAnalytics,
-                                                              vector<string>({"simulationConfigFile"}));
+                                                              vector<string>({"simulationConfigFile", "simulationUri"}));
     inputs->loadParameterXML<SensitivityScenarioData>(sensiScenarioData_, correlationAnalytics, "sensitivityConfigFile");
 
     inputs->loadParameter<string>(lookbackPeriod_, correlationAnalytics,
