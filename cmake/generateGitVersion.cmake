e4db0fc16c63b4166bfa1f3c5a503891166b05c4
Peter Caspers <peter.caspers@lseg.com>
2026-05-16 19:19:28 +0200

    ACADIAQPR-14103 more output to debug ci

--- cmake/generateGitVersion.cmake
+++ cmake/generateGitVersion.cmake
@@ -3,12 +3,14 @@ set(GIT_HASH "unknown")
 find_package(Git QUIET)
 
 if(GIT_FOUND)
+    message(STATUS "Git was found, running git log to extract hash")
     execute_process(
         COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
         OUTPUT_VARIABLE GIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
     )
+else()
+    message(STATUS "Git was not found, can not extract hash")
 endif()
 
 message(STATUS "Git hash is ${GIT_HASH}")
