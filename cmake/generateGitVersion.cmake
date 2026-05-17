55964b5f31b86d0263e84ec7ac6ded6e74ef772e
francis duffy <francis.duffy@lseg.com>
2026-05-17 17:21:02 +0100

    Allow --hash in CI build and log it for tests.

--- cmake/generateGitVersion.cmake
+++ cmake/generateGitVersion.cmake
@@ -1,14 +1,20 @@
-find_package(Git QUIET)
+if(DEFINED GIT_HASH AND NOT GIT_HASH STREQUAL "")
+    message(STATUS "Git hash supplied as ${GIT_HASH}")
+else()
+    find_package(Git QUIET)
 
-if(GIT_FOUND)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
-        OUTPUT_VARIABLE GIT_HASH
-        RESULT_VARIABLE RETURN_CODE
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
-    if(NOT RETURN_CODE EQUAL 0)
+    if(GIT_FOUND)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%h
+            OUTPUT_VARIABLE GIT_HASH
+            RESULT_VARIABLE RETURN_CODE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET
+        )
+        if(NOT RETURN_CODE EQUAL 0)
+            set(GIT_HASH "unknown")
+        endif()
+    else()
         set(GIT_HASH "unknown")
     endif()
 endif()
