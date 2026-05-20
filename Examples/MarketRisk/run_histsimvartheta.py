3f8879a6539346082600956e856c7055d1511783
Peter.Caspers <peter.caspers@lseg.com>
2026-05-20 13:12:57 +0200

    Merge branch 'feature/QPR-14058' into 'master'
    QPR-14058 DirtyPnL for HistSimVar
    
    Closes QPR-14058
    
    See merge request app/app-51172/qs/oreplus!3481

---
+++ Examples/MarketRisk/run_histsimvartheta.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../')
+from ore_examples_helper import OreExample
+
+oreex = OreExample(sys.argv[1] if len(sys.argv)>1 else False)
+
+print("+----------------------------------------+")
+print("| Hist Sim VaR Theta                     |")
+print("+----------------------------------------+")
+
+# legacy example 58
+
+oreex.print_headline("Run ORE for HistSim VaR Dirty PnL")
+oreex.run("Input/ore_histsimvar_theta.xml")
+
