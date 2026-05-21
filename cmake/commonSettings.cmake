8d34de21a2d67141f9f572dee96a40cba1e77f81
Farah.Khashman <farah.khashman@lseg.com>
2026-05-21 18:04:41 +0000

    Merge branch 'bugfix/QPR-14106' into 'master'
    QPR-14106 BugFix Compiling Stacktrace Windows
    
    Closes QPR-14106
    
    See merge request app/app-51172/qs/oreplus!3531

--- cmake/commonSettings.cmake
+++ cmake/commonSettings.cmake
@@ -230,7 +230,10 @@ endif()
 set(Boost_NO_WARN_NEW_VERSIONS ON)
 
 # Find Boost components.
-list(APPEND BOOST_COMPONENT_LIST filesystem serialization timer log iostreams thread stacktrace_basic)
+list(APPEND BOOST_COMPONENT_LIST filesystem serialization timer log iostreams thread)
+if(NOT MSVC)
+    list(APPEND BOOST_COMPONENT_LIST stacktrace_basic)
+endif()
 find_package(Boost REQUIRED COMPONENTS ${BOOST_COMPONENT_LIST})
 
 if (MSVC)
