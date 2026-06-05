585867773d495fb31bd223dd434ba5f97d429ae0
Peter Caspers <peter.caspers@lseg.com>
2026-06-05 12:25:34 +0200

    ACADIAQPR-12228 temp commit: disable cache for testing

--- QuantExt/qle/math/randomvariable_ops.cpp
+++ QuantExt/qle/math/randomvariable_ops.cpp
@@ -33,6 +33,9 @@ RandomVariable randomVariableOpConditionalExpectation(const Size size, const Siz
                                                       const std::vector<const RandomVariable*>& args,
                                                       RandomVariableRegressionCache* cache) {
 
+    // disable cache for testing....
+    cache = nullptr;
+
     QL_REQUIRE(args.size() >= 2,
                "randomVariableOpConditionalExpectation(): args size (" << args.size() << ") must be geq 2");
     QL_REQUIRE(args.size() % 2 == 0,
