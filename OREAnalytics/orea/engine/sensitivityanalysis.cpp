a9c47d2f1e8cad9b1a61861bff367b07637893c2
Shen <zeyu.shen@lseg.com>
2026-06-09 21:08:44 -0400

    ACADIAQPR-12064:: Add missing parameter to calculators

--- OREAnalytics/orea/engine/sensitivityanalysis.cpp
+++ OREAnalytics/orea/engine/sensitivityanalysis.cpp
@@ -367,7 +367,7 @@ void SensitivityAnalysis::generateSensitivities() {
             auto baseCcy = simMarketData_->baseCcy();
             engine.buildCube(
                 pf,
-                [&baseCcy, this](const QuantLib::ext::shared_ptr<ore::data::Portfolio>& p)
+                [&baseCcy, this](const QuantLib::Size, const QuantLib::ext::shared_ptr<ore::data::Portfolio>& p)
                     -> std::vector<QuantLib::ext::shared_ptr<ValuationCalculator>> {
                     return {QuantLib::ext::make_shared<NPVCalculator>(baseCcy, 0, laxFxConversion_)};
                 },
