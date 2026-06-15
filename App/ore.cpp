982980487b3d4f5dcfd4e400393a336d10882038
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-06-15 09:06:14 +0100

    QPR-14083 ore_plus and ore to allow command lines override

--- App/ore.cpp
+++ App/ore.cpp
@@ -32,7 +32,11 @@
 #include <qle/version.hpp>
 #include <qle/gitversion.hpp>
 
+#include <boost/program_options.hpp>
+
 #include <iostream>
+
+namespace po = boost::program_options;
 
 #if !defined(BOOST_ALL_NO_LIB) && defined(BOOST_MSVC)
 #include <orea/auto_link.hpp>
@@ -45,6 +49,8 @@
 #define BOOST_LIB_NAME boost_date_time
 #include <boost/config/auto_link.hpp>
 #define BOOST_LIB_NAME boost_system
+#include <boost/config/auto_link.hpp>
+#define BOOST_LIB_NAME boost_program_options
 #include <boost/config/auto_link.hpp>
 #define BOOST_LIB_NAME boost_timer
 #include <boost/config/auto_link.hpp>
@@ -70,18 +76,87 @@ int main(int argc, char** argv) {
         exit(0);
     }
 
-    if (argc != 2) {
-        std::cout << endl << "usage: ORE path/to/ore.xml" << endl << endl;
+    // Parse command line arguments. The following form is supported:
+    //   ore --input path/to/ore.xml [--output OutputDir]
+    // In addition, any other --<name> <value> option is applied as an override
+    // to the "setup" group of the parameters, e.g. --logMask 255, --marketDataFile market.txt.
+    string inputFile;
+    string outputPath;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "Produce help message")
+        ("input,i", po::value<string>(&inputFile), "Input file (path to ore.xml)")
+        ("output,o", po::value<string>(&outputPath), "Output directory, overrides setup/outputPath");
+
+    po::variables_map vm;
+    std::vector<std::string> unrecognized;
+    try {
+        po::parsed_options parsed = po::command_line_parser(argc, argv)
+                                        .options(desc)
+                                        .allow_unregistered()
+                                        .run();
+        po::store(parsed, vm);
+        po::notify(vm);
+        unrecognized = po::collect_unrecognized(parsed.options, po::include_positional);
+    } catch (const std::exception& e) {
+        cout << "error parsing command line: " << e.what() << endl << endl << desc << endl;
+        return -1;
+    }
+
+    if (vm.count("help")) {
+        cout << "usage: ORE --input path/to/ore.xml [--output OutputDir] [--<setupParam> <value> ...]" << endl
+             << endl
+             << desc << endl;
+        return 0;
+    }
+
+    // Collect any additional --<name> <value> options as overrides for the "setup" group.
+    std::map<std::string, std::string> setupOverrides;
+    for (std::size_t i = 0; i < unrecognized.size(); ++i) {
+        const std::string& token = unrecognized[i];
+        if (token.size() > 2 && token.substr(0, 2) == "--") {
+            std::string key = token.substr(2);
+            std::string value;
+            // Support both "--name value" and "--name=value".
+            auto eq = key.find('=');
+            if (eq != std::string::npos) {
+                value = key.substr(eq + 1);
+                key = key.substr(0, eq);
+            } else if (i + 1 < unrecognized.size() && unrecognized[i + 1].substr(0, 2) != "--") {
+                value = unrecognized[++i];
+            }
+            if (key.empty()) {
+                cout << "error parsing command line: empty option name" << endl << endl << desc << endl;
+                return -1;
+            }
+            setupOverrides[key] = value;
+        } else {
+            cout << "error parsing command line: unexpected argument '" << token << "'" << endl << endl
+                 << desc << endl;
+            return -1;
+        }
+    }
+
+    if (inputFile.empty()) {
+        cout << endl
+             << "usage: ORE --input path/to/ore.xml [--output OutputDir] [--<setupParam> <value> ...]" << endl
+             << endl
+             << desc << endl;
         return -1;
     }
 
     ore::analytics::initBuilders();
 
-    string inputFile(argv[1]);
-
     try {
         auto params = QuantLib::ext::make_shared<Parameters>();
         params->fromFile(inputFile);
+        if (!outputPath.empty())
+            params->set("setup", "outputPath", outputPath);
+        for (const auto& kv : setupOverrides) {
+            cout << "overriding setup/" << kv.first << " = " << kv.second << endl;
+            params->set("setup", kv.first, kv.second);
+        }
         OREApp ore(params, true);
         ore.run();
         return 0;
