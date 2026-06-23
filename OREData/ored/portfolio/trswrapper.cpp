e76b1cc5fbc997d6ed8752ca451b562ca70817f9
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-06-23 14:58:32 +0100

    QPR-14218 Optimise Logic

--- OREData/ored/portfolio/trswrapper.cpp
+++ OREData/ored/portfolio/trswrapper.cpp
@@ -513,23 +513,12 @@ void TRSWrapperAccrualEngine::calculate() const {
                     s1 = getUnderlyingNPV(i, s1AdditionalData);
                     fx1 = getFxConversionRate(today, arguments_.assetCurrency_[i], arguments_.returnCurrency_, true);
                 } else if (!arguments_.portfolioId_.empty() && arguments_.pricePerIndexUnit_) {
-                    // Basket priced per index unit: use basket fixing once (i == 0) and set other decomposed
-                    // constituents to zero. If basket fixing is unavailable, fall back to underlying valuation.
-                    if (i == 0) {
-                        try {
-                            s1 = getUnderlyingFixing(i, endDate, false, s1AdditionalData) * arguments_.indexQuantity_;
-                            fx1 = getFxConversionRate(endDate, arguments_.initialPriceCurrency_,
-                                                      arguments_.returnCurrency_, false);
-                        } catch (...) {
-                            s1 = getUnderlyingNPV(i, s1AdditionalData);
-                            fx1 =
-                                getFxConversionRate(today, arguments_.assetCurrency_[i], arguments_.returnCurrency_, true);
-                        }
-                    } else {
-                        // i > 0: use individual underlying component fixing, consistent with S0 logic.
-                        s1 = getUnderlyingFixing(i, endDate, false, s1AdditionalData) * arguments_.underlyingMultiplier_[i];
-                        fx1 = getFxConversionRate(endDate, arguments_.assetCurrency_[i], arguments_.returnCurrency_, false);
-                    }
+                    // Basket priced per index unit: use basket fixing for i==0, individual component fixing for i>0.
+                    // If basket fixing is unavailable, fall back to underlying valuation.
+                    Real multiplier = (i == 0) ? arguments_.indexQuantity_ : arguments_.underlyingMultiplier_[i];
+                    const Currency& priceCurrency = (i == 0) ? arguments_.initialPriceCurrency_ : arguments_.assetCurrency_[i];
+                    s1 = getUnderlyingFixing(i, endDate, false, s1AdditionalData) * multiplier;
+                    fx1 = getFxConversionRate(endDate, priceCurrency, arguments_.returnCurrency_, false);
                 } else {
                     s1 = getUnderlyingFixing(i, endDate, false, s1AdditionalData) * arguments_.underlyingMultiplier_[i];
                     fx1 = getFxConversionRate(endDate, arguments_.assetCurrency_[i], arguments_.returnCurrency_, false);
