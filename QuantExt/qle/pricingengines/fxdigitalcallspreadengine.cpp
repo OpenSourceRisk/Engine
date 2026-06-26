0f678ecfbbcfb007440845114211aabad7158211
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-06-26 10:56:34 +0100

    QPR-13679 Update to fit CashSettlement Framework

--- QuantExt/qle/pricingengines/fxdigitalcallspreadengine.cpp
+++ QuantExt/qle/pricingengines/fxdigitalcallspreadengine.cpp
@@ -59,58 +59,111 @@ void FxDigitalCallSpreadEngine::calculate() const {
     }
 
     if (expiryDate <= today) {
-        // Expiry has passed: the payout is fixed. Handled identically to
-        // AnalyticCashSettledEuropeanEngine, discounting the cash payoff from the payment date.
+        // If expiry has occurred, we attempt to establish the payoff amount, if any, and discount it.
         Real payoffAmount = 0.0;
+        Real priceAtExercise = 0.0;
+        bool deterministicPayoff = true;
         if (arguments_.automaticExercise) {
+            // If we have automatic exercise, we base the payoff on the value of the index on the expiry date.
             QL_REQUIRE(arguments_.underlying, "Expect a valid underlying index when exercise is automatic.");
-            payoffAmount = (*arguments_.payoff)(arguments_.underlying->fixing(expiryDate));
+            priceAtExercise = arguments_.underlying->fixing(expiryDate);
+            payoffAmount = (*arguments_.payoff)(priceAtExercise);
         } else if (arguments_.exercised) {
-            QL_REQUIRE(arguments_.priceAtExercise != Null<Real>(),
-                       "Expect a valid price at exercise when option has been manually exercised.");
-            payoffAmount = (*arguments_.payoff)(arguments_.priceAtExercise);
+            // If we have manually exercised, we base the payoff on the value at exercise.
+            QL_REQUIRE(arguments_.priceAtExercise != Null<Real>(), "Expect a valid price at exercise when option "
+                                                                       << "has been manually exercised.");
+            priceAtExercise = arguments_.priceAtExercise;
+            payoffAmount = (*arguments_.payoff)(priceAtExercise);
         } else if (expiryDate == today) {
-            payoffAmount = (*arguments_.payoff)(bsp_->x0());
+            // Expiry date is today, not automatic exercise and hasn't been manually exercised - use spot.
+            priceAtExercise = bsp_->x0();
+            payoffAmount = (*arguments_.payoff)(priceAtExercise);
+            deterministicPayoff = false;
         }
 
+        if(deterministicPayoff) {
+            results_.delta = 0.0;
+            results_.deltaForward = 0.0;
+            results_.elasticity = 0.0;
+            results_.gamma = 0.0;
+            results_.dividendRho = 0.0;
+            results_.vega = 0.0;
+        }
+
+        double fxRate = 1.0;
+        if (arguments_.fxIndex != nullptr) {
+            Date fixingDate = arguments_.cashSettlementFxFixingDate.has_value()
+                                  ? *arguments_.cashSettlementFxFixingDate
+                                  : arguments_.fxIndex->fixingDate(expiryDate);
+            fxRate = arguments_.fxIndex->fixing(fixingDate, false);
+        }
+
+        // Discount factor to payment date.
         DiscountFactor df_tp = dts->discount(arguments_.paymentDate);
+        Time delta_tp = dts->timeFromReference(arguments_.paymentDate);
+
+        // Only value, rho and theta are meaningful now.
         results_.value = df_tp * payoffAmount * fxRate;
-        return;
+        results_.rho = -delta_tp * results_.value * fxRate;
+        results_.theta = 0.0;
+        if (delta_tp > 0.0 && !close(delta_tp, 0.0)) {
+            results_.theta = -std::log(df_tp) / delta_tp * results_.value * fxRate;
+        }
+        results_.thetaPerDay = results_.theta / 365.0;
+
+        // Populate some additional results.
+        results_.additionalResults["spot"] = bsp_->x0();
+        auto payoff = QuantLib::ext::dynamic_pointer_cast<StrikedTypePayoff>(arguments_.payoff);
+        if (payoff)
+            results_.additionalResults["strike"] = payoff->strike();
+        results_.additionalResults["priceAtExercise"] = priceAtExercise;
+        results_.additionalResults["payoffAmount"] = payoffAmount;
+        results_.additionalResults["discountFactor"] = df_tp;
+        results_.additionalResults["timeToExpiry"] = delta_tp;
+        results_.additionalResults["settlementFxFwd"] = fxRate;
+    }else{
+
+        // Expiry in the future: price the call spread assuming payment on the option expiry date, then
+        // account for the delayed payment with the forward discount factor between expiry and payment.
+        Real strikeLo = strike - eps_ / 2.0;
+        Real strikeHi = strike + eps_ / 2.0;
+
+        auto vanillaEngine = ext::make_shared<QuantExt::AnalyticEuropeanEngine>(bsp_, flipResults_);
+
+        auto vanillaData = [&](Real k) {
+            VanillaOption option(ext::make_shared<PlainVanillaPayoff>(type, k), arguments_.exercise);
+            option.setPricingEngine(vanillaEngine);
+            return std::make_pair(option.NPV(), option.delta());
+        };
+
+        auto lo = vanillaData(strikeLo);
+        auto hi = vanillaData(strikeHi);
+        Real priceLo = lo.first, deltaLo = lo.second;
+        Real priceHi = hi.first, deltaHi = hi.second;
+
+        Real cpSpread = cash * (priceLo - priceHi) / eps_;
+        Real spreadValue = type == Option::Call ? cpSpread : -1.0 * cpSpread;
+
+        // Forward discount factor between expiry and payment date, P(t_e, t_p), under deterministic rates.
+        DiscountFactor df_te_tp = dts->discount(arguments_.paymentDate) / dts->discount(expiryDate);
+
+        Real spreadDelta = cash * (deltaLo - deltaHi) / eps_;
+        if (type == Option::Put)
+            spreadDelta = -spreadDelta;
+
+        results_.value = df_te_tp * spreadValue * fxRate;
+        results_.delta = df_te_tp * spreadDelta * fxRate;
+
+        results_.additionalResults["discountFactorTeTp"] = df_te_tp;
+        results_.additionalResults["settlementFxFwd"] = fxRate;
+        results_.additionalResults["priceLo"] = priceLo;
+        results_.additionalResults["priceHi"] = priceHi;
+        results_.additionalResults["deltaLo"] = deltaLo;
+        results_.additionalResults["deltaHi"] = deltaHi;
+
     }
 
-    // Expiry in the future: price the call spread assuming payment on the option expiry date, then
-    // account for the delayed payment with the forward discount factor between expiry and payment.
-    Real strikeLo = strike - eps_ / 2.0;
-    Real strikeHi = strike + eps_ / 2.0;
-
-    auto vanillaEngine = ext::make_shared<QuantExt::AnalyticEuropeanEngine>(bsp_, flipResults_);
-
-    auto vanillaData = [&](Real k) {
-        VanillaOption option(ext::make_shared<PlainVanillaPayoff>(type, k), arguments_.exercise);
-        option.setPricingEngine(vanillaEngine);
-        return std::make_pair(option.NPV(), option.delta());
-    };
-
-    auto lo = vanillaData(strikeLo);
-    auto hi = vanillaData(strikeHi);
-    Real priceLo = lo.first, deltaLo = lo.second;
-    Real priceHi = hi.first, deltaHi = hi.second;
-
-    Real cpSpread = cash * (priceLo - priceHi) / eps_;
-    Real spreadValue = type == Option::Call ? cpSpread : -cpSpread;
-
-    // Forward discount factor between expiry and payment date, P(t_e, t_p), under deterministic rates.
-    DiscountFactor df_te_tp = dts->discount(arguments_.paymentDate) / dts->discount(expiryDate);
-
-    results_.value = df_te_tp * spreadValue * fxRate;
-    results_.delta = df_te_tp * (deltaHi - deltaLo);
-
-    results_.additionalResults["discountFactorTeTp"] = df_te_tp;
-    results_.additionalResults["settlementFxFwd"] = fxRate;
-    results_.additionalResults["priceLo"] = priceLo;
-    results_.additionalResults["priceHi"] = priceHi;
-    results_.additionalResults["deltaLo"] = deltaLo;
-    results_.additionalResults["deltaHi"] = deltaHi;
+    
 }
 
 } // namespace QuantExt
