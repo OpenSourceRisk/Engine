c7cea9977a9d7e612408c3a6dcd6468c3be21e81
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-07-02 15:46:33 +0100

    ACADIAQPR-12832 AMC Multithreading to handle ORE_MULTITHREADING_CPU_AFFINITY

--- OREAnalytics/orea/engine/amcvaluationengine.cpp
+++ OREAnalytics/orea/engine/amcvaluationengine.cpp
@@ -43,6 +43,11 @@
 
 #include <future>
 
+#ifdef ORE_MULTITHREADING_CPU_AFFINITY
+#include <pthread.h>
+#include <sched.h>
+#endif
+
 using namespace ore::data;
 using namespace ore::analytics;
 
@@ -71,6 +76,35 @@ struct AmcTradeInfo {
 };
 
 namespace {
+
+#ifdef ORE_MULTITHREADING_CPU_AFFINITY
+std::vector<std::size_t> getCpuIds(std::size_t nThreads) {
+
+    std::size_t nCPU = std::max(1U, std::thread::hardware_concurrency());
+    WLOG("[AMC_MULTITHREADING] Number of CPUs found: " << nCPU);
+
+    std::vector<std::size_t> result(nThreads);
+
+    std::mt19937 gen{std::random_device{}()};
+    std::vector<std::size_t> availableCpus;
+
+    for (std::size_t i = 0; i < nThreads; ++i) {
+        if (availableCpus.empty()) {
+            availableCpus.resize(nCPU);
+            std::iota(availableCpus.begin(), availableCpus.end(), 0);
+        }
+        std::uniform_int_distribution<> distrib(0, availableCpus.size() - 1);
+        auto pos = std::next(availableCpus.begin(), distrib(gen));
+        result[i] = *pos;
+        availableCpus.erase(pos);
+    }
+
+    for (std::size_t i = 0; i < nThreads; ++i) {
+        WLOG("[AMC_MULTITHREADING] Assigning thread " << i << " to CPU #" << result[i]);
+    }
+    return result;
+}
+#endif
 
 Real fx(const std::vector<std::vector<std::vector<Real>>>& fxBuffer, const Size ccyIndex, const Size timeIndex,
         const Size sample) {
@@ -840,6 +874,9 @@ void AMCValuationEngine::buildCube(const QuantLib::ext::shared_ptr<Portfolio>& p
 }
 
 void AMCValuationEngine::buildCube(const QuantLib::ext::shared_ptr<ore::data::Portfolio>& portfolio) {
+
+    boost::timer::cpu_timer timer;
+
     LOG("Starting multi-threaded AMCValuationEngine for "
         << portfolio->size() << " trades, " << nSamples_ << " samples and " << scenarioGeneratorData_->getGrid()->size()
         << " dates.");
@@ -928,6 +965,10 @@ void AMCValuationEngine::buildCube(const QuantLib::ext::shared_ptr<ore::data::Po
 
     std::vector<std::thread> jobs; // not needed if thread pool is used
 
+    // pricing stats accumulated in worker threads
+    // std::vector<std::map<std::string, std::pair<std::size_t, unsigned long long>>> workerPricingStats(
+    //     eff_nThreads);
+
     // get obs mode of main thread, so that we can set this mode in the worker threads below
 
     ore::analytics::ObservationMode::Mode obsMode = ore::analytics::ObservationMode::instance().mode();
@@ -974,12 +1015,34 @@ void AMCValuationEngine::buildCube(const QuantLib::ext::shared_ptr<ore::data::Po
     }
 
     // run amc simulation on multiple threads
-
+    std::vector<std::size_t> cpuIds;
+#ifdef ORE_MULTITHREADING_CPU_AFFINITY
+    cpuIds = getCpuIds(eff_nThreads);
+#endif
     for (Size i = 0; i < eff_nThreads; ++i) {
 
-        auto job = [this, obsMode, includeTodaysCashFlows, localIncRefDateEvents, &portfoliosAsString, &loaders,
+        auto job = [this, 
+#ifdef ORE_MULTITHREADING_CPU_AFFINITY
+                    &cpuIds,
+#endif
+                    obsMode, includeTodaysCashFlows, localIncRefDateEvents, &portfoliosAsString, &loaders,
                     &simDates, &stickyCloseOutDates, &progressIndicator, &pathData,
                     &marketModelBuilder](int id) -> resultType {
+
+#ifdef ORE_MULTITHREADING_CPU_AFFINITY
+            pthread_t self = pthread_self();
+            cpu_set_t cpuset;
+            CPU_ZERO(&cpuset);
+            CPU_SET(cpuIds[id], &cpuset);
+            if (int rc = pthread_setaffinity_np(self, sizeof(cpu_set_t), &cpuset)) {
+                WLOG("[AMC_MULTITHREADING] Error while setting cpu affinity for thread "
+                     << id << " to cpu id " << cpuIds[id] << ": got return code " << rc);
+            } else {
+                WLOG("[AMC_MULTITHREADING] Setting cpu affinity for thread " << id << " to cpu id " << cpuIds[id]
+                                                                         << ", running on cpu " << sched_getcpu());
+            }
+#endif
+
             // set thread local singletons
 
             QuantLib::Settings::instance().evaluationDate() = today_;
