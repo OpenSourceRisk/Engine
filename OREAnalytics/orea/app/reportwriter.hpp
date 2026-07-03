440f136501a81434956eeb746082f99d78b95257
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-07-03 17:00:54 +0100

    QPR-14241 Modify variable name for consistency

--- OREAnalytics/orea/app/reportwriter.hpp
+++ OREAnalytics/orea/app/reportwriter.hpp
@@ -251,7 +251,7 @@ public:
         const ext::shared_ptr<InMemoryReport>& t1m1p0NpvReport,
         const ext::shared_ptr<InMemoryReport>& t1m0p1NpvReport,
         const ext::shared_ptr<InMemoryReport>& t1m1p1NpvReport,
-	    const std::map<std::string, std::vector<ore::data::TradeCashflowReportData>>& tradeCashflows,
+	    const std::map<std::string, std::vector<ore::data::TradeCashflowReportData>>& t0TradeCashflows,
 	    const Date& startDate, const Date& endDate,
 	    const std::string& baseCurrency,
 	    const ext::shared_ptr<ore::data::Market>& market, const std::string& configuration,
