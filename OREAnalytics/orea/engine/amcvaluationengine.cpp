1df1c775cc789ad5c98d6510a1008cfb9dffb6b7
sebastien.bouvard <sebastien.bouvard@lseg.com>
2026-07-03 08:34:00 +0100

    QPR-12832 Add headers

--- OREAnalytics/orea/engine/amcvaluationengine.cpp
+++ OREAnalytics/orea/engine/amcvaluationengine.cpp
@@ -42,6 +42,9 @@
 #include <boost/timer/timer.hpp>
 
 #include <future>
+#include <random>   // std::mt19937, std::random_device, std::uniform_int_distribution
+#include <numeric>  // std::iota
+#include <thread>   // std::thread::hardware_concurrency
 
 #ifdef ORE_MULTITHREADING_CPU_AFFINITY
 #include <pthread.h>
