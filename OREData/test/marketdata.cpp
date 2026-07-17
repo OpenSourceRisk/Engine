485420a7b9fdf2d817828f8b88eb52889a1fbc9b
Peter Caspers <peter.caspers@lseg.com>
2026-07-17 11:40:27 +0200

    ACADIAQPR-13601 update unit test

--- OREData/test/marketdata.cpp
+++ OREData/test/marketdata.cpp
@@ -1,3 +1,21 @@
+/*
+ Copyright (C) 2016 Quaternion Risk Management Ltd
+ All rights reserved.
+
+ This file is part of ORE, a free-software/open-source library
+ for transparent pricing and risk analysis - http://opensourcerisk.org
+
+ ORE is free software: you can redistribute it and/or modify it
+ under the terms of the Modified BSD License.  You should have received a
+ copy of the license along with this program.
+ The license is also available online at <http://opensourcerisk.org>
+
+ This program is distributed on the basis that it will form a useful
+ contribution to risk analytics and model standardisation, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+*/
+
 #include <boost/test/unit_test.hpp>
 #include <chrono>
 #include <ored/utilities/marketdata.hpp>
@@ -16,7 +34,7 @@ BOOST_AUTO_TEST_CASE(testValidTwoDigitYear) {
 
 BOOST_AUTO_TEST_CASE(testValidSingleDigitYear) {
     // Test single digit year - should use current decade
-    auto today = std::chrono::year_month_day(std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now()));
+    auto today = Settings::instance().evaluationDate();
     int currentYear = static_cast<int>(today.year());
     int currentYearDecade = (currentYear / 10) * 10;
 
