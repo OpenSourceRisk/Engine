84ec2b4394efd49b2e8aa561ace843e0bd422c99
Peter Caspers <peter.caspers@lseg.com>
2026-07-24 09:34:16 +0200

    ACADIAQPR-14328 initialize fixing manager and update the date at the right time

--- OREAnalytics/orea/app/analytics/pnlanalytic.cpp
+++ OREAnalytics/orea/app/analytics/pnlanalytic.cpp
@@ -152,6 +152,8 @@ void PnlAnalyticImpl::runAnalytic(const QuantLib::ext::shared_ptr<ore::data::InM
     analytic()->setMarket(t0SimMarket_);
     analytic()->buildPortfolio();
 
+    fixingManager->initialise(analytic()->portfolio(), t0SimMarket_);
+
     QuantLib::ext::shared_ptr<InMemoryReport> t0NpvReport = QuantLib::ext::make_shared<InMemoryReport>(inputs_->reportBufferSize());
     ReportWriter(inputs_->reportNaString())
         .writeNpv(*t0NpvReport, effectiveResultCurrency, analytic()->market(), marketConfig,
@@ -277,6 +279,7 @@ void PnlAnalyticImpl::runAnalytic(const QuantLib::ext::shared_ptr<ore::data::InM
      ***********************************************************************************************/
 
     Date d1 = mporDate();
+    fixingManager->update(d1);
     Settings::instance().evaluationDate() = d1;
     analytic()->configurations().asofDate = d1;
     auto simMarket1 = sai->scenarioSimMarket();
@@ -284,7 +287,6 @@ void PnlAnalyticImpl::runAnalytic(const QuantLib::ext::shared_ptr<ore::data::InM
     analytic()->setMarket(simMarket1);
     sgen1->setScenario(t1Scenario);
     simMarket1->scenarioGenerator() = sgen1;
-    fixingManager->update(d1);
     simMarket1->update(d1);
     analytic()->buildPortfolio();
 
@@ -312,10 +314,9 @@ void PnlAnalyticImpl::runAnalytic(const QuantLib::ext::shared_ptr<ore::data::InM
      * 6. Price the t0 portfolio as of t1 using the t1 market for the actual P&L calculation
      *
      ***************************************************************************************/
-        
+
     sgen1->setScenario(sai->scenarioSimMarket()->baseScenario());
     simMarket1->scenarioGenerator() = sgen1;
-    fixingManager->update(d1);
     simMarket1->update(d1);
 
     analytic()->buildPortfolio();
